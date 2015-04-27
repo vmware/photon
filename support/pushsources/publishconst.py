df73f09a71ab261c0e8b6e87837c1bcfb8145098
Siju Maliakkal <smaliakkal@vmware.com>
2015-04-27 12:20:56 -0700

    added the script to push source to the bintray repository

---
+++ support/pushsources/publishconst.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python2
+#
+#    Copyright (C) 2015 VMware, Inc. All rights reserved.
+
+def constant(f):
+    def fset(self, value):
+        raise SyntaxError
+    def fget(self):
+        return f()
+    return property(fget, fset)
+
+
+class publishConst:
+    @constant
+    def new():
+        return "new"
+    @constant
+    def updates():
+        return "updates"
+    @constant
+    def obsoletes():
+        return "obsoletes"
+    @constant
+    def verified():
+        return "verified"
+#pkg attribs
+    @constant
+    def path():
+        return "path"
+    @constant
+    def published():
+        return "published"
+    @constant
+    def name():
+        return "name"
+    @constant
+    def sha1():
+        return "sha1"
+#other 
+    @constant
+    def sha1allfilename():
+        return 'sha1-all'
