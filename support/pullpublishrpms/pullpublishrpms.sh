cfbcb56ec0630baac6beb95041143a894d416212
YustasSwamp <amakhalov@vmware.com>
2015-06-09 15:31:11 -0700

    Improved publishrpm pulling

--- support/pullpublishrpms/pullpublishrpms.sh
+++ support/pullpublishrpms/pullpublishrpms.sh
@@ -20,16 +20,4 @@ if [ ${EUID} -ne 0 ]; then
     exit 1
 fi
 
-mkdir -p ${PUBLISHRPMSPATHDIR}/x86_64
-mkdir -p ${PUBLISHRPMSPATHDIR}/noarch
-
-for line in $(cat rpmfilelist) 
-do 
-    echo $line
-    subRPMpath="x86_64"
-    if [ `echo $line | grep -c "noarch" ` -gt 0 ]; then
-        subRPMpath="noarch"
-    fi
-    cmd= wget https://bintray.com/artifact/download/vmware/photon_release_1.0_TP1_x86_64/$line -P ${PUBLISHRPMSPATHDIR}/${subRPMpath}
-    echo $cmd
-done 
+wget -c -nv  -r -nH --cut-dirs=4 -B https://bintray.com/artifact/download/vmware/photon_release_1.0_TP1_x86_64/ -i rpmfilelist -P ${PUBLISHRPMSPATHDIR}
