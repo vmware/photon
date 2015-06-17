f70dd68851c7fe6694785db12e24c0629d12f6ad
Mahmoud Bassiouny <mbassiouny@vmware.com>
2015-06-17 13:20:13 -0700

    Adding kickstart support
    * Kickstart support enabled using json configuration file.
    * Checked in sample_ks.cfg for reference.
    * The ks conf can be served from:
    	* file system, or
    	* http server, or
    	* an attached cd.

---
+++ installer/modules/m_postinstall.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+import commons
+
+install_phase = commons.POST_INSTALL
+enabled = True
+
+def execute(name, ks_config, config, root):
+
+    if ks_config and 'postinstall' in ks_config:
+        config['postinstall'] = ks_config['postinstall']
+    if 'postinstall' not in config:
+    	return
+    # run the script in the chroot environment
+    script = config['postinstall']
+
+    script_file = os.path.join(root, 'tmp/postinstall.sh')
+
+    with open(script_file,  'wb') as outfile:
+        outfile.write("\n".join(script))
+
+    os.chmod(script_file, 0700);
+    process = subprocess.Popen(["./mk-run-chroot.sh", '-w', root, "/tmp/postinstall.sh"])
+    process.wait()
