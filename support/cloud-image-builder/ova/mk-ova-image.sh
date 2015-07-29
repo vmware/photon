f2b434439ba9399c90aed4ca0419126901a1e8ed
Anish Swaminathan <anishs@vmware.com>
2015-07-28 18:54:40 -0700

    Enable cloud images build

---
+++ support/cloud-image-builder/ova/mk-ova-image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+PHOTON_IMG_OUTPUT_PATH=$1
+SRC_ROOT=$2
+sed "s|VMDK_IMAGE|$PHOTON_IMG_OUTPUT_PATH/photon-ova.vmdk|" vmx-template > /tmp/vmx-temp.vmx
+
+qemu-img convert -f raw -O vmdk -o adapter_type=lsilogic,subformat=streamOptimized $PHOTON_IMG_OUTPUT_PATH/photon-ova.raw $PHOTON_IMG_OUTPUT_PATH/photon-ova.vmdk
+cd $SRC_ROOT/tools/src/vixDiskUtil
+mkdir -p $SRC_ROOT/tools/bin
+make clean
+make
+$SRC_ROOT/tools/bin/vixdiskutil -wmeta toolsVersion 2147483647 $PHOTON_IMG_OUTPUT_PATH/photon-ova.vmdk
+
+cd $PHOTON_IMG_OUTPUT_PATH
+
+ovftool /tmp/vmx-temp.vmx $PHOTON_IMG_OUTPUT_PATH/photon-stream-ova.ova
+
