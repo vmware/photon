47bb39b3f7cd787ea125965994df422037a53c61
Anish Swaminathan <anishs@vmware.com>
2015-08-11 11:58:02 -0700

    Fixes for GCE image

--- support/cloud-image-builder/gce/mk-gce-image.sh
+++ support/cloud-image-builder/gce/mk-gce-image.sh
@@ -4,5 +4,7 @@ PHOTON_IMG_OUTPUT_PATH=$1
 RAWFILE="photon-gce.raw"
 TARFILE="photon-gce.tar.gz"
 
-echo "Creating ${TARFILE} file from ${RAWFILE}.raw."
-tar -Szcf $PHOTON_IMG_OUTPUT_PATH/${TARFILE} $PHOTON_IMG_OUTPUT_PATH/${RAWFILE}
+echo "Creating ${TARFILE} file from ${RAWFILE}."
+cd $PHOTON_IMG_OUTPUT_PATH
+mv ${RAWFILE} disk.raw
+tar -Szcf $PHOTON_IMG_OUTPUT_PATH/${TARFILE} disk.raw
