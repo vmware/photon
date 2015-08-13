606ddfb54974edfe5eac7d14944c0e28e994400b
Mahmoud Bassiouny <mbassiouny@vmware.com>
2015-08-13 09:47:32 -0700

    Add ks support to configure swap and docker partitions size.

--- installer/modules/m_partition.py
+++ installer/modules/m_partition.py
@@ -8,4 +8,10 @@ enabled = True
 def execute(name, ks_config, config, root):
 
 	if ks_config:
-		config['disk'] = commons.partition_disk(ks_config['disk'])
+		docker_partition_size = None
+		swap_partition_size = None
+		if 'docker_partition_size' in ks_config:
+			docker_partition_size = ks_config['docker_partition_size']
+		if 'swap_partition_size' in ks_config:
+			swap_partition_size = ks_config['swap_partition_size']
+		config['disk'] = commons.partition_disk(ks_config['disk'], docker_partition_size, swap_partition_size)
