7dfe9eaa89e28b0fe8883b72a1efd6ae6afd9f08
Vinay Kulkarni <kulkarniv@vmware.com>
2015-11-12 19:44:05 +0000

    Remove symlinks to build artifacts
    Change-Id: Ibac6f2a115028bcc63994e701f0a1986e9c1745a

--- support/cloud-image-builder/ami/mk-ami-image.sh
+++ support/cloud-image-builder/ami/mk-ami-image.sh
@@ -5,6 +5,5 @@ PHOTON_IMG_OUTPUT_PATH=$1
 cd $PHOTON_IMG_OUTPUT_PATH
 mv photon-ami.raw photon-ami-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.raw
 tar -Szcf photon-ami-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.tar.gz photon-ami-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.raw
-ln -s photon-ami-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.tar.gz photon-ami.tar.gz
 
 rm -f photon-ami-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.raw
