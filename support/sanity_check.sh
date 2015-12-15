83bae3fd527535277bd1008dcf64abd0542fd06a
Kiril Nesenko <knesenko@vmware.com>
2015-12-15 20:31:16 +0200

    build: improve sanity_check.sh
    - use full path instead of relative path
    - fail on script errors
    
    Signed-off-by: Kiril Nesenko <knesenko@vmware.com>

--- support/sanity_check.sh
+++ support/sanity_check.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+SCRIPTS_DIR="$(dirname ${0})"
+
 # Sanify check for all json files.
 echo "Sanity check for all json files..."
-for i in `find . -name "*.json" -type f -not -path "*stage*"`; do echo $i; json_pp -t null < $i || break -1 ; done
+while read f; do
+	echo "Checking:${f}"
+	if ! json_pp -t null < "${f}" >& /dev/null; then
+		echo "Please check:${f} for syntax errors"
+		exit 1
+	fi
+done < <(find "${SCRIPTS_DIR}/../" -name "*.json" -type f -not -path "*stage*")
 
 echo "Checking all python code is compilable..."
-for i in `find . -name "*.py" -type f -not -path "*stage*"`; do echo $i; python -m py_compile $i || break -1 ; done
+while read f; do
+	echo "Checking:${f}"
+	if ! python -m py_compile "${f}"; then
+		echo "Please check:${f} for complitation errors"
+		exit 1
+	fi
+done < <(find "${SCRIPTS_DIR}/../" -name "*.py" -type f -not -path "*stage*")
