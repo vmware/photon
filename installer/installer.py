b8529473a61be75a563673ac117f2a8965401dab
xiaolin-vmware <xiaolinl@vmware.com>
2016-04-06 16:09:48 -0700

    Reduce the size of initrd image to fit into 384M ram. (bug 1625460)
    Change-Id: Iada6179331c78546471db7e7af6d9dd170b1d411

--- installer/installer.py
+++ installer/installer.py
@@ -17,7 +17,6 @@ import signal
 import sys
 import glob
 import modules.commons
-import xml.etree.ElementTree as ET
 from jsonwrapper import JsonWrapper
 from progressbar import ProgressBar
 from window import Window
@@ -104,6 +103,13 @@ class Installer(object):
                 if retval != 0:
                     modules.commons.log(modules.commons.LOG_INFO, "Failed to reset repo")
                     self.exit_gracefully(None, None)
+
+            cmdoption = 's/cachedir=\/var/cachedir={}/g'.format(self.photon_root.replace('/','\/'))
+            process = subprocess.Popen(['sed', '-i', cmdoption,'/etc/tdnf/tdnf.conf']) 
+            retval = process.wait()
+            if retval != 0:
+                modules.commons.log(modules.commons.LOG_INFO, "Failed to reset tdnf cachedir")
+                self.exit_gracefully(None, None)
         self.execute_modules(modules.commons.PRE_INSTALL)
 
         self.initialize_system()
@@ -284,47 +290,17 @@ class Installer(object):
         initrd_dir = 'boot'
         initrd_file_name = 'initrd.img-no-kmods'
         if self.iso_installer:
-            # just copy the initramfs /boot -> /photon_mnt/boot
-            shutil.copy(os.path.join(initrd_dir, initrd_file_name), self.photon_root + '/boot/')
             # unmount the installer directory
             process = subprocess.Popen(['umount', os.path.join(self.photon_root, "installer")], stdout=self.output)
             retval = process.wait()
             # remove the installer directory
             process = subprocess.Popen(['rm', '-rf', os.path.join(self.photon_root, "installer")], stdout=self.output)
             retval = process.wait()
-        elif not self.install_config['vmdk_install']:
-            #Build the initramfs by passing in the kernel version
-            version_string = ''	
-            for root, dirs, files in os.walk(self.rpm_path):
-                for name in files:
-                    if fnmatch.fnmatch(name, 'linux-[0-9]*.rpm'):
-                        version_array = name.split('-')
-                        if len(version_array) > 2:
-                            version_string = version_array[1]
-
-            if 'initrd_dir' in self.install_config:
-                initrd_dir = self.install_config['initrd_dir']
-            self.add_dracut_configuration()
-            process = subprocess.Popen([self.chroot_command, '-w', self.photon_root, 'dracut', '--force', '--kver', version_string, os.path.join(initrd_dir,initrd_file_name)],  stdout=self.output, stderr=self.output)
+            # Disable the swap file
+            process = subprocess.Popen(['swapoff', '-a'], stdout=self.output)
             retval = process.wait()
-
-    def add_dracut_configuration(self):
-        if self.install_config.has_key("dracut_configuration"):
-            dracut_configuration = []
-            
-            for key in self.install_config["dracut_configuration"].keys():
-                keyValue = self.install_config["dracut_configuration"][key]
-                config=key
-                if isinstance(keyValue,list):
-                    config=config+'+="'+" ".join(keyValue)+'"'
-                else:
-                    config=config+'="'+keyValue+'"'
-                config=config+"\n"
-                dracut_configuration.append(config)
-            
-            f = open(self.photon_root+"/etc/dracut.conf","a")
-            f.writelines(dracut_configuration)
-        
+            # remove the tdnf cache directory and the swapfile.
+            process = subprocess.Popen(['rm', '-rf', os.path.join(self.photon_root, "cache")], stdout=self.output)
 
     def install_package(self,  package_name):
         rpm_params = ''
