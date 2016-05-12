56113bc3a033eecd61aa1f3bf2c1343c46c14584
suezzelur <anishs@vmware.com>
2016-05-11 17:11:36 -0700

    Add another ova for h/w version 10
    Change-Id: I60fc0baf7d6482c578d8ff78601b62d5af95b6a0
    Reviewed-on: http://photon-jenkins.eng.vmware.com/856
    Tested-by: jenkins-photon <wangnan2015@hotmail.com>
    Reviewed-by: Sharath George
    (cherry picked from commit 81de4219b38c1fa8ef0f70a05ebb7edc39397bcd)

--- support/cloud-image-builder/ova/mk-ova-image.sh
+++ support/cloud-image-builder/ova/mk-ova-image.sh
@@ -28,11 +28,20 @@ cd $PHOTON_IMG_OUTPUT_PATH/temp
 
 sed -i "s/otherGuest/other3xLinux64Guest/g" $PHOTON_IMG_OUTPUT_PATH/temp/photon-ova.ovf
 #Add product info
-sed -i '/\/VirtualSystem>/i \ \t<ProductSection> \n \t\t<Info>Information about the installed software</Info> \n \t\t<Product>Photon</Product> \n \t\t<Vendor>VMware Inc.</Vendor> \n \t\t<Version>1.0.0</Version> \n \t\t<FullVersion>1.0.0-TP2</FullVersion> \n \t</ProductSection> ' $PHOTON_IMG_OUTPUT_PATH/temp/photon-ova.ovf
+sed -i '/\/VirtualSystem>/i \ \t<ProductSection> \n \t\t<Info>Information about the installed software</Info> \n \t\t<Product>Photon</Product> \n \t\t<Vendor>VMware Inc.</Vendor> \n \t\t<Version>1.0</Version> \n \t\t<FullVersion>1.0</FullVersion> \n \t</ProductSection> ' $PHOTON_IMG_OUTPUT_PATH/temp/photon-ova.ovf
 rm -f $PHOTON_IMG_OUTPUT_PATH/temp/photon-ova.mf
 openssl sha1 *.vmdk photon-ova.ovf > photon-ova.mf
 tar cf photon-ova-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.ova photon-ova.ovf photon-ova.mf photon-ova-disk1.vmdk
 mv $PHOTON_IMG_OUTPUT_PATH/temp/photon-ova-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.ova $PHOTON_IMG_OUTPUT_PATH/
+
+#generate a second ova with lower hardware version
+cp photon-ova.ovf photon-ova-hw10.ovf
+sed -i 's/vmx-11/vmx-10/g' photon-ova-hw10.ovf
+rm -f $PHOTON_IMG_OUTPUT_PATH/temp/photon-ova.mf
+openssl sha1 *.vmdk photon-ova-hw10.ovf > photon-ova-hw10.mf
+tar cf photon-ova-hw10-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.ova photon-ova-hw10.ovf photon-ova-hw10.mf photon-ova-disk1.vmdk
+mv $PHOTON_IMG_OUTPUT_PATH/temp/photon-ova-hw10-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.ova $PHOTON_IMG_OUTPUT_PATH/
+
 cd $PHOTON_IMG_OUTPUT_PATH
 rm -rf photon-custom
 DISK_DEVICE=`losetup --show -f ${PHOTON_IMG_OUTPUT_PATH}/photon-ova.raw`
@@ -69,11 +78,20 @@ ovftool /tmp/vmx-temp-custom.vmx $PHOTON_IMG_OUTPUT_PATH/temp1/photon-custom.ovf
 cd $PHOTON_IMG_OUTPUT_PATH/temp1
 sed -i "s/otherGuest/other3xLinux64Guest/g" $PHOTON_IMG_OUTPUT_PATH/temp1/photon-custom.ovf
 #Add product info
-sed -i '/\/VirtualSystem>/i \ \t<ProductSection> \n \t\t<Info>Information about the installed software</Info> \n \t\t<Product>Photon</Product> \n \t\t<Vendor>VMware Inc.</Vendor> \n \t\t<Version>1.0.0</Version> \n \t\t<FullVersion>1.0.0-TP2</FullVersion> \n \t</ProductSection> ' $PHOTON_IMG_OUTPUT_PATH/temp1/photon-custom.ovf
+sed -i '/\/VirtualSystem>/i \ \t<ProductSection> \n \t\t<Info>Information about the installed software</Info> \n \t\t<Product>Photon</Product> \n \t\t<Vendor>VMware Inc.</Vendor> \n \t\t<Version>1.0</Version> \n \t\t<FullVersion>1.0</FullVersion> \n \t</ProductSection> ' $PHOTON_IMG_OUTPUT_PATH/temp1/photon-custom.ovf
 rm -f $PHOTON_IMG_OUTPUT_PATH/temp1/photon-custom.mf
 openssl sha1 *.vmdk photon-custom.ovf > photon-custom.mf
 tar cf photon-custom-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.ova photon-custom.ovf photon-custom.mf photon-custom-disk1.vmdk
 mv $PHOTON_IMG_OUTPUT_PATH/temp1/photon-custom-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.ova $PHOTON_IMG_OUTPUT_PATH/
+
+#generate a second custom ova with lower hardware version
+cp photon-custom.ovf photon-custom-hw10.ovf
+sed -i 's/vmx-11/vmx-10/g' photon-custom-hw10.ovf
+rm -f $PHOTON_IMG_OUTPUT_PATH/temp/photon-custom.mf
+openssl sha1 *.vmdk photon-custom-hw10.ovf > photon-custom-hw10.mf
+tar cf photon-custom-hw10-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.ova photon-custom-hw10.ovf photon-custom-hw10.mf photon-custom-disk1.vmdk
+mv $PHOTON_IMG_OUTPUT_PATH/temp1/photon-custom-hw10-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.ova $PHOTON_IMG_OUTPUT_PATH/
+
 cd $PHOTON_IMG_OUTPUT_PATH
 rm -rf $PHOTON_IMG_OUTPUT_PATH/temp/
 rm -rf $PHOTON_IMG_OUTPUT_PATH/temp1/
