31461c958c81f211f4ecc7595f476fa5bc166662
suezzelur <anishs@vmware.com>
2016-11-11 12:47:36 -0800

    Remove unneeded files, reduce the size of ami raw image
    Change-Id: I4b75e5b623a10927af82a424eb64ac4100288b91

--- support/cloud-image-builder/ami/mk-ami-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PHOTON_IMG_OUTPUT_PATH=$1
-
-cd $PHOTON_IMG_OUTPUT_PATH
-mv photon-ami.raw photon-ami-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.raw
-tar -Szcf photon-ami-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.tar.gz photon-ami-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.raw
-
-rm -f photon-ami-$PHOTON_RELEASE_VER-$PHOTON_BUILD_NUM.raw
