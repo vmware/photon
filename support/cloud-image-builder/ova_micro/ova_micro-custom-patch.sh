986d434ffbdc5841365a433ce10e7aadd8aa69b5
Alexey Makhalov <amakhalov@vmware.com>
2016-11-16 10:50:20 -0800

    ova_micro: shrink disk size to 152MB
    Change-Id: I11c060068b8f9251572983ad7cf6c05ada6291ad

--- support/cloud-image-builder/ova_micro/ova_micro-custom-patch.sh
+++ support/cloud-image-builder/ova_micro/ova_micro-custom-patch.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
 echo -e "changeme\nchangeme" | passwd root
-chage -d 0 root
+
+# create empty cracklib dictionary and remove cracklib-dicts rpm
+echo a | cracklib-packer /root/pw_dict
+rpm -e cracklib-dicts --nodeps
+mv /root/pw_dict* /usr/share/cracklib/
+
+# removing this file is safe
+rm /boot/system.map*
+
+# TODO: remove it from grub rpm - not used
+rm -rf /boot/grub/fonts
+# TODO: clean up spec file
+rm -rf /usr/include
