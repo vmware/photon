681ff2b05559a86c0db67cb6c9f081b794e03c6c
dthaluru <dthaluru@vmware.com>
2016-12-08 16:42:50 -0800

    Added logic to build package if sub-packages are not available
    Change-Id: I5378e67c650d5fb5bd5d61f3806ed3206501c9a6

--- support/package-builder/PackageManager.py
+++ support/package-builder/PackageManager.py
@@ -9,7 +9,7 @@ from Scheduler import Scheduler
 from ThreadPool import ThreadPool
 
 class PackageManager(object):
-    
+
     def __init__(self,logName=None,logPath=None):
         if logName is None:
             logName = "PackageManager"
@@ -27,8 +27,8 @@ class PackageManager(object):
         self.mapThreadsLaunchTime={}
         self.listAvailableCyclicPackages=[]
         self.listBuildOptionPackages=[]
-        self.pkgBuildOptionFile="" 
-        
+        self.pkgBuildOptionFile=""
+
     def readPackageBuildData(self, listPackages):
         try:
             pkgBuildDataGen = PackageBuildDataGenerator(self.logName,self.logPath)
@@ -37,15 +37,16 @@ class PackageManager(object):
             self.logger.error("unable to get sorted list")
             return False
         return True
-    
+
     def readAlreadyAvailablePackages(self):
         listAvailablePackages=[]
+        listFoundRPMPackages=[]
         listRPMFiles=[]
         listDirectorys=[]
         listDirectorys.append(constants.rpmPath)
         if constants.inputRPMSPath is not None:
             listDirectorys.append(constants.inputRPMSPath)
-        
+
         while len(listDirectorys) > 0:
             dirPath=listDirectorys.pop()
             for dirEntry in os.listdir(dirPath):
@@ -61,7 +62,19 @@ class PackageManager(object):
                 specVersion=constants.specData.getVersion(package)
                 specRelease=constants.specData.getRelease(package)
                 if version == specVersion and release == specRelease:
-                    listAvailablePackages.append(package)
+                    listFoundRPMPackages.append(package)
+        #Mark package available only if all sub packages are available
+        for package in listFoundRPMPackages:
+            basePkg = constants.specData.getSpecName(package)
+            if basePkg in listAvailablePackages:
+                continue;
+            listRPMPackages = constants.specData.getRPMPackages(basePkg)
+            packageIsAlreadyBuilt = True
+            for rpmpkg in listRPMPackages:
+                if rpmpkg not in listFoundRPMPackages:
+                    packageIsAlreadyBuilt = False
+            if packageIsAlreadyBuilt:
+                listAvailablePackages.append(package)
         self.logger.info("List of Already built packages")
         self.logger.info(listAvailablePackages)
         return listAvailablePackages
@@ -96,11 +109,11 @@ class PackageManager(object):
         for pkg in listPackages:
             if pkg in self.listOfPackagesAlreadyBuilt:
                 listPackagesToBuild.remove(pkg)
-        
+
         if not self.readPackageBuildData(listPackagesToBuild):
             return False
         return True
-    
+
     def buildToolChain(self):
         try:
             tUtils=ToolChainUtils()
@@ -109,17 +122,17 @@ class PackageManager(object):
             self.logger.error("Unable to build tool chain")
             self.logger.error(e)
             raise e
-    
+
     def buildToolChainPackages(self, listBuildOptionPackages, pkgBuildOptionFile, buildThreads):
         self.buildToolChain()
         self.buildGivenPackages(constants.listToolChainPackages, buildThreads)
-        
+
     def buildPackages(self,listPackages, listBuildOptionPackages, pkgBuildOptionFile, buildThreads):
         self.listBuildOptionPackages = listBuildOptionPackages
         self.pkgBuildOptionFile = pkgBuildOptionFile
         self.buildToolChainPackages(listBuildOptionPackages, pkgBuildOptionFile, buildThreads)
         self.buildGivenPackages(listPackages, buildThreads)
-    
+
     def initializeThreadPool(self,statusEvent):
         ThreadPool.clear()
         ThreadPool.mapPackageToCycle=self.mapPackageToCycle
@@ -128,57 +141,57 @@ class PackageManager(object):
         ThreadPool.pkgBuildOptionFile=self.pkgBuildOptionFile
         ThreadPool.logger=self.logger
         ThreadPool.statusEvent=statusEvent
-        
+
     def initializeScheduler(self,statusEvent):
         Scheduler.setLog(self.logName, self.logPath)
         Scheduler.setParams(self.sortedPackageList, self.listOfPackagesAlreadyBuilt)
         Scheduler.setEvent(statusEvent)
         Scheduler.stopScheduling=False
-    
+
     def buildGivenPackages (self, listPackages, buildThreads):
         returnVal=self.calculateParams(listPackages)
         if not returnVal:
             self.logger.error("Unable to set paramaters. Terminating the package manager.")
             raise Exception("Unable to set paramaters")
-        
+
         statusEvent=threading.Event()
         self.initializeScheduler(statusEvent)
         self.initializeThreadPool(statusEvent)
-        
+
         i=0
         while i < buildThreads:
             workerName="WorkerThread"+str(i)
             ThreadPool.addWorkerThread(workerName)
             ThreadPool.startWorkerThread(workerName)
             i = i + 1
-        
+
         statusEvent.wait()
         Scheduler.stopScheduling=True
         self.logger.info("Waiting for all remaining worker threads")
         listWorkerObjs=ThreadPool.getAllWorkerObjects()
         for w in listWorkerObjs:
             w.join()
-            
+
         setFailFlag=False
         allPackagesBuilt=False
-        
+
         if Scheduler.isAnyPackagesFailedToBuild():
             setFailFlag=True
-        
+
         if Scheduler.isAllPackagesBuilt():
             allPackagesBuilt=True
-        
+
         if setFailFlag:
             self.logger.error("Some of the packages failed:")
             self.logger.error(Scheduler.listOfFailedPackages)
             raise Exception("Failed during building package")
-        
+
         if not setFailFlag:
             if allPackagesBuilt:
                 self.logger.info("All packages built successfully")
             else:
                 self.logger.error("Build stopped unexpectedly.Unknown error.")
                 raise Exception("Unknown error")
-        
+
         self.logger.info("Terminated")
 
