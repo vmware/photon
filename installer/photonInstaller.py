ad8c743769e4f9bbeaacd19e45c6fb8cb44fe27d
xiaolin-vmware <xiaolinl@vmware.com>
2016-12-13 10:14:49 -0800

    Use pkg_info.json to find rpm files of a package.
    Change-Id: I5e935dc97fc05a2858b46c1c03c2e6634aab89fe

--- installer/photonInstaller.py
+++ installer/photonInstaller.py
@@ -87,7 +87,13 @@ def create_vmdk_and_partition(config, vmdk_path):
 
     return partitions_data, count == 2
 
-def create_rpm_list_to_copy_in_iso(build_install_option, output_data_path):
+def get_file_name_with_last_folder(filename):
+    basename = os.path.basename(filename)
+    dirname = os.path.dirname(filename)
+    lastfolder = os.path.basename(dirname)
+    name = os.path.join(lastfolder, basename)
+    return name
+def create_pkg_list_to_copy_to_iso(build_install_option, output_data_path):
     json_wrapper_option_list = JsonWrapper(build_install_option)
     option_list_json = json_wrapper_option_list.read()
     options_sorted = option_list_json.items()
@@ -99,6 +105,31 @@ def create_rpm_list_to_copy_in_iso(build_install_option, output_data_path):
             package_list_json = json_wrapper_package_list.read()
             packages = packages + package_list_json["packages"]
     return packages
+
+    #copy_flags 1: add the rpm file for the package
+    #           2: add debuginfo rpm file for the package.
+    #           4: add src rpm file for the package
+def create_rpm_list_to_be_copied_to_iso(pkg_to_rpm_map_file, build_install_option, copy_flags, output_data_path):
+    packages = []
+    if build_install_option is None:
+        packages = []
+    else:
+        packages = create_pkg_list_to_copy_to_iso(build_install_option, output_data_path)
+
+    rpm_list = []
+    json_pkg_to_rpm_map = JsonWrapper(pkg_to_rpm_map_file)
+    pkg_to_rpm_map = json_pkg_to_rpm_map.read()
+    for k in pkg_to_rpm_map:
+        if build_install_option is None or k in packages:
+            if not pkg_to_rpm_map[k]['rpm'] is None and bool(copy_flags & 1):
+                filename = pkg_to_rpm_map[k]['rpm']
+                rpm_list.append(get_file_name_with_last_folder(filename))
+            if not pkg_to_rpm_map[k]['debugrpm'] is None and bool(copy_flags & 2):
+                filename = pkg_to_rpm_map[k]['debugrpm']
+                rpm_list.append(get_file_name_with_last_folder(filename))
+            if not pkg_to_rpm_map[k]['sourcerpm'] is None and bool(copy_flags & 4):
+                rpm_list.append(pkg_to_rpm_map[k]['sourcerpm'])
+    return rpm_list
 
 def create_additional_file_list_to_copy_in_iso(base_path, build_install_option):
     json_wrapper_option_list = JsonWrapper(build_install_option)
@@ -121,6 +152,22 @@ def get_live_cd_status_string(build_install_option):
                 return "true"
     return "false"
 
+def make_src_iso(working_directory, src_iso_path, rpm_list):
+    if os.path.isdir(working_directory):
+        process = subprocess.Popen(['rm', '-rf', working_directory])
+        retval = process.wait()
+    process = subprocess.Popen(['mkdir', '-p', os.path.join(working_directory, "SRPMS")])
+    retval = process.wait()
+    for aaa in rpm_list:
+        if os.path.isfile(aaa):
+            process = subprocess.Popen(['cp', aaa, os.path.join(working_directory, "SRPMS")])
+            retval = process.wait()
+    process = subprocess.Popen(['mkisofs', '-r', '-o', src_iso_path, working_directory])
+    retval = process.wait()
+    process = subprocess.Popen(['rm', '-rf', options.working_directory])
+    retval = process.wait()
+
+
 if __name__ == '__main__':
     usage = "Usage: %prog [options] <config file> <tools path>"
     parser = OptionParser(usage)
@@ -138,120 +185,108 @@ if __name__ == '__main__':
     parser.add_option("-m", "--stage-path", dest="stage_path", default="../stage")
     parser.add_option("-c", "--dracut-configuration", dest="dracut_configuration_file", default="../common/data/dracut_configuration.json")
     parser.add_option("-s", "--json-data-path", dest="json_data_path", default="../stage/common/data/")
+    parser.add_option("-d", "--pkg-to_rpm-map-file", dest="pkg_to_rpm_map_file", default="../stage/pkg_info.json")
 
     (options,  args) = parser.parse_args()
-    if options.iso_path or options.src_iso_path:
-        # Check the arguments
-        if (len(args)) != 0:
-            parser.error("Incorrect arguments")
-        config = {}
-        config['iso_system'] = True
-        config['vmdk_install'] = False
-
-    elif options.vmdk_path:
-        # Check the arguments
-        if (len(args)) != 1:
-            parser.error("Incorrect arguments")
-
-        # Read the conf file
-        config = (JsonWrapper(args[0])).read()
-        config['disk'], success = create_vmdk_and_partition(config, options.vmdk_path)
-        if not success:
-            print "Unexpected failure, please check the logs"
-            sys.exit(1)
-
-        config['initrd_dir'] = "/boot"
-
-        config['iso_system'] = False
-        config['vmdk_install'] = True
-
-    else:
-        # Check the arguments
-        if (len(args)) != 1:
-            parser.error("Incorrect arguments")
-
-        # Read the conf file
-        config = (JsonWrapper(args[0])).read()
-
-        config['iso_system'] = False
-        config['vmdk_install'] = False
-
-    if 'password' in config:
-        # crypt the password if needed
-        if config['password']['crypted']:
-            config['password'] = config['password']['text']
-        else:
-            config['password'] = crypt.crypt(config['password']['text'], "$6$" + "".join([random.choice(string.ascii_letters + string.digits) for _ in range(16)]))
-
-    # Check the installation type
-    json_wrapper_option_list = JsonWrapper(options.package_list_file)
-    option_list_json = json_wrapper_option_list.read()
-    options_sorted = option_list_json.items()
-    base_path = os.path.dirname(options.package_list_file)
-
-    packages = []
-    additional_files_to_copy_from_stage_to_iso = []
-    if config['iso_system'] == True:
-        for install_option in options_sorted:
-            if install_option[0] == "iso":
-                json_wrapper_package_list = JsonWrapper(os.path.join(base_path, install_option[1]["file"]))
-                package_list_json = json_wrapper_package_list.read()
-                packages = package_list_json["packages"]
-    else:
-        packages = PackageSelector.get_packages_to_install(options_sorted, config['type'], options.output_data_path)
-
-    config['packages'] = packages
-
-    if options.iso_path:
-        if os.path.isfile(options.dracut_configuration_file):
-            json_wrapper_package_list = JsonWrapper(options.dracut_configuration_file)
-            dracut_configuration_list_json = json_wrapper_package_list.read()
-            config["dracut_configuration"]=dracut_configuration_list_json["dracut_configuration"]
-
     # Cleanup the working directory
     if not options.force:
         proceed = query_yes_no("This will remove everything under {0}. Are you sure?".format(options.working_directory))
         if not proceed:
             sys.exit(0)
 
-    if (os.path.isdir(options.working_directory)):
-        process = subprocess.Popen(['rm', '-rf', options.working_directory])
-        retval = process.wait()
-    else:
-        process = subprocess.Popen(['mkdir', '-p', options.working_directory])
-        retval = process.wait()
-
-    process = subprocess.Popen(['mkdir', '-p', os.path.join(options.working_directory, "photon-chroot")])
-    retval = process.wait()
-
-    config['working_directory'] = options.working_directory
-
-    # Run the installer
-    package_installer = Installer(config, rpm_path = options.rpm_path, log_path = options.log_path)
-    package_installer.install(None)
-
-    # Making the iso if needed
-    if options.iso_path:
-        rpm_list = " ".join(create_rpm_list_to_copy_in_iso(options.package_list_file, options.output_data_path))
-        files_to_copy = " ".join(create_additional_file_list_to_copy_in_iso(os.path.abspath(options.stage_path), options.package_list_file))
-        live_cd = get_live_cd_status_string(options.package_list_file)
-        process = subprocess.Popen(['./mk-install-iso.sh', '-w', options.working_directory, options.iso_path, options.rpm_path, options.package_list_file, rpm_list, options.stage_path, files_to_copy, live_cd, options.json_data_path])
-        retval = process.wait()
-
     if options.src_iso_path:
-        rpm_list = " ".join(create_rpm_list_to_copy_in_iso(options.package_list_file, options.output_data_path))
-        process = subprocess.Popen(['./mk-src-iso.sh', '-w', options.working_directory, options.src_iso_path, options.srpm_path, rpm_list])
-        retval = process.wait()
-        list_rpms = rpm_list.split(" ")
-        list_rpms = list(set(list_rpms))
-        list_rpms.sort()
+        rpm_list = create_rpm_list_to_be_copied_to_iso(options.pkg_to_rpm_map_file, options.package_list_file, 4, options.output_data_path)
+        make_src_iso(options.working_directory, options.src_iso_path, rpm_list)
 
-    # Cleaning up for vmdk
-    if 'vmdk_install' in config and config['vmdk_install']:
-        process = subprocess.Popen(['./mk-clean-vmdk.sh', config['disk']['disk']])
-        process.wait()
+    else:
+        if options.iso_path:
+            # Check the arguments
+            if (len(args)) != 0:
+                parser.error("Incorrect arguments")
+            config = {}
+            config['iso_system'] = True
+            config['vmdk_install'] = False
+            config['type'] = 'iso'
 
-    #Clean up the working directories
-    if (options.iso_path or options.vmdk_path or options.src_iso_path):
-        process = subprocess.Popen(['rm', '-rf', options.working_directory])
+        elif options.vmdk_path:
+            # Check the arguments
+            if (len(args)) != 1:
+                parser.error("Incorrect arguments")
+
+            # Read the conf file
+            config = (JsonWrapper(args[0])).read()
+            config['disk'], success = create_vmdk_and_partition(config, options.vmdk_path)
+            if not success:
+                print "Unexpected failure, please check the logs"
+                sys.exit(1)
+
+            config['initrd_dir'] = "/boot"
+
+            config['iso_system'] = False
+            config['vmdk_install'] = True
+
+        else:
+            # Check the arguments
+            if (len(args)) != 1:
+                parser.error("Incorrect arguments")
+
+            # Read the conf file
+            config = (JsonWrapper(args[0])).read()
+
+            config['iso_system'] = False
+            config['vmdk_install'] = False
+
+        if 'password' in config:
+            # crypt the password if needed
+            if config['password']['crypted']:
+                config['password'] = config['password']['text']
+            else:
+                config['password'] = crypt.crypt(config['password']['text'], "$6$" + "".join([random.choice(string.ascii_letters + string.digits) for _ in range(16)]))
+
+        # Check the installation type
+        json_wrapper_option_list = JsonWrapper(options.package_list_file)
+        option_list_json = json_wrapper_option_list.read()
+        options_sorted = option_list_json.items()
+
+        packages = []
+        packages = PackageSelector.get_packages_to_install(options_sorted, config['type'], options.output_data_path)
+
+        config['packages'] = packages
+
+        if options.iso_path:
+            if os.path.isfile(options.dracut_configuration_file):
+                json_wrapper_package_list = JsonWrapper(options.dracut_configuration_file)
+                dracut_configuration_list_json = json_wrapper_package_list.read()
+                config["dracut_configuration"]=dracut_configuration_list_json["dracut_configuration"]
+
+
+        if (os.path.isdir(options.working_directory)):
+            process = subprocess.Popen(['rm', '-rf', options.working_directory])
+            retval = process.wait()
+
+        process = subprocess.Popen(['mkdir', '-p', os.path.join(options.working_directory, "photon-chroot")])
         retval = process.wait()
+
+        config['working_directory'] = options.working_directory
+
+        # Run the installer
+        package_installer = Installer(config, rpm_path = options.rpm_path, log_path = options.log_path)
+        package_installer.install(None)
+
+        # Making the iso if needed
+        if options.iso_path:
+            rpm_list = " ".join(create_rpm_list_to_be_copied_to_iso(options.pkg_to_rpm_map_file, options.package_list_file, 3, options.output_data_path))
+            files_to_copy = " ".join(create_additional_file_list_to_copy_in_iso(os.path.abspath(options.stage_path), options.package_list_file))
+            live_cd = get_live_cd_status_string(options.package_list_file)
+            process = subprocess.Popen(['./mk-install-iso.sh', '-w', options.working_directory, options.iso_path, options.rpm_path, options.package_list_file, rpm_list, options.stage_path, files_to_copy, live_cd, options.json_data_path])
+            retval = process.wait()
+
+        # Cleaning up for vmdk
+        if 'vmdk_install' in config and config['vmdk_install']:
+            process = subprocess.Popen(['./mk-clean-vmdk.sh', config['disk']['disk']])
+            process.wait()
+
+        #Clean up the working directories
+        if (options.iso_path or options.vmdk_path):
+            process = subprocess.Popen(['rm', '-rf', options.working_directory])
+            retval = process.wait()
