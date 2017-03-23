3dbf8fa9095ed8e740355def9110b3f59c659ce7
xiaolin-vmware <xiaolinl@vmware.com>
2017-03-22 23:07:51 -0700

    Add a screen to select between linux and linux-esx when vmware virtualization is detected.
    Change-Id: I6b46b3edbf6356e11963c56a7c800da87dd58881

---
+++ installer/linuxselector.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python2
+#
+#    Copyright (C) 2017 vmware inc.
+#
+#    Author: Xiaolin Li <xiaolinl@vmware.com>
+
+import json
+import os
+import curses
+from sets import Set
+from jsonwrapper import JsonWrapper
+from menu import Menu
+from window import Window
+from actionresult import ActionResult
+
+class LinuxSelector(object):
+    def __init__(self,  maxy, maxx, install_config):
+        self.install_config = install_config
+        self.maxx = maxx
+        self.maxy = maxy
+        self.win_width = 50
+        self.win_height = 13
+
+        self.win_starty = (self.maxy - self.win_height) / 2
+        self.win_startx = (self.maxx - self.win_width) / 2
+
+        self.menu_starty = self.win_starty + 3
+
+        self.menu_items = []
+        self.menu_items.append(("1. linux", self.set_linux_esx_installation, False))
+        self.menu_items.append(("2. linux-esx", self.set_linux_esx_installation, True))
+
+        self.host_menu = Menu(self.menu_starty, self.maxx, self.menu_items,
+                              default_selected = 0, tab_enable=False)
+
+        self.window = Window(self.win_height, self.win_width, self.maxy, self.maxx,
+                             'Select Linux', True, items=[], tab_enabled = False,
+                             position = 1, can_go_next=True)
+        self.window.set_action_panel(self.host_menu)
+
+    def set_linux_esx_installation(self, is_linux_esx):
+        self.install_config['install_linux_esx'] = is_linux_esx
+        return ActionResult(True, None)
+
+    def display(self, params):
+        return self.window.do_action()
+
