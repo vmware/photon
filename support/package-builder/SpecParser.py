0657ef96fb545372fb1fa29388a41b36ffa5e342
harishspqr <harishspqr@gmail.com>
2017-05-09 17:20:37 -0700

    Add new package cassandra
    Change-Id: Id368016b1a81274e8bdcfaec1a2057d98f11b622

--- support/package-builder/SpecParser.py
+++ support/package-builder/SpecParser.py
@@ -366,6 +366,7 @@ class SpecParser(object):
             print "Error: Can not find match for sha1 "+value[0]
             return False
         if (len(matchedSources) > 1):
+            print "Matched Sources:" + ' '.join(matchedSources)
             print "Error: Too many matches in sources: "+matchedSources+" for sha1 "+value[0]
             return False
         pkg.checksums[sourceName] = value[1]
