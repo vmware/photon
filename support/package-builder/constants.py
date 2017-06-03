2b3425cbaba81bc0a3f5f33c029b9b2c4f5bc099
xiaolin-vmware <xiaolinl@vmware.com>
2017-06-02 21:20:36 -0700

    Added expat, libffi, python2, python2-libs, python2-devel, openssl, openssl-devel to listToolChainRPMPkgsToInstall.
    Change-Id: Ibcfa5807fb672c19b2ccef92fd85ef022ffebf8a
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/2863
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Alexey Makhalov <amakhalov@vmware.com>

--- support/package-builder/constants.py
+++ support/package-builder/constants.py
@@ -105,8 +105,8 @@ class constants(object):
     listToolChainXRPMsToInstall=[
         "glib-devel",
         "icu-devel",
-	"openjdk",
-	"openjre",
+        "openjdk",
+        "openjre",
         "icu",
         "harfbuzz",
         "harfbuzz-devel",
@@ -140,8 +140,7 @@ class constants(object):
         "libXt-devel",
         "libXmu-devel",
         "libXrender",
-        "libXrender-devel"
-	]
+        "libXrender-devel"]
 
     listToolChainRPMPkgsToInstall=[
         "linux-api-headers",
@@ -210,7 +209,14 @@ class constants(object):
         "man-pages",
         "elfutils",
         "cpio",
-		"go"]
+        "go",
+        "expat",
+        "libffi",
+        "openssl",
+        "openssl-devel",
+        "python2",
+        "python2-libs",
+        "python2-devel"]
 
     listToolChainRPMPkgsToBuild=[
         "linux-api-headers",
