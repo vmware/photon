245daa5691a10204e0e7b630872c0a1ba3ba8f77
xiaolin-vmware <xiaolinl@vmware.com>
2017-06-08 14:37:38 -0700

    Changed message of linuxselector page of installer. (Bug 1865043)
    Change-Id: I7a914acd71cb16174a858898468c054b0acec153

--- installer/linuxselector.py
+++ installer/linuxselector.py
@@ -1,41 +1,35 @@
-#!/usr/bin/python2
 #
 #    Copyright (C) 2017 vmware inc.
 #
 #    Author: Xiaolin Li <xiaolinl@vmware.com>
 
-import json
-import os
-import curses
-from sets import Set
-from jsonwrapper import JsonWrapper
 from menu import Menu
 from window import Window
 from actionresult import ActionResult
 
 class LinuxSelector(object):
-    def __init__(self,  maxy, maxx, install_config):
+    def __init__(self, maxy, maxx, install_config):
         self.install_config = install_config
         self.maxx = maxx
         self.maxy = maxy
-        self.win_width = 50
+        self.win_width = 60
         self.win_height = 13
 
         self.win_starty = (self.maxy - self.win_height) / 2
         self.win_startx = (self.maxx - self.win_width) / 2
 
-        self.menu_starty = self.win_starty + 3
+        self.menu_starty = self.win_starty + 6
 
         self.menu_items = []
-        self.menu_items.append(("1. linux-esx", self.set_linux_esx_installation, True))
-        self.menu_items.append(("2. linux", self.set_linux_esx_installation, False))
+        self.menu_items.append(("1. Hypervisor optimized", self.set_linux_esx_installation, True))
+        self.menu_items.append(("2. Generic", self.set_linux_esx_installation, False))
 
         self.host_menu = Menu(self.menu_starty, self.maxx, self.menu_items,
-                              default_selected = 0, tab_enable=False)
+                              default_selected=0, tab_enable=False)
 
         self.window = Window(self.win_height, self.win_width, self.maxy, self.maxx,
-                             'Select Linux', True, items=[], tab_enabled = False,
-                             position = 1, can_go_next=True)
+                             'Select Linux kernel to install', True, items=[], tab_enabled=False,
+                             position=1, can_go_next=True)
         self.window.set_action_panel(self.host_menu)
 
     def set_linux_esx_installation(self, is_linux_esx):
@@ -43,5 +37,8 @@ class LinuxSelector(object):
         return ActionResult(True, None)
 
     def display(self, params):
+        self.window.addstr(0, 0, 'The installer has detected that you are installing')
+        self.window.addstr(1, 0, 'Photon OS on a VMware hypervisor.')
+        self.window.addstr(2, 0, 'Which type of Linux kernel would you like to install?')
         return self.window.do_action()
 
