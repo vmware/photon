f939e4e9fada7a3b6762f53af7a78b897def01a5
dthaluru <dthaluru@vmware.com>
2017-06-14 17:42:10 -0700

    Increased boot partition size
    Change-Id: I3abc2919570b0cf1bf2fecd43c11a87f2b26cf9c

--- support/cloud-image-builder/mk-setup-vmdk.sh
+++ support/cloud-image-builder/mk-setup-vmdk.sh
@@ -81,9 +81,9 @@ DISK_DEVICE=`losetup --show -f $VMDK_IMAGE_NAME`
 echo "Creating partition on raw disk"
 if [ $SWAP_PARTITION_SIZE -gt 0 ] 
 then
-      sgdisk -n 1::+2M -n 2::+${ROOT_PARTITION_SIZE}G -n 3: -p $DISK_DEVICE >> $LOGFILE
+      sgdisk -n 1::+3M -n 2::+${ROOT_PARTITION_SIZE}G -n 3: -p $DISK_DEVICE >> $LOGFILE
 else
-      sgdisk -n 1::+2M -n 2: -p $DISK_DEVICE >> $LOGFILE
+      sgdisk -n 1::+3M -n 2: -p $DISK_DEVICE >> $LOGFILE
 fi
 
 sgdisk -t1:ef02 $DISK_DEVICE >> $LOGFILE
