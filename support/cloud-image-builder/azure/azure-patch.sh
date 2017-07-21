016865027f71c24585823607811c58f772f99a6e
suezzelur <anishs@vmware.com>
2017-07-21 16:00:57 -0700

    Update WALinuxAgent to 2.2.14
    Change-Id: I5787bcc19c53c63aaf7ad403c03decbffa47c60f

--- support/cloud-image-builder/azure/azure-patch.sh
+++ support/cloud-image-builder/azure/azure-patch.sh
@@ -48,11 +48,8 @@ echo "Ciphers aes128-ctr,aes192-ctr,aes256-ctr,arcfour256,arcfour128,aes128-cbc,
 echo "Tunnel no" >> /etc/ssh/ssh_config
 echo "ServerAliveInterval 180" >> /etc/ssh/ssh_config
 
-sed -i 's/net.ifnames=0//' /boot/grub/grub.cfg
-sed -i 's/$photon_cmdline/init=\/lib\/systemd\/systemd loglevel=3 ro console=ttyS0 earlyprintk=ttyS0 rootdelay=30/' /boot/grub/grub.cfg
+sed -i 's/$photon_cmdline $systemd_cmdline/init=\/lib\/systemd\/systemd loglevel=3 ro console=tty1 console=ttyS0,115200n8 earlyprintk=ttyS0,115200 rootdelay=300/' /boot/grub/grub.cfg
 
-# Disable loading/unloading of modules
-echo 1 > /proc/sys/kernel/modules_disabled
 
 # Remove kernel symbols
 rm /boot/system.map*
