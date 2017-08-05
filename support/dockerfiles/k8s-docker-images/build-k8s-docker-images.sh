1746bf69b46092013ea5441e7d71bab58a815e0b
Vinay Kulkarni <kulkarniv@vmware.com>
2017-08-05 03:20:00 +0000

    k8s pause container based on PhotonOS
    Change-Id: I2f1d867571c3642e8626509dbad321802d32f819
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/3425
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Bo Gan <ganb@vmware.com>

--- support/dockerfiles/k8s-docker-images/build-k8s-docker-images.sh
+++ support/dockerfiles/k8s-docker-images/build-k8s-docker-images.sh
@@ -13,6 +13,8 @@ K8S_VER=`cat ${SPEC_DIR}/kubernetes/kubernetes.spec | grep Version | cut -d: -f2
 K8S_VER_REL=${K8S_VER}-`cat ${SPEC_DIR}/kubernetes/kubernetes.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
 K8S_RPM=kubernetes-${K8S_VER_REL}${DIST_TAG}.${ARCH}.rpm
 K8S_RPM_FILE=../../../stage/RPMS/x86_64/${K8S_RPM}
+K8S_PAUSE_RPM=kubernetes-pause-${K8S_VER_REL}${DIST_TAG}.${ARCH}.rpm
+K8S_PAUSE_RPM_FILE=../../../stage/RPMS/x86_64/${K8S_PAUSE_RPM}
 
 if [ ! -f ${K8S_RPM_FILE} ]
 then
@@ -32,8 +34,10 @@ done
 
 mkdir -p tmp/k8s
 cp ${K8S_RPM_FILE} tmp/k8s/
+cp ${K8S_PAUSE_RPM_FILE} tmp/k8s/
 pushd ./tmp/k8s
 rpm2cpio ${K8S_RPM} | cpio -vid
+rpm2cpio ${K8S_PAUSE_RPM} | cpio -vid
 popd
 
 for K8S_BIN in ${K8S_BINS[*]}; do
@@ -43,5 +47,22 @@ for K8S_BIN in ${K8S_BINS[*]}; do
     docker save -o ${K8S_TAR_NAME} ${IMG_NAME}
     mv -f ${K8S_TAR_NAME} ${STAGE_DIR}/
 done
+
+
+#
+# K8S Pause container
+#
+PAUSE_IMG_NAME=vmware_photon2/pause-amd64:2.0
+PAUSE_TAR_NAME=k8s-pause.tar
+
+PAUSE_IMG_ID=`docker images -q ${PAUSE_IMG_NAME} 2> /dev/null`
+if [[ ! -z "${PAUSE_IMG_ID}" ]]; then
+    echo "Removing image ${PAUSE_IMG_NAME}"
+    docker rmi -f ${PAUSE_IMG_NAME}
+fi
+
+docker build --rm -t ${PAUSE_IMG_NAME} -f ./Dockerfile.pause .
+docker save -o ${PAUSE_TAR_NAME} ${PAUSE_IMG_NAME}
+mv -f ${PAUSE_TAR_NAME} ${STAGE_DIR}/
 
 rm -rf ./tmp
