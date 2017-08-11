1384354065d7a61c9447be19e26d6dd9f8dcec30
Vinay Kulkarni <kulkarniv@vmware.com>
2017-08-11 17:30:17 +0000

    flannel kubernetes pod and systemd service file

---
+++ support/dockerfiles/k8s-docker-images/build-flannel-docker-image.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+DIST_TAG=$1
+DIST_VER=$2
+SPEC_DIR=$3
+STAGE_DIR=$4
+ARCH=x86_64
+
+#
+# Docker image for flannel
+#
+FLANNEL_VER=`cat ${SPEC_DIR}/flannel/flannel.spec | grep Version | cut -d: -f2 | tr -d ' '`
+FLANNEL_VER_REL=${FLANNEL_VER}-`cat ${SPEC_DIR}/flannel/flannel.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
+FLANNEL_RPM=flannel-${FLANNEL_VER_REL}${DIST_TAG}.${ARCH}.rpm
+FLANNEL_RPM_FILE=${STAGE_DIR}/RPMS/x86_64/${FLANNEL_RPM}
+FLANNEL_TAR=flannel.tar
+
+if [ ! -f ${FLANNEL_RPM_FILE} ]
+then
+    echo "flannel RPM ${FLANNEL_RPM_FILE} not found. Exiting.."
+    exit 1
+fi
+
+IMG_NAME=vmware_photon/flannel:v${FLANNEL_VER}
+
+IMG_ID=`docker images -q ${IMG_NAME} 2> /dev/null`
+if [[ ! -z "${IMG_ID}" ]]; then
+    echo "Removing image ${IMG_NAME}"
+    docker rmi -f ${IMG_NAME}
+fi
+
+mkdir -p tmp/flannel
+cp ${FLANNEL_RPM_FILE} tmp/flannel/
+pushd ./tmp/flannel
+rpm2cpio ${FLANNEL_RPM} | cpio -vid
+popd
+docker build --rm -t ${IMG_NAME} -f Dockerfile.flannel .
+docker save -o ${FLANNEL_TAR} ${IMG_NAME}
+mv -f ${FLANNEL_TAR} ${STAGE_DIR}/
+
+rm -rf ./tmp
