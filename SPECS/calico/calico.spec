ac3712a62c9f56a2256348fd116d304d2feec0d5
Vinay Kulkarni <kulkarniv@vmware.com>
2017-08-23 22:35:47 +0000

    calico-node, calico-cni, flannel containers for kubernetes based on PhotonOS.

---
+++ SPECS/calico/calico.spec
@@ -0,0 +1,55 @@
+Summary:        Calico node and documentation for project calico.
+Name:           calico
+Version:        2.4.1
+Release:        1%{?dist}
+License:        Apache-2.0
+URL:            https://github.com/projectcalico/calico
+Source0:        %{name}-%{version}.tar.gz
+%define sha1 calico=2d26dbc187819231ec0d0e0fe096d5eb78aee691
+Group:          Development/Tools
+Vendor:         VMware, Inc.
+Distribution:   Photon
+BuildRequires:  git
+BuildRequires:  glide
+BuildRequires:  go >= 1.7
+BuildRequires:  make
+BuildRequires:  which
+
+%description
+Calico node is a container that bundles together various components reqiured for networking containers using project calico. This includes key components such as felix agent for programming routes and ACLs, BIRD routing daemon, and confd datastore monitor engine. 
+
+%prep
+%setup
+
+%build
+mkdir -p /root/.glide
+mkdir -p ${GOPATH}/src/github.com/projectcalico/calico
+cp -r * ${GOPATH}/src/github.com/projectcalico/calico/.
+pushd ${GOPATH}/src/github.com/projectcalico/calico
+cd calico_node
+glide install --strip-vendor
+mkdir -p dist
+mkdir -p .go-pkg-cache
+make CALICO_GIT_VER=%{version} allocate-ipip-addr
+make CALICO_GIT_VER=%{version} startup
+
+%install
+pushd ${GOPATH}/src/github.com/projectcalico/calico
+install -vdm 755 %{buildroot}%{_bindir}
+install calico_node/dist/allocate-ipip-addr %{buildroot}%{_bindir}/
+install calico_node/dist/startup %{buildroot}%{_bindir}/
+install -vdm 0755 %{buildroot}/usr/share/calico/docker/fs
+cp -r calico_node/filesystem/etc %{buildroot}/usr/share/calico/docker/fs/
+cp -r calico_node/filesystem/sbin %{buildroot}/usr/share/calico/docker/fs/
+sed -i 's/. startup.env/source \/startup.env/g' %{buildroot}/usr/share/calico/docker/fs/etc/rc.local
+sed -i 's/. startup.env/source \/startup.env/g' %{buildroot}/usr/share/calico/docker/fs/sbin/start_runit
+
+%files
+%defattr(-,root,root)
+%{_bindir}/allocate-ipip-addr
+%{_bindir}/startup
+/usr/share/calico/docker/fs/*
+
+%changelog
+*   Mon Aug 16 2017 Vinay Kulkarni <kulkarniv@vmware.com> 2.4.1-1
+-   Calico Node for PhotonOS.
---
+++ support/dockerfiles/k8s-docker-images/build-calico-docker-images.sh
@@ -0,0 +1,147 @@
+#!/bin/bash -e
+
+DIST_TAG=$1
+DIST_VER=$2
+SPEC_DIR=$3
+STAGE_DIR=$4
+ARCH=x86_64
+
+CALICO_NODE_TAR=calico-node.tar
+CALICO_CNI_TAR=calico-cni.tar
+
+#
+# Docker images for calico-node, calico-cni
+#
+CALICO_VER=`cat ${SPEC_DIR}/calico/calico.spec | grep Version | cut -d: -f2 | tr -d ' '`
+CALICO_VER_REL=${CALICO_VER}-`cat ${SPEC_DIR}/calico/calico.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
+CALICO_RPM=calico-${CALICO_VER_REL}${DIST_TAG}.${ARCH}.rpm
+CALICO_RPM_FILE=${STAGE_DIR}/RPMS/x86_64/${CALICO_RPM}
+
+CALICO_BGP_VER=`cat ${SPEC_DIR}/calico-bgp-daemon/calico-bgp-daemon.spec | grep Version | cut -d: -f2 | tr -d ' '`
+CALICO_BGP_VER_REL=${CALICO_BGP_VER}-`cat ${SPEC_DIR}/calico-bgp-daemon/calico-bgp-daemon.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
+CALICO_BGP_RPM=calico-bgp-daemon-${CALICO_BGP_VER_REL}${DIST_TAG}.${ARCH}.rpm
+CALICO_BGP_RPM_FILE=${STAGE_DIR}/RPMS/x86_64/${CALICO_BGP_RPM}
+
+CALICO_BIRD_VER=`cat ${SPEC_DIR}/calico-bird/calico-bird.spec | grep Version | cut -d: -f2 | tr -d ' '`
+CALICO_BIRD_VER_REL=${CALICO_BIRD_VER}-`cat ${SPEC_DIR}/calico-bird/calico-bird.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
+CALICO_BIRD_RPM=calico-bird-${CALICO_BIRD_VER_REL}${DIST_TAG}.${ARCH}.rpm
+CALICO_BIRD_RPM_FILE=${STAGE_DIR}/RPMS/x86_64/${CALICO_BIRD_RPM}
+
+CALICO_CONFD_VER=`cat ${SPEC_DIR}/calico-confd/calico-confd.spec | grep Version | cut -d: -f2 | tr -d ' '`
+CALICO_CONFD_VER_REL=${CALICO_CONFD_VER}-`cat ${SPEC_DIR}/calico-confd/calico-confd.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
+CALICO_CONFD_RPM=calico-confd-${CALICO_CONFD_VER_REL}${DIST_TAG}.${ARCH}.rpm
+CALICO_CONFD_RPM_FILE=${STAGE_DIR}/RPMS/x86_64/${CALICO_CONFD_RPM}
+
+CALICO_FELIX_VER=`cat ${SPEC_DIR}/calico-felix/calico-felix.spec | grep ^Version | cut -d: -f2 | tr -d ' '`
+CALICO_FELIX_VER_REL=${CALICO_FELIX_VER}-`cat ${SPEC_DIR}/calico-felix/calico-felix.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
+CALICO_FELIX_RPM=calico-felix-${CALICO_FELIX_VER_REL}${DIST_TAG}.${ARCH}.rpm
+CALICO_FELIX_RPM_FILE=${STAGE_DIR}/RPMS/x86_64/${CALICO_FELIX_RPM}
+
+CALICO_LIBNET_VER=`cat ${SPEC_DIR}/calico-libnetwork/calico-libnetwork.spec | grep ^Version | cut -d: -f2 | tr -d ' '`
+CALICO_LIBNET_VER_REL=${CALICO_LIBNET_VER}-`cat ${SPEC_DIR}/calico-libnetwork/calico-libnetwork.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
+CALICO_LIBNET_RPM=calico-libnetwork-${CALICO_LIBNET_VER_REL}${DIST_TAG}.${ARCH}.rpm
+CALICO_LIBNET_RPM_FILE=${STAGE_DIR}/RPMS/x86_64/${CALICO_LIBNET_RPM}
+
+CALICO_CNI_VER=`cat ${SPEC_DIR}/calico-cni/calico-cni.spec | grep Version | cut -d: -f2 | tr -d ' '`
+CALICO_CNI_VER_REL=${CALICO_CNI_VER}-`cat ${SPEC_DIR}/calico-cni/calico-cni.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
+CALICO_CNI_RPM=calico-cni-${CALICO_CNI_VER_REL}${DIST_TAG}.${ARCH}.rpm
+CALICO_CNI_RPM_FILE=${STAGE_DIR}/RPMS/x86_64/${CALICO_CNI_RPM}
+
+K8S_CNI_VER=`cat ${SPEC_DIR}/cni/cni.spec | grep ^Version | cut -d: -f2 | tr -d ' '`
+K8S_CNI_VER_REL=${K8S_CNI_VER}-`cat ${SPEC_DIR}/cni/cni.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
+K8S_CNI_RPM=cni-${K8S_CNI_VER_REL}${DIST_TAG}.${ARCH}.rpm
+K8S_CNI_RPM_FILE=${STAGE_DIR}/RPMS/x86_64/${K8S_CNI_RPM}
+
+if [ ! -f ${CALICO_RPM_FILE} ]
+then
+    echo "Calico RPM ${CALICO_RPM_FILE} not found. Exiting.."
+    exit 1
+fi
+
+if [ ! -f ${CALICO_BGP_RPM_FILE} ]
+then
+    echo "Calico BGP RPM ${CALICO_BGP_RPM_FILE} not found. Exiting.."
+    exit 1
+fi
+
+if [ ! -f ${CALICO_BIRD_RPM_FILE} ]
+then
+    echo "Calico BIRD RPM ${CALICO_BIRD_RPM_FILE} not found. Exiting.."
+    exit 1
+fi
+
+if [ ! -f ${CALICO_CONFD_RPM_FILE} ]
+then
+    echo "Calico confd RPM ${CALICO_CONFD_RPM_FILE} not found. Exiting.."
+    exit 1
+fi
+
+if [ ! -f ${CALICO_FELIX_RPM_FILE} ]
+then
+    echo "Calico felix RPM ${CALICO_FELIX_RPM_FILE} not found. Exiting.."
+    exit 1
+fi
+
+if [ ! -f ${CALICO_LIBNET_RPM_FILE} ]
+then
+    echo "Calico libnetwork RPM ${CALICO_LIBNET_RPM_FILE} not found. Exiting.."
+    exit 1
+fi
+
+if [ ! -f ${CALICO_CNI_RPM_FILE} ]
+then
+    echo "Calico CNI RPM ${CALICO_CNI_RPM_FILE} not found. Exiting.."
+    exit 1
+fi
+
+if [ ! -f ${K8S_CNI_RPM_FILE} ]
+then
+    echo "K8S CNI RPM ${K8S_CNI_RPM_FILE} not found. Exiting.."
+    exit 1
+fi
+
+
+CALICO_NODE_IMG_NAME=vmware_photon2/calico-node:v${CALICO_VER}
+CALICO_CNI_IMG_NAME=vmware_photon2/calico-cni:v${CALICO_CNI_VER}
+
+NODE_IMG_ID=`docker images -q ${CALICO_NODE_IMG_NAME} 2> /dev/null`
+if [[ ! -z "${NODE_IMG_ID}" ]]; then
+    echo "Removing image ${CALICO_NODE_IMG_NAME}"
+    docker rmi -f ${CALICO_NODE_IMG_NAME}
+fi
+
+CNI_IMG_ID=`docker images -q ${CALICO_CNI_IMG_NAME} 2> /dev/null`
+if [[ ! -z "${CNI_IMG_ID}" ]]; then
+    echo "Removing image ${CALICO_CNI_IMG_NAME}"
+    docker rmi -f ${CALICO_CNI_IMG_NAME}
+fi
+
+mkdir -p tmp/calico
+cp ${CALICO_RPM_FILE} tmp/calico/
+cp ${CALICO_BGP_RPM_FILE} tmp/calico/
+cp ${CALICO_BIRD_RPM_FILE} tmp/calico/
+cp ${CALICO_CONFD_RPM_FILE} tmp/calico/
+cp ${CALICO_FELIX_RPM_FILE} tmp/calico/
+cp ${CALICO_LIBNET_RPM_FILE} tmp/calico/
+cp ${CALICO_CNI_RPM_FILE} tmp/calico/
+cp ${K8S_CNI_RPM_FILE} tmp/calico/
+pushd ./tmp/calico
+rpm2cpio ${CALICO_RPM} | cpio -vid
+rpm2cpio ${CALICO_BGP_RPM} | cpio -vid
+rpm2cpio ${CALICO_BIRD_RPM} | cpio -vid
+rpm2cpio ${CALICO_CONFD_RPM} | cpio -vid
+rpm2cpio ${CALICO_FELIX_RPM} | cpio -vid
+rpm2cpio ${CALICO_LIBNET_RPM} | cpio -vid
+rpm2cpio ${CALICO_CNI_RPM} | cpio -vid
+rpm2cpio ${K8S_CNI_RPM} | cpio -vid
+popd
+
+docker build --rm -t ${CALICO_NODE_IMG_NAME} -f Dockerfile.calico-node .
+docker save -o ${CALICO_NODE_TAR} ${CALICO_NODE_IMG_NAME}
+mv -f ${CALICO_NODE_TAR} ${STAGE_DIR}/
+
+docker build --rm -t ${CALICO_CNI_IMG_NAME} -f Dockerfile.calico-cni .
+docker save -o ${CALICO_CNI_TAR} ${CALICO_CNI_IMG_NAME}
+mv -f ${CALICO_CNI_TAR} ${STAGE_DIR}/
+
+rm -rf ./tmp
