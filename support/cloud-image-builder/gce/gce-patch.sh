431d3d58735c44fbde52600919193ca046a282e6
suezzelur <anishs@vmware.com>
2017-08-23 16:04:32 -0700

    Modify gce config and remove boto config from compute init
    Change-Id: I9a7c291d40e3602843dcdc759e96d9b1746462bd

--- support/cloud-image-builder/gce/gce-patch.sh
+++ support/cloud-image-builder/gce/gce-patch.sh
@@ -8,14 +8,13 @@ for i in ../google*; do  ln -s $i `basename $i`; done
 
 ln -s ../ntpd.service ntpd.service
 ln -s ../docker.service docker.service
-ln -s ../eth0.service eth0.service
-
 
 # Update /etc/hosts file with GCE values
 echo "169.254.169.254 metadata.google.internal metadata" >> /etc/hosts
 
 # Remove all servers from ntp.conf and add Google's ntp server.
 sed -i -e "/server/d" /etc/ntp.conf
+echo "server metadata.google.internal iburst" >> /etc/ntp.conf
 echo "server 169.254.169.254" >> /etc/ntp.conf
 
 
@@ -80,10 +79,13 @@ usermod -L root
 echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.d/ipv6-disable.conf
 
 # Disable loading/unloading of modules
-#echo "kernel.modules_disabled = 1" > /etc/sysctl.d/modules_disabled.conf
+echo "kernel.modules_disabled = 1" > /etc/sysctl.d/modules_disabled.conf
 
 # Remove kernel symbols
 rm -f /boot/System.map*
+
+echo "[Link]" >> /etc/systemd/network/*.network
+echo "MTUBytes=1460" >> /etc/systemd/network/*.network
 
 cat > /usr/bin/gcloud << "EOF"
 docker inspect google/cloud-sdk &> /dev/null
