9a168e5c31f810391ce8ef9a9e2aa54ac6f2b3f1
Bo Gan <ganb@vmware.com>
2017-09-12 15:16:58 -0700

    close extra file descriptors in run-in-chroot
    Change-Id: I08343963498e7baa35a8be02288aa4ef51c5abad
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/3761
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Bo Gan <ganb@vmware.com>

--- support/package-builder/run-in-chroot.sh
+++ support/package-builder/run-in-chroot.sh
@@ -35,6 +35,12 @@ else
     CHROOT_CMD="contain -b $RPMS:usr/src/photon/RPMS -c -n"
 fi
 
+
+# Close all fds except stdin, stdout and stderr
+for fd in $(ls /proc/$$/fd/); do
+    [ $fd -gt 2 ] && exec {fd}<&-
+done
+
 $CHROOT_CMD "${BUILDROOT}" \
 	/usr/bin/env -i \
 	HOME=/root \
