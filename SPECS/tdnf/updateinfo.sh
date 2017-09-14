8d36c4d93fdef6f2250d7b46a4e5cde3566e4722
Bo Gan <ganb@vmware.com>
2017-09-14 15:48:26 -0700

    add tdnf motd suggestion
    Change-Id: I66a6f7c227852bc093b6e2643ac30ae411d5cf22

--- SPECS/tdnf/updateinfo.sh
+++ SPECS/tdnf/updateinfo.sh
@@ -2,6 +2,7 @@ path=/var/cache/tdnf/cached-updateinfo.txt
 
 if [ -f "$path" ]; then
     cat $path
+    echo "Run 'tdnf updateinfo info' to see the details."
 else
     echo "tdnf update info not available yet!"
 fi
