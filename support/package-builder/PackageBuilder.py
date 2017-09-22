8a15ef62afd53df1e428c398300f3e9808d6482e
Alexey Makhalov <amakhalov@vmware.com>
2017-09-21 17:30:39 -0700

    Fix toybox conflicts
    build system imprevements:
    - use --noorder to install toolchain RPMS. We know the order better.
    - improve logging, put more logs into stage/LOGS/build-<pkgname> folder.
    - removed duplicated dist param
    toybox:
    - move /usr/bin/sed to /bin/sed to align with sed package
    
    Change-Id: Ia6b147e2fe0c7b356be209684d9c68e6302862c2

--- support/package-builder/PackageBuilder.py
+++ support/package-builder/PackageBuilder.py
@@ -10,22 +10,21 @@ from SpecData import SPECS
 
 class PackageBuilder(object):
 
-    def __init__(self,mapPackageToCycles,listAvailableCyclicPackages,listBuildOptionPackages,pkgBuildOptionFile,logName=None,logPath=None):
-        if logName is None:
-            logName = "PackageBuilder"
-        if logPath is None:
-            logPath = constants.logPath
-        self.logName=logName
-        self.logPath=logPath
-        self.logger=Logger.getLogger(logName,logPath)
+    def __init__(self,mapPackageToCycles,listAvailableCyclicPackages,listBuildOptionPackages,pkgBuildOptionFile):
+        # will be initialized in buildPackageThreadAPI()
+        self.logName=None
+        self.logPath=None
+        self.logger=None
+        self.package=None
         self.mapPackageToCycles = mapPackageToCycles
         self.listAvailableCyclicPackages = listAvailableCyclicPackages
         self.listNodepsPackages = ["glibc","gmp","zlib","file","binutils","mpfr","mpc","gcc","ncurses","util-linux","groff","perl","texinfo","rpm","openssl","go"]
         self.listBuildOptionPackages=listBuildOptionPackages
         self.pkgBuildOptionFile=pkgBuildOptionFile
 
-    def prepareBuildRoot(self,chrootName, packageName):
+    def prepareBuildRoot(self):
         chrootID=None
+        chrootName="build-"+self.package
         try:
             chrUtils = ChrootUtils(self.logName,self.logPath)
             returnVal,chrootID = chrUtils.createChroot(chrootName)
@@ -33,7 +32,7 @@ class PackageBuilder(object):
             if not returnVal:
                 raise Exception("Unable to prepare build root")
             tUtils=ToolChainUtils(self.logName,self.logPath)
-            tUtils.installToolChainRPMS(chrootID, packageName)
+            tUtils.installToolChainRPMS(chrootID, self.package, self.logPath)
         except Exception as e:
             if chrootID is not None:
                 self.logger.debug("Deleting chroot: " + chrootID)
@@ -65,15 +64,24 @@ class PackageBuilder(object):
         return listInstalledPackages
 
     def buildPackageThreadAPI(self,package,outputMap, threadName,):
+        self.package=package
+        self.logName="build-"+package
+        self.logPath=constants.logPath+"/build-"+package
+        if not os.path.isdir(self.logPath):
+            cmdUtils = CommandUtils()
+            cmdUtils.runCommandInShell("mkdir -p "+self.logPath)
+        self.logger=Logger.getLogger(self.logName,self.logPath)
+
         try:
-            self.buildPackage(package)
+            self.buildPackage()
             outputMap[threadName]=True
         except Exception as e:
+            # TODO: self.logger might be None
             self.logger.error(e)
             outputMap[threadName]=False
 
-    def checkIfPackageIsAlreadyBuilt(self, package):
-        basePkg=SPECS.getData().getSpecName(package)
+    def checkIfPackageIsAlreadyBuilt(self):
+        basePkg=SPECS.getData().getSpecName(self.package)
         listRPMPackages=SPECS.getData().getRPMPackages(basePkg)
         packageIsAlreadyBuilt=True
         pkgUtils = PackageUtils(self.logName,self.logPath)
@@ -83,34 +91,27 @@ class PackageBuilder(object):
                 break
         return packageIsAlreadyBuilt
 
-    def buildPackage(self, package):
+    def buildPackage(self):
         #do not build if RPM is already built
         #test only if the package is in the testForceRPMS with rpmCheck
         #build only if the package is not in the testForceRPMS with rpmCheck
-        if self.checkIfPackageIsAlreadyBuilt(package):
+        if self.checkIfPackageIsAlreadyBuilt():
             if not constants.rpmCheck:
-                self.logger.info("Skipping building the package:"+package)
+                self.logger.info("Skipping building the package:"+self.package)
                 return
-            elif constants.rpmCheck and package not in constants.testForceRPMS:
-                self.logger.info("Skipping testing the package:"+package)
+            elif constants.rpmCheck and self.package not in constants.testForceRPMS:
+                self.logger.info("Skipping testing the package:"+self.package)
                 return
 
-        #should initialize a logger based on package name
         chrUtils = ChrootUtils(self.logName,self.logPath)
-        chrootName="build-"+package
         chrootID=None
         try:
-            chrootID = self.prepareBuildRoot(chrootName, package)
-            destLogPath=constants.logPath+"/build-"+package
-            if not os.path.isdir(destLogPath):
-                cmdUtils = CommandUtils()
-                cmdUtils.runCommandInShell("mkdir -p "+destLogPath)
-
+            chrootID = self.prepareBuildRoot()
             listInstalledPackages=self.findInstalledPackages(chrootID)
-            listDependentPackages=self.findBuildTimeRequiredPackages(package)
-            if constants.rpmCheck and package in constants.testForceRPMS:
-                listDependentPackages.extend(self.findBuildTimeCheckRequiredPackages(package))
-                testPackages=set(constants.listMakeCheckRPMPkgtoInstall)-set(listInstalledPackages)-set([package])
+            listDependentPackages=self.findBuildTimeRequiredPackages()
+            if constants.rpmCheck and self.package in constants.testForceRPMS:
+                listDependentPackages.extend(self.findBuildTimeCheckRequiredPackages())
+                testPackages=set(constants.listMakeCheckRPMPkgtoInstall)-set(listInstalledPackages)-set([self.package])
                 listDependentPackages.extend(testPackages)
                 listDependentPackages=list(set(listDependentPackages))
 
@@ -118,17 +119,17 @@ class PackageBuilder(object):
             if len(listDependentPackages) != 0:
                 self.logger.info("Installing the build time dependent packages......")
                 for pkg in listDependentPackages:
-                    self.installPackage(pkgUtils, pkg,chrootID,destLogPath,listInstalledPackages)
-                pkgUtils.installRPMSInAOneShot(chrootID,destLogPath)
+                    self.installPackage(pkgUtils, pkg,chrootID,self.logPath,listInstalledPackages)
+                pkgUtils.installRPMSInAOneShot(chrootID,self.logPath)
                 self.logger.info("Finished installing the build time dependent packages......")
 
-            pkgUtils.adjustGCCSpecs(package, chrootID, destLogPath)
-            pkgUtils.buildRPMSForGivenPackage(package,chrootID,self.listBuildOptionPackages,self.pkgBuildOptionFile,destLogPath)
-            self.logger.info("Successfully built the package:"+package)
+            pkgUtils.adjustGCCSpecs(self.package, chrootID, self.logPath)
+            pkgUtils.buildRPMSForGivenPackage(self.package,chrootID,self.listBuildOptionPackages,self.pkgBuildOptionFile,self.logPath)
+            self.logger.info("Successfully built the package:"+self.package)
         except Exception as e:
-            self.logger.error("Failed while building package:" + package)
+            self.logger.error("Failed while building package:" + self.package)
             self.logger.debug("Chroot with ID: " + chrootID + " not deleted for debugging.")
-            logFileName = os.path.join(destLogPath, package + ".log")
+            logFileName = os.path.join(self.logPath, self.package + ".log")
             fileLog = os.popen('tail -n 100 ' + logFileName).read()
             self.logger.debug(fileLog)
             raise e
@@ -140,12 +141,12 @@ class PackageBuilder(object):
         listRequiredPackages=SPECS.getData().getRequiresForPackage(rpmPackage)
         return listRequiredPackages
 
-    def findBuildTimeRequiredPackages(self,package):
-        listRequiredPackages=SPECS.getData().getBuildRequiresForPackage(package)
+    def findBuildTimeRequiredPackages(self):
+        listRequiredPackages=SPECS.getData().getBuildRequiresForPackage(self.package)
         return listRequiredPackages
 
-    def findBuildTimeCheckRequiredPackages(self,package):
-        listRequiredPackages=SPECS.getData().getCheckBuildRequiresForPackage(package)
+    def findBuildTimeCheckRequiredPackages(self):
+        listRequiredPackages=SPECS.getData().getCheckBuildRequiresForPackage(self.package)
         return listRequiredPackages
 
     def installPackage(self,pkgUtils,package,chrootID,destLogPath,listInstalledPackages):
