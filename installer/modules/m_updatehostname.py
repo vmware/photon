8ad1dde39796f40fc15b75ed613bdf462381244f
xiaolin-vmware <xiaolinl@vmware.com>
2017-09-30 20:30:21 -0700

    Chage hostname alignment.
    Change-Id: I288bd0311c73504399dd586710988af83f3c562d

--- installer/modules/m_updatehostname.py
+++ installer/modules/m_updatehostname.py
@@ -5,7 +5,15 @@ import random
 install_phase = commons.POST_INSTALL
 enabled = True
 
-def execute(name, config, root):
+def execute(name, ks_config, config, root):
+
+    if ks_config:
+        if "hostname" in ks_config:
+            config['hostname'] = ks_config["hostname"].strip(" ")
+        if "hostname" not in config or config['hostname'] == "":
+            random_id = '%12x' % random.randrange(16**12)
+            config['hostname'] = "photon-" + random_id.strip()
+
     hostname = config['hostname']
 
     hostname_file = os.path.join(root, 'etc/hostname')
@@ -14,4 +22,4 @@ def execute(name, config, root):
     with open(hostname_file,  'wb') as outfile:
         outfile.write(hostname)
 
-    commons.replace_string_in_file(hosts_file, r'127\.0\.0\.1\s+localhost\s*\Z', '127.0.0.1\tlocalhost\n127.0.0.1\t' + hostname)
+    commons.replace_string_in_file(hosts_file, r'(127\.0\.0\.1)(\s+)(localhost)\s*\Z', r'\1\2\3\n\1\2' + hostname)
