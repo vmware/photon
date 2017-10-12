d1c40fdd4cbef7ffad9d19dbd02cac6f41bbe7a2
Vinay Kulkarni <kulkarniv@vmware.com>
2017-10-12 01:17:18 +0000

    Rename docker images to include version info
    Change-Id: I659b690c339b1f5f963d4f8d79f1d62f596d8ddd

--- support/dockerfiles/k8s-docker-images/build-calico-docker-images.sh
+++ support/dockerfiles/k8s-docker-images/build-calico-docker-images.sh
@@ -6,10 +6,6 @@ SPEC_DIR=$3
 STAGE_DIR=$4
 ARCH=x86_64
 
-CALICO_NODE_TAR=calico-node.tar
-CALICO_CNI_TAR=calico-cni.tar
-CALICO_K8S_POLICY_TAR=calico-k8s-policy.tar
-
 #
 # Docker images for calico-node, calico-cni
 #
@@ -126,6 +122,9 @@ fi
 CALICO_NODE_IMG_NAME=vmware_photon_${DIST_VER}/calico-node:v${CALICO_VER}
 CALICO_CNI_IMG_NAME=vmware_photon_${DIST_VER}/calico-cni:v${CALICO_CNI_VER}
 CALICO_K8S_POLICY_IMG_NAME=vmware_photon_${DIST_VER}/calico-kube-policy-controller:v${CALICO_K8S_POLICY_VER}
+CALICO_NODE_TAR=calico-node-v${CALICO_VER}.tar
+CALICO_CNI_TAR=calico-cni-v${CALICO_CNI_VER}.tar
+CALICO_K8S_POLICY_TAR=calico-k8s-policy-v${CALICO_K8S_POLICY_VER}.tar
 
 NODE_IMG_ID=`docker images -q ${CALICO_NODE_IMG_NAME} 2> /dev/null`
 if [[ ! -z "${NODE_IMG_ID}" ]]; then
