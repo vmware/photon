701d1f26bb67c4e976be2b7469185987f0ebc9ce
Rui Gu <ruig@vmware.com>
2017-11-08 15:13:04 -0800

    Fix shutil.copyfile() race condition
    shutil.copyfile() is not atomic. Package builder may use
    an broken RPM accidently during the copy process.
    The race will only affect replacement of publishRPMs. Because
    other packages use a map in python to notify others when the copy is
    ready. But publishRPMs does not do any check at all.
    As a result, doing publishRPM replacement, installToolChainRPM()
    may accidently try to install a broken RPM.
    
    Change-Id: Ib4c2e1927d16cc6156d535c33bf5c2f96cf07caf

--- support/package-builder/CommandUtils.py
+++ support/package-builder/CommandUtils.py
@@ -6,10 +6,16 @@ class CommandUtils(object):
         self.findBinary = "find"
 
     def findFile (self, filename, sourcePath):
-        process = subprocess.Popen([self.findBinary,  "-L", sourcePath,  "-name", filename, "-not", "-type", "d"],  stdout=subprocess.PIPE)
-        returnVal = process.wait()
-        if returnVal != 0:
-            return None
+        process = subprocess.Popen([self.findBinary, "-L", sourcePath, "-name", filename, "-not", "-type", "d"],  stdout=subprocess.PIPE)
+        # We don't check the return val here because the only situation find returns 1 is when
+        # it finds out the directory stats is inconsistent during the find process.
+        # find's flag ignore_readdir_race can suppress this but it isn't working.
+        # https://bugs.centos.org/view.php?id=13685
+        # The reason we want to suppress this is because for built RPMs, we first copy it to
+        # the location with a random name and move it to the real name.
+
+        #if returnVal != 0:
+        #    return None
         result=process.communicate()[0]
         if result is None:
             return None
@@ -27,7 +33,7 @@ class CommandUtils(object):
         if retval==0:
             return True
         return False
-    
+
     def runCommandInShell2(self,cmd,chrootCmd=None):
         if chrootCmd is not None:
             cmd = chrootCmd+" "+cmd
