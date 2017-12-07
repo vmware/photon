4fa1c4ff9b6d72391fabfd363ce54ea640e8ed80
xiaolin-vmware <xiaolinl@vmware.com>
2017-12-06 18:04:06 -0800

    Use the with keyword when dealing with file objects.
    Change-Id: I3586b00e5c671707c221347fdc542ec28b4db9a9
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/4472
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Bo Gan <ganb@vmware.com>
    (cherry picked from commit e51de7175ebcb19fc411864e45b83c17c2a57623)

--- installer/jsonwrapper.py
+++ installer/jsonwrapper.py
@@ -13,12 +13,11 @@ class JsonWrapper(object):
         self.filename = filename
 
     def read(self):
-        json_data = open(self.filename)
-        self.data = json.load(json_data, object_pairs_hook=collections.OrderedDict)
-        json_data.close()
+        with open(self.filename) as json_data:
+            self.data = json.load(json_data, object_pairs_hook=collections.OrderedDict)
         return self.data
 
     def write(self,  data):
         self.data = data
-        outfile = open(self.filename,  'wb')
-        json.dump(data,  outfile)
+        with open(self.filename,  'wb') as outfile:
+            json.dump(data,  outfile)
