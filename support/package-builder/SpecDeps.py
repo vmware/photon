8f5f4df4df7a912727a10aefb002e9e0b43c85c1
xiaolin-vmware <xiaolinl@vmware.com>
2018-01-05 15:06:59 -0800

    SpecDeps.py : Convert from optparse to argparse module
    Change-Id: Ic8afe9d3484100aea13670ad4be2c0f9a21a3618
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/4605
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Divya Thaluru <dthaluru@vmware.com>

--- support/package-builder/SpecDeps.py
+++ support/package-builder/SpecDeps.py
@@ -5,9 +5,7 @@
 #    Author: Harish Udaiya Kumar <hudaiyakumar@vmware.com>
 import sys
 import os
-from optparse import OptionParser
-from SpecUtils import Specutils
-from SpecData import SerializableSpecObject
+from argparse import ArgumentParser
 from SpecData import SerializedSpecObjects
 from jsonwrapper import JsonWrapper
 
@@ -17,19 +15,17 @@ SPEC_FILE_DIR = "../../SPECS"
 LOG_FILE_DIR = "../../stage/LOGS"
 
 def main():
-    usage = (os.path.basename(__file__) +
-             "--input-type=[json/pkg/who-needs/who-needs-build] " +
-             "--pkg=[pkg_name] --file=<JSON_FILE_NAME> --disp=[tree/list/json]")
-    parser = OptionParser(usage)
-    parser.add_option("-i", "--input-type", dest="input_type", default=DEFAULT_INPUT_TYPE)
-    parser.add_option("-p", "--pkg", dest="pkg")
-    parser.add_option("-f", "--file", dest="json_file", default="packages_minimal.json")
-    parser.add_option("-d", "--disp", dest="display_option", default=DEFAULT_DISPLAY_OPTION)
-    parser.add_option("-s", "--spec-dir", dest="spec_dir", default=SPEC_FILE_DIR)
-    parser.add_option("-t", "--stage-dir", dest="stage_dir", default="../../stage")
-    parser.add_option("-a", "--input-data-dir", dest="input_data_dir",
-                      default="../../common/data/")
-    (options,  args) = parser.parse_args()
+    usage = "Usage: %prog [options]"
+    parser = ArgumentParser(usage)
+    parser.add_argument("-i", "--input-type", dest="input_type", default=DEFAULT_INPUT_TYPE)
+    parser.add_argument("-p", "--pkg", dest="pkg")
+    parser.add_argument("-f", "--file", dest="json_file", default="packages_minimal.json")
+    parser.add_argument("-d", "--disp", dest="display_option", default=DEFAULT_DISPLAY_OPTION)
+    parser.add_argument("-s", "--spec-dir", dest="spec_dir", default=SPEC_FILE_DIR)
+    parser.add_argument("-t", "--stage-dir", dest="stage_dir", default="../../stage")
+    parser.add_argument("-a", "--input-data-dir", dest="input_data_dir",
+                        default="../../common/data/")
+    options = parser.parse_args()
 
     if not options.input_data_dir.endswith('/'):
         options.input_data_dir += '/'
@@ -42,13 +38,13 @@ def main():
 
         # To display/print package dependencies on console
         if(options.input_type == "pkg" or
-            options.input_type == "who-needs" or
-            options.input_type == "who-needs-build"):
+           options.input_type == "who-needs" or
+           options.input_type == "who-needs-build"):
             targetName = options.pkg
             specDeps.readSpecsAndConvertToSerializableObjects(options.spec_dir,
                                                               options.input_type,
                                                               targetName, displayOption)
-        elif(options.input_type == "json"):
+        elif options.input_type == "json":
         # Generate the expanded package dependencies json file based on package_list_file
             json_wrapper_option_list = JsonWrapper(options.json_file)
             option_list_json = json_wrapper_option_list.read()
@@ -60,7 +56,7 @@ def main():
                     options.spec_dir,
                     options.input_type, install_option[1]["file"],
                     displayOption)
-    except Exception as error:
+    except Exception as _:
         sys.stderr.write("Failed to generate dependency lists from spec files\n")
         sys.exit(1)
 
