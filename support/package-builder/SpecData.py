7ded431942173aa180c4c80c244346f2d53f916e
dthaluru <dthaluru@vmware.com>
2018-01-09 16:25:44 -0800

    Cleanup build code: Removed the unused and duplicated code
    Change-Id: If581bcf04386052d5a787bcca4e50e8ff44212b3

--- support/package-builder/SpecData.py
+++ support/package-builder/SpecData.py
@@ -8,6 +8,8 @@ from SpecUtils import Specutils
 from Logger import Logger
 from constants import constants
 
+
+
 class SerializableSpecObject(object):
     def __init__(self):
         self.listPackages = []
@@ -28,6 +30,7 @@ class SerializableSpecObject(object):
         self.sourceurl = ""
         self.license = ""
         self.specDefs = {}
+
 
 class SerializableSpecObjectsUtils(object):
 
@@ -71,7 +74,7 @@ class SerializableSpecObjectsUtils(object):
                 self.mapPackageToSpec[specPkg] = specName
                 if spec.getIsRPMPackage(specPkg):
                     specObj.listRPMPackages.append(specPkg)
-            if skipUpdating == False:
+            if not skipUpdating:
                 self.mapSerializableSpecObjects[specName] = specObj
 
     def getListSpecFiles(self, listSpecFiles, path):
@@ -135,10 +138,11 @@ class SerializableSpecObjectsUtils(object):
         specName = self.getSpecName(package)
         return self.mapSerializableSpecObjects[specName].listRPMPackages
 
-    def getReleaseNum(self, releaseVal):
-        id = releaseVal.find("%")
-        if id != -1:
-            return releaseVal[0:id]
+    @staticmethod
+    def getReleaseNum(releaseVal):
+        releaseNum = releaseVal.find("%")
+        if releaseNum != -1:
+            return releaseVal[0:releaseNum]
         else:
             return releaseVal
 
@@ -223,6 +227,7 @@ class SerializableSpecObjectsUtils(object):
             self.logger.info("security_hardening: " + specObj.securityHardening)
             self.logger.info("------------------------------------------------")
 
+
 class SPECS(object):
     __instance = None
     specData = None
@@ -244,7 +249,7 @@ class SPECS(object):
 
     def initialize(self):
         # Preparse some files
-        #adding openjre8 version rpm macro
+        # adding openjre8 version rpm macro
         if platform.machine() == "x86_64":
             spec = Specutils(constants.specPath + "/openjdk8/openjdk8.spec")
         else:
@@ -252,16 +257,16 @@ class SPECS(object):
         java8version = spec.getVersion()
         constants.addMacro("JAVA8_VERSION", java8version)
 
-        #adding kernelversion rpm macro
+        # adding kernelversion rpm macro
         spec = Specutils(constants.specPath + "/linux/linux.spec")
         kernelversion = spec.getVersion()
         constants.addMacro("KERNEL_VERSION", kernelversion)
 
-        #adding kernelrelease rpm macro
+        # adding kernelrelease rpm macro
         kernelrelease = spec.getRelease()
         constants.addMacro("KERNEL_RELEASE", kernelrelease)
 
-        #adding kernelsubrelease rpm macro
+        # adding kernelsubrelease rpm macro
         a, b, c = kernelversion.split(".")
         kernelsubrelease = ('%02d%02d%03d%03d' % (int(a),
                                                   int(b), int(c),
@@ -274,177 +279,79 @@ class SPECS(object):
         self.specData = SerializableSpecObjectsUtils(constants.logPath)
         self.specData.readSpecsAndConvertToSerializableObjects(constants.specPath)
 
-# Little bit of duplication
-# Used by SpecVerify and SpecDeps only
-class SerializedSpecObjects(object):
 
-    def __init__(self, inputDataDir, stageDir):
-        self.mapSerializableSpecObjects = {}
-        self.mapPackageToSpec = {}
-        self.jsonFilesOutPath = stageDir + "/common/data/"
-        self.inputDataDir = inputDataDir
+class SpecDependencyGenerator(object):
 
-    def findTotalRequires(self, allDeps, depQue, parent, displayOption):
+    def findTotalRequires(self, mapDependencies, depQue, parent):
         while not depQue.empty():
             specPkg = depQue.get()
-            specName = self.getSpecName(specPkg)
-            if specName is None:
+            try:
+                listRequiredPackages = SPECS.getData().getRequiresForPackage(specPkg)
+            except Exception as e:
+                print("Caught Exception:"+str(e))
                 print(specPkg + " is missing")
-            specObj = self.mapSerializableSpecObjects[specName]
-            for depPkg in specObj.installRequiresPackages[specPkg]:
-                if depPkg in allDeps:
-                    if allDeps[depPkg] < allDeps[specPkg] + 1:
-                        allDeps[depPkg] = allDeps[specPkg] + 1
+
+            for depPkg in listRequiredPackages:
+                if depPkg in mapDependencies:
+                    if mapDependencies[depPkg] < mapDependencies[specPkg] + 1:
+                        mapDependencies[depPkg] = mapDependencies[specPkg] + 1
                         parent[depPkg] = specPkg
-                        self.updateLevels(allDeps, depPkg, parent, allDeps[depPkg])
+                        self.updateLevels(mapDependencies, depPkg, parent, mapDependencies[depPkg])
                 else:
-                    allDeps[depPkg] = allDeps[specPkg] + 1
+                    mapDependencies[depPkg] = mapDependencies[specPkg] + 1
                     parent[depPkg] = specPkg
                     depQue.put(depPkg)
 
-    def findTotalWhoNeedsToBuild(self, depQue, whoBuildDeps, whoBuildDepSet, displayOption):
+    def findTotalWhoNeedsToBuild(self, depQue, whoNeedsBuild):
         while not depQue.empty():
             specPkg = depQue.get()
-            specName = self.getSpecName(specPkg)
-            spec = Specutils(self.getSpecFile(specPkg))
-            RPMName = spec.getRPMName(specPkg)
-            debuginfoRPMName = spec.getDebuginfoRPMName(specPkg)
-            whoBuildDepSet.add(RPMName)
-            whoBuildDepSet.add(debuginfoRPMName)
-            if specName is None:
-                print(specPkg + " is missing")
-            if specPkg not in whoBuildDeps:
-                continue
-            for depPkg in whoBuildDeps[specPkg]:
-                depQue.put(depPkg)
+            listPackagesRequiredToBuild = SPECS.getData().getBuildRequiresForPackage(specPkg)
+            for depPkg in listPackagesRequiredToBuild:
+                depSpecPkg = SPECS.getData().getSpecName(depPkg)
+                if depSpecPkg not in whoNeedsBuild:
+                    whoNeedsBuild.append(depSpecPkg)
+                    depQue.put(depSpecPkg)
 
-    def printTree(self, allDeps, children, curParent, depth):
+    def printTree(self, children, curParent, depth):
         if curParent in children:
             for child in children[curParent]:
-                print("\t" * depth + child)
-                self.printTree(allDeps, children, child, depth + 1)
+                print ("\t" * depth + child)
+                self.printTree(children, child, depth + 1)
 
-    def get_all_package_names(self, jsonFilePath):
-        base_path = os.path.dirname(jsonFilePath)
+    def getAllPackageNames(self, jsonFilePath):
         jsonData = open(jsonFilePath)
         option_list_json = json.load(jsonData)
         jsonData.close()
         packages = option_list_json["packages"]
         return packages
 
-    def updateLevels(self, allDeps, inPkg, parent, level):
-        specName = self.getSpecName(inPkg)
-        specObj = self.mapSerializableSpecObjects[specName]
-        for depPkg in specObj.installRequiresPackages[inPkg]:
-            # ignore circular deps within single spec file
-            if (depPkg in specObj.installRequiresPackages and
-                    inPkg in specObj.installRequiresPackages[depPkg] and
-                    self.getSpecName(depPkg) == specName):
+    def updateLevels(self, mapDependencies, inPkg, parent, level):
+        listPackages = SPECS.getData().getPackages(inPkg)
+        for depPkg in SPECS.getData().getRequiresForPackage(inPkg):
+            if depPkg in listPackages:
                 continue
-            if depPkg in allDeps and allDeps[depPkg] < level + 1:
-                allDeps[depPkg] = level + 1
+            if depPkg in mapDependencies and mapDependencies[depPkg] < level + 1:
+                mapDependencies[depPkg] = level + 1
                 parent[depPkg] = inPkg
-                self.updateLevels(allDeps, depPkg, parent, allDeps[depPkg])
+                self.updateLevels(mapDependencies, depPkg, parent, mapDependencies[depPkg])
 
-    def readSpecsAndConvertToSerializableObjects(self, specFilesPath, inputType,
-                                                 inputValue, displayOption):
-        children = {}
-        listSpecFiles = []
-        whoNeedsList = []
-        whoBuildDepSet = set()
-        # list of all RPMS not built from photon and that must be blindly copied.
-        independentRPMS = []
-        whoBuildDeps = {}
-        allDeps = {}
-        parent = {}
+    def calculateSpecDependency(self, inputPackages, mapDependencies, parent):
         depQue = queue.Queue()
-        packageFound = False
-        self.getListSpecFiles(listSpecFiles, specFilesPath)
-        for specFile in listSpecFiles:
-            spec = Specutils(specFile)
-            specName = spec.getBasePackageName()
-            specObj = SerializableSpecObject()
-            specObj.name = specName
-            specObj.buildRequirePackages = spec.getBuildRequiresAllPackages()
-            specObj.installRequiresAllPackages = spec.getRequiresAllPackages()
-            specObj.listPackages = spec.getPackageNames()
-            specObj.specFile = specFile
-            specObj.version = spec.getVersion()
-            specObj.release = spec.getRelease()
-            specObj.listSources = spec.getSourceNames()
-            specObj.listPatches = spec.getPatchNames()
-            specObj.securityHardening = spec.getSecurityHardeningOption()
-            for specPkg in specObj.listPackages:
-                specObj.installRequiresPackages[specPkg] = spec.getRequires(specPkg)
-                if inputType == "pkg" and inputValue == specPkg:
-                # all the first level dependencies to a dictionary and queue
-                    packageFound = True
-                    for depPkg in specObj.installRequiresPackages[specPkg]:
-                        if depPkg not in allDeps:
-                            allDeps[depPkg] = 0
-                            parent[depPkg] = ""
-                            depQue.put(depPkg)
-                elif (inputType == "who-needs" and
-                      inputValue in specObj.installRequiresPackages[specPkg]):
-                    whoNeedsList.append(specPkg)
-                elif inputType == "who-needs-build":
-                    for bdrq in specObj.buildRequirePackages:
-                        if bdrq in whoBuildDeps:
-                            whoBuildDeps[bdrq].add(specPkg)
-                        else:
-                            whoBuildDeps[bdrq] = set()
-                            whoBuildDeps[bdrq].add(specPkg)
-                    if inputValue == specPkg:
-                        packageFound = True
-                        for depPkg in specObj.listPackages:
-                            depQue.put(depPkg)
-
-                self.mapPackageToSpec[specPkg] = specName
-            self.mapSerializableSpecObjects[specName] = specObj
-
-        # Generate dependencies for individual packages
-        if inputType == "pkg":
-            if packageFound == True:
-                self.findTotalRequires(allDeps, depQue, parent, displayOption)
+        for pkg in inputPackages:
+            if SPECS.getData().isRPMPackage(pkg):
+                if pkg not in mapDependencies:
+                    mapDependencies[pkg] = 0
+                    parent[pkg] = ""
+                    depQue.put(pkg)
+                    self.findTotalRequires(mapDependencies, depQue, parent)
             else:
-                print("No spec file builds a package named {}".format(inputValue))
-                return
+                print("Could not find spec for "+pkg)
 
-        # Generate dependencies for all packages in the given JSON input file
-        elif inputType == "json":
-            filePath = self.inputDataDir + "/" + inputValue
-            data = self.get_all_package_names(filePath)
-            for pkg in data:
-                if pkg not in allDeps:
-                    spName = self.getSpecName(pkg)
-                    if spName is not None:
-                        allDeps[pkg] = 0
-                        parent[pkg] = ""
-                        depQue.put(pkg)
-                        self.findTotalRequires(allDeps, depQue, parent, displayOption)
-                    else:
-                        independentRPMS.append(pkg)
-
-        #Generating the list of packages that requires the given input package at install time
-        elif inputType == "who-needs":
-            print(whoNeedsList)
-            return
-
-        #Generating the list of packages that the modified package will affect at build time
-        elif inputType == "who-needs-build":
-            if packageFound == True:
-                self.findTotalWhoNeedsToBuild(depQue, whoBuildDeps, whoBuildDepSet, displayOption)
-                print(whoBuildDepSet)
-            else:
-                print("No spec file builds a package named {}".format(inputValue))
-            return
-
-        # construct the sorted list of all packages (sorted by dependency)
+    def displayDependencies(self, displayOption, inputType, inputValue, allDeps, parent):
+        children = {}
         sortedList = []
         for elem in sorted(allDeps.items(), key=operator.itemgetter(1), reverse=True):
             sortedList.append(elem[0])
-        sortedList.extend(independentRPMS)
-
         # construct all children nodes
         if displayOption == "tree":
             for k, v in parent.iteritems():
@@ -456,95 +363,52 @@ class SerializedSpecObjects(object):
             if "" in children:
                 for child in children[""]:
                     print(child)
-                    self.printTree(allDeps, children, child, 1)
-                for pkg in independentRPMS:
-                    print(pkg)
+                    self.printTree(children, child, 1)
                 print("*" * 18 + " {} ".format(len(sortedList)) +
                       "packages in total " + "*" * 18)
             else:
                 if inputType == "pkg" and len(children) > 0:
-                    print("cyclic dependency detected, mappings:")
-                    print(children)
+                    print ("cyclic dependency detected, mappings: \n", children)
 
         # To display a flat list of all packages
         elif displayOption == "list":
-            print(sortedList)
+            print (sortedList)
 
         # To generate a new JSON file based on given input json file
         elif displayOption == "json" and inputType == "json":
-            d = {}
-            d['packages'] = sortedList
-            outFilePath = self.jsonFilesOutPath + inputValue
-            with open(outFilePath, 'w') as outfile:
+            d = {'packages': sortedList}
+            with open(inputValue, 'w') as outfile:
                 json.dump(d, outfile)
+
         return sortedList
 
-    def getListSpecFiles(self, listSpecFiles, path):
-        for dirEntry in os.listdir(path):
-            dirEntryPath = os.path.join(path, dirEntry)
-            if (os.path.isfile(dirEntryPath) and
-                    dirEntryPath.endswith(".spec") and
-                    os.path.basename(dirEntryPath) not in
-                    constants.skipSpecsForArch.get(platform.machine(), [])):
-                listSpecFiles.append(dirEntryPath)
-            elif os.path.isdir(dirEntryPath):
-                self.getListSpecFiles(listSpecFiles, dirEntryPath)
-
-    def getBuildRequiresForPackage(self, package):
-        specName = self.getSpecName(package)
-        return self.mapSerializableSpecObjects[specName].buildRequirePackages
-
-    def getRequiresForPackage(self, package):
-        specName = self.getSpecName(package)
-        if package in self.mapSerializableSpecObjects[specName].installRequiresPackages:
-            return self.mapSerializableSpecObjects[specName].installRequiresPackages[package]
-        return None
-
-    def getRelease(self, package):
-        specName = self.getSpecName(package)
-        return self.mapSerializableSpecObjects[specName].release
-
-    def getVersion(self, package):
-        specName = self.getSpecName(package)
-        return self.mapSerializableSpecObjects[specName].version
-
-    def getSpecFile(self, package):
-        specName = self.getSpecName(package)
-        return self.mapSerializableSpecObjects[specName].specFile
-
-    def getPatches(self, package):
-        specName = self.getSpecName(package)
-        return self.mapSerializableSpecObjects[specName].listPatches
-
-    def getSources(self, package):
-        specName = self.getSpecName(package)
-        return self.mapSerializableSpecObjects[specName].listSources
-
-    def getPackages(self, package):
-        specName = self.getSpecName(package)
-        return self.mapSerializableSpecObjects[specName].listPackages
-
-    def getSpecName(self, package):
-        if package in self.mapPackageToSpec:
-            specName = self.mapPackageToSpec[package]
-            if specName in self.mapSerializableSpecObjects:
-                return specName
+    def process(self, inputType, inputValue, displayOption, outputFile=None):
+        whoNeedsList = []
+        inputPackages = []
+        whoNeedsBuild = []
+        mapDependencies = {}
+        parent = {}
+        if inputType == "pkg" or inputType == "json":
+            if inputType == "pkg":
+                inputPackages.append(inputValue)
             else:
-                print("SpecDeps: Could not able to find " + package + " package from specs")
-                raise Exception("Invalid package:" + package)
-        else:
-            return None
+                inputPackages = self.getAllPackageNames(inputValue)
 
-    def isRPMPackage(self, package):
-        if package in self.mapPackageToSpec:
-            specName = self.mapPackageToSpec[package]
-        if specName in self.mapSerializableSpecObjects:
-            return True
-        return False
-
-    def getSecurityHardeningOption(self, package):
-        specName = self.getSpecName(package)
-        return self.mapSerializableSpecObjects[specName].securityHardening
-
-    def getSpecDetails(self, name):
-        print(self.mapSerializableSpecObjects[name].installRequiresAllPackages)
+            self.calculateSpecDependency(inputPackages, mapDependencies, parent)
+            if outputFile is not None:
+                return self.displayDependencies(displayOption, inputType, outputFile, mapDependencies, parent)
+            else:
+                return self.displayDependencies(displayOption, inputType, inputValue, mapDependencies, parent)
+        elif inputType == "who-needs":
+            for depPkg in SPECS.getData().mapPackageToSpec:
+                if inputValue in SPECS.getData().getRequiresForPackage(depPkg):
+                    whoNeedsList.append(depPkg)
+            print (whoNeedsList)
+            return whoNeedsList
+        elif inputType == "who-needs-build":
+            depQue = queue.Queue()
+            depQue.put(inputValue)
+            self.findTotalWhoNeedsToBuild(depQue, whoNeedsBuild)
+            print ("Following specs need to be build again")
+            print (whoNeedsBuild)
+            return whoNeedsBuild
