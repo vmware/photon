97b59b74b12a8c39cb4109fbf692e1505fb5155b
Srivatsa S. Bhat <srivatsa@csail.mit.edu>
2018-02-02 03:18:51 -0800

    tools / update-script: Use slightly better wording
    Change-Id: I25f8522e12dd5882811dd0b2a3168842c0e0e345

--- tools/update_linux.sh
+++ tools/update_linux.sh
@@ -14,5 +14,5 @@ for spec in $specs; do
 	sed -i '/^Version:/ s/4.9.[0-9]*/'$version'/' SPECS/$spec
 	sed -i '/^Release:/ s/[0-9]*%/1%/' SPECS/$spec
 	sed -i '/^%define sha1 linux/ s/=[0-9a-f]*$/='$sha1'/' SPECS/$spec
-	sed -i '/^%changelog/a*   '"$changelog_entry"'\n-   Update version to '"$version"'' SPECS/$spec
+	sed -i '/^%changelog/a*   '"$changelog_entry"'\n-   Update to version '"$version"'' SPECS/$spec
 done
