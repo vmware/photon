044ea03754023034f6610f6a7ab97d94f56be339
DheerajSShetty <dheerajs@vmware.com>
2018-07-11 15:27:18 -0700

    Add kubernetes-metrics-server
     * Create kubernetes container images and rpm for metrics-server
    
    Change-Id: If50871e1efbfa604cea9baa6a958d7a1b73b80ca
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/5343
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Sharath George
    (cherry picked from commit 994e0f7a71d7d133990a7e09230fa28331b54548)

---
+++ support/dockerfiles/k8s-docker-images/build-k8s-metrics-server-image.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+DIST_TAG=$1
+DIST_VER=$2
+SPEC_DIR=$3
+STAGE_DIR=$4
+ARCH=x86_64
+
+#
+# Docker images for kubernetes-metrics-server
+#
+K8S_MET_SERV_VER=`cat ${SPEC_DIR}/kubernetes-metrics-server/kubernetes-metrics-server.spec | grep Version | cut -d: -f2 | tr -d ' '`
+K8S_MET_SERV_VER_REL=${K8S_MET_SERV_VER}-`cat ${SPEC_DIR}/kubernetes-metrics-server/kubernetes-metrics-server.spec | grep Release | cut -d: -f2 | tr -d ' ' | cut -d% -f1`
+K8S_MET_SERV_RPM=kubernetes-metrics-server-${K8S_MET_SERV_VER_REL}${DIST_TAG}.${ARCH}.rpm
+K8S_MET_SERV_RPM_FILE=${STAGE_DIR}/RPMS/x86_64/${K8S_MET_SERV_RPM}
+K8S_MET_SERV_TAR=kubernetes-metrics-server-v${K8S_MET_SERV_VER_REL}.tar
+
+if [ ! -f ${K8S_MET_SERV_RPM_FILE} ]
+then
+    echo "Kubernetes Metrics Server RPM ${K8S_MET_SERV_RPM_FILE} not found. Exiting.."
+    exit 1
+fi
+
+IMG_NAME=vmware/photon-${DIST_VER}-kubernetes-metrics-server-amd64:v${K8S_MET_SERV_VER}
+
+IMG_ID=`docker images -q ${IMG_NAME} 2> /dev/null`
+if [[ ! -z "${IMG_ID}" ]]; then
+    echo "Removing image ${IMG_NAME}"
+    docker rmi -f ${IMG_NAME}
+fi
+
+mkdir -p tmp/k8smetserv
+cp ${K8S_MET_SERV_RPM_FILE} tmp/k8smetserv/
+pushd ./tmp/k8smetserv
+rpm2cpio ${K8S_MET_SERV_RPM} | cpio -vid
+popd
+
+docker build --rm -t ${IMG_NAME} -f ./Dockerfile.metrics-server .
+docker save -o ${K8S_MET_SERV_TAR} ${IMG_NAME}
+gzip ${K8S_MET_SERV_TAR}
+mv -f ${K8S_MET_SERV_TAR}.gz ${STAGE_DIR}/docker_images/
+
+rm -rf ./tmp
