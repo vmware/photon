d8445469c77608b523ebb40a47d402050043cf8d
akaher <akaher@vmware.com>
2018-10-10 13:24:32 +0000

    arch64: adding rpi3 b, b+ firmware files
    Change-Id: I872b0123ea33d25b7e81077b3eb405409b75699e
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/5902
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Alexey Makhalov <amakhalov@vmware.com>

--- SPECS/linux-firmware/generate-linux-firmware-tarball.sh
+++ SPECS/linux-firmware/generate-linux-firmware-tarball.sh
@@ -5,14 +5,25 @@ DATE_TAG=`date "+%Y%m%d"`
 DST=linux-firmware-$DATE_TAG
 mkdir $DST
 
+git clone https://github.com/RPi-Distro/firmware-nonfree.git --depth=1
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git --depth=1
 
 cp linux-firmware/WHENCE $DST/
 
 mkdir $DST/brcm
-cp linux-firmware/brcm/brcmfmac43430-sdio.bin $DST/brcm/
-cp linux-firmware/LICENCE.broadcom_bcm43xx $DST/
+# rpi3 b requires:
+cp firmware-nonfree/brcm/brcmfmac43430-sdio.bin $DST/brcm/
+cp firmware-nonfree/brcm/brcmfmac43430-sdio.txt $DST/brcm/
 
+# rpi3 b+ requires:
+cp firmware-nonfree/brcm/brcmfmac43455-sdio.clm_blob $DST/brcm/
+cp firmware-nonfree/brcm/brcmfmac43455-sdio.bin $DST/brcm/
+cp firmware-nonfree/brcm/brcmfmac43455-sdio.txt $DST/brcm/
+
+# rpi3 licence
+cp firmware-nonfree/LICENCE.broadcom_bcm43xx $DST/
+
+# Dell Edge Gateway requires:
 cp -a linux-firmware/rsi $DST/
 cp linux-firmware/rsi_91x.fw $DST/
 
