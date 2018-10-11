7268925ef556a3b67be02c0e13f9974513e65705
Alexey Makhalov <amakhalov@vmware.com>
2018-10-11 12:50:20 -0700

    Package builder cleanup
    1. Move packageWeights from build infra to package builder.
    2. Cleanup PullSources class.
    3. Moved openssl fips logic into spec files.
    4. Redesigned package build options logic.
    5. Incremental build - do full build iff HEAD commit touches support folder.
    6. Makefile: replaces spaces by tabs
    
    Change-Id: I1b16bd8b3636841f367d3b11e39ea016119b948a

--- support/package-builder/constants.py
+++ support/package-builder/constants.py
@@ -10,7 +10,8 @@ class constants(object):
     buildRootPath = "/mnt"
     prevPublishRPMRepo = ""
     prevPublishXRPMRepo = ""
-    pullsourcesConfig = ""
+    pullsourcesURL = ""
+    extrasourcesURLs = []
     buildPatch = False
     inputRPMSPath = ""
     rpmCheck = False
@@ -537,8 +538,19 @@ class constants(object):
         constants.buildRootPath = buildRootPath
 
     @staticmethod
-    def setPullSourcesConfig(pullSourcesConfig):
-        constants.pullsourcesConfig = pullSourcesConfig
+    def setPullSourcesURL(url):
+        constants.pullsourcesURL = url
+
+    @staticmethod
+    def setExtraSourcesURLs(urls):
+        constants.extrasourcesURLs = urls
+
+    @staticmethod
+    def getPullSourcesURLs():
+        urls=[]
+        urls.append(constants.pullsourcesURL)
+        urls.extend(constants.extrasourcesURLs)
+        return urls
 
     @staticmethod
     def setInputRPMSPath(inputRPMSPath):
@@ -611,3 +623,13 @@ class constants(object):
     @staticmethod
     def setBuildOptions(options):
         constants.buildOptions = options
+
+    @staticmethod
+    def getAdditionalMacros(package):
+        macros = {}
+        if package in constants.buildOptions.keys():
+            pkg = constants.buildOptions[package]
+            for m in pkg["macros"]:
+                k, v = m.split(' ', 1)
+            macros[k] = v
+        return macros
