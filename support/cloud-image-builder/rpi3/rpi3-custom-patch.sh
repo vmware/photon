bbcfca57b9347ed52439fffdde317160c7c74e43
Alexey Makhalov <amakhalov@vmware.com>
2018-10-25 18:40:01 -0700

    rpi3 image: use photon-repos rpm
    Change-Id: If0e7f811c436088b4cc3c0ef9191cf9eda20c6d3

--- support/cloud-image-builder/rpi3/rpi3-custom-patch.sh
+++ support/cloud-image-builder/rpi3/rpi3-custom-patch.sh
@@ -3,8 +3,6 @@
 echo -e "changeme\nchangeme" | passwd root
 chage -d 0 -M 999 root
 
-(cd /etc/yum.repos.d; rm `ls | grep -v photon-dev`)
-
 echo "/dev/mmcblk0p3 none swap defaults 0 0" >> /etc/fstab
 
 chmod +x /usr/local/bin/resizefs.sh
