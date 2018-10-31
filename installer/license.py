62bf0273d9ee95caa365f25b6e55a17354c91bd5
suezzelur <anishs@vmware.com>
2018-10-31 15:54:36 -0700

    Update splash screen for 3.0
    Change-Id: I48e8587e3dea8591ea5cfcc175648df899ac0a9b

--- installer/license.py
+++ installer/license.py
@@ -28,7 +28,7 @@ class License(object):
         accept_decline_items = [('<Accept>', self.accept_function),
                                 ('<Cancel>', self.exit_function)]
 
-        title = 'VMWARE 2.0 LICENSE AGREEMENT'
+        title = 'VMWARE 3.0 LICENSE AGREEMENT'
         self.window.addstr(0, (self.win_width - len(title)) // 2, title)
         self.text_pane = TextPane(self.text_starty, self.maxx, self.text_width,
                                   "EULA.txt", self.text_height, accept_decline_items)
