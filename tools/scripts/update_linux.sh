80509734ff78cbaea205e4c164076babd162e8f3
Srivatsa S. Bhat (VMware) <srivatsa@csail.mit.edu>
2018-11-05 17:15:14 -0800

    linux: Update to version 4.19.1
    This is a major kernel version update from 4.18 to 4.19 LTS.
    
    While at it, make a few cosmetic improvements to the rdrand-rng
    driver:
    
     - Make the error message more informative -- i.e., make it known that
       the unavailability of RDSEED/RDRAND is tied to the underlying CPU,
       and is not a kernel bug.
    
     - Also, log the message using pr_info() rather than pr_err(), because
       it is not an error as such -- systems with CPUs that don't support
       RDSEED/RDRAND cannot load this driver, and that is fine.
    
    Change-Id: I06786750ede3977ec94eec03647ed499692d597f

--- tools/scripts/update_linux.sh
+++ tools/scripts/update_linux.sh
@@ -2,7 +2,7 @@
 
 specs="linux-api-headers/linux-api-headers.spec linux/linux.spec linux/linux-esx.spec linux/linux-secure.spec linux/linux-aws.spec"
 
-tarball_url=`curl -s https://www.kernel.org  | grep -Eo 'https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.14.[0-9]*.tar.xz'`
+tarball_url=`curl -s https://www.kernel.org  | grep -Eo 'https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.[0-9]*.tar.xz' | uniq`
 tarball=$(basename $tarball_url)
 version=`echo $tarball | sed 's/linux-//; s/.tar.xz//'`
 echo latest linux version: $version
@@ -11,7 +11,7 @@ $(cd stage/SOURCES && wget $tarball_url)
 sha1=`sha1sum stage/SOURCES/$tarball | awk '{print $1}'`
 changelog_entry=$(echo "`date +"%a %b %d %Y"` `git config user.name` <`git config user.email`> $version-1")
 for spec in $specs; do
-	sed -i '/^Version:/ s/4.14.[0-9]*/'$version'/' SPECS/$spec
+	sed -i '/^Version:/ s/4.19.[0-9]*/'$version'/' SPECS/$spec
 	sed -i '/^Release:/ s/[0-9]*%/1%/' SPECS/$spec
 	sed -i '/^%define sha1 linux/ s/=[0-9a-f]*$/='$sha1'/' SPECS/$spec
 	sed -i '/^%changelog/a*   '"$changelog_entry"'\n-   Update to version '"$version"'' SPECS/$spec
