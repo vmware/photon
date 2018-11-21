25b5d3184f8505a601016e2e47376965573663a6
srinidhira0 <srinidhir@vmware.com>
2018-11-21 08:58:09 +0000

    nxp:ls1012a : Add support to build image for NXP board
    This changelist will create a compressed raw image as required
    by NXP ls1012a ARM-64 based board.
    This image can be flashed to a SD Card and boot the
    NXP ls1012a board.
    
    Change-Id: I49be98df8ea45e891f60707f13e5cbc8100a7c0c
    Signed-off-by: srinidhira0 <srinidhir@vmware.com>

--- SPECS/linux-firmware/generate-linux-firmware-tarball.sh
+++ SPECS/linux-firmware/generate-linux-firmware-tarball.sh
@@ -6,6 +6,7 @@ DST=linux-firmware-$DATE_TAG
 mkdir $DST
 
 git clone https://github.com/RPi-Distro/firmware-nonfree.git --depth=1
+git clone https://github.com/NXP/qoriq-engine-pfe-bin.git --depth=1
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git --depth=1
 
 cp linux-firmware/WHENCE $DST/
@@ -26,5 +27,10 @@ cp firmware-nonfree/LICENCE.broadcom_bcm43xx $DST/
 # Dell Edge Gateway requires:
 cp -a linux-firmware/rsi $DST/
 cp linux-firmware/rsi_91x.fw $DST/
+
+# NXP ls1012a FRWY requires:
+mkdir $DST/ls1012a_ppe/
+cp qoriq-engine-pfe-bin/ls1012a/slow_path/ppfe_class_ls1012a.elf $DST/ls1012a_ppe/
+cp qoriq-engine-pfe-bin/ls1012a/slow_path/ppfe_tmu_ls1012a.elf $DST/ls1012a_ppe/
 
 tar -czvf $DST.tar.gz $DST
