143c263e1be66f0ae6929bb52e09769efcec87fa
srinidhira0 <srinidhir@vmware.com>
2018-11-21 08:58:09 +0000

    nxp:ls1012a : Add support to build image for NXP board
    This changelist will create a compressed raw image as required
    by NXP ls1012a ARM-64 based board.
    This image can be flashed to a SD Card and boot the
    NXP ls1012a board.
    
    Change-Id: I49be98df8ea45e891f60707f13e5cbc8100a7c0c
    Signed-off-by: srinidhira0 <srinidhir@vmware.com>

---
+++ support/image-builder/ls1012afrwy/resizefs.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+parted /dev/mmcblk0 resizepart 2 90%
+parted /dev/mmcblk0 mkpart primary linux-swap 90% 100%
+partprobe
+resize2fs /dev/mmcblk0p2
+mkswap /dev/mmcblk0p3
+#swapon /dev/mmcblk0p4
