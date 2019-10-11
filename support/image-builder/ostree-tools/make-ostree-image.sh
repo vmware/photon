7a7f0c892b5814c66815450ec03003d3bc6a09c7
Ankit Jain <ankitja@vmware.com>
2019-10-11 22:45:37 +0530

    ostree: Moved build dependency from installer to image-builder
    - Removed unnecessary mk-ostree-server.sh
    - Moved build related files to image-builder
    
    Change-Id: I35de2e21971f9819dd110ec9f16ea0e00d020d4f
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/8218
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Alexey Makhalov <amakhalov@vmware.com>

--- support/image-builder/ostree-tools/make-ostree-image.sh
+++ support/image-builder/ostree-tools/make-ostree-image.sh
@@ -11,18 +11,29 @@ fi
 PROGRAM=$0
 SRCROOT=$1
 
-createrepo stage/RPMS
+cat > ${SRCROOT}/support/image-builder/ostree-tools/mk-ostree-server.sh << EOF
+#!/bin/bash
 
-cp ${SRCROOT}/installer/photon-ostree.repo ${SRCROOT}/installer/photon-ostree.repo.bak
-echo "baseurl=file:///RPMS" >> ${SRCROOT}/installer/photon-ostree.repo
+ROOT=$1
+
+mkdir -p ${ROOT}/srv/rpm-ostree
+ostree --repo=${ROOT}/srv/rpm-ostree/repo init --mode=archive-z2
+rpm-ostree compose tree --repo=${ROOT}/srv/rpm-ostree/repo photon-base.json
+EOF
+
+chmod +x ${SRCROOT}/support/image-builder/ostree-tools/mk-ostree-server.sh
+
+cp ${SRCROOT}/support/image-builder/ostree-tools/photon-ostree.repo ${SRCROOT}/support/image-builder/ostree-tools/photon-ostree.repo.bak
+echo "baseurl=file:///RPMS" >> ${SRCROOT}/support/image-builder/ostree-tools/photon-ostree.repo
 
 rm -rf stage/ostree-repo
 mkdir -p stage/ostree-repo
 
-sudo docker run -it --privileged -v ${SRCROOT}:/photon -v $(pwd)/stage/RPMS:/RPMS -v $(pwd)/stage/ostree-repo:/srv/rpm-ostree -w="/photon/installer"  ankitaj/photon-build:rpm-ostree-3.0 ./mk-ostree-server.sh /
+sudo docker run -it --privileged -v ${SRCROOT}:/photon -v $(pwd)/stage/RPMS:/RPMS -v $(pwd)/stage/ostree-repo:/srv/rpm-ostree -w="/photon/support/image-builder/ostree-tools/" ankitaj/photon-build:rpm-ostree-3.0 ./mk-ostree-server.sh /
 
 (cd stage/ostree-repo/repo/; tar -zcf ../../ostree-repo.tar.gz .; )
 
 # Restore file
-mv -f ${SRCROOT}/installer/photon-ostree.repo.bak ${SRCROOT}/installer/photon-ostree.repo
+mv -f ${SRCROOT}/support/image-builder/ostree-tools/photon-ostree.repo.bak ${SRCROOT}/support/image-builder/ostree-tools/photon-ostree.repo
+sudo rm ${SRCROOT}/support/image-builder/ostree-tools/mk-ostree-server.sh
 sudo rm -rf ${SRCROOT}/stage/ostree-repo
