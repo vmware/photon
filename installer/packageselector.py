vmware/photon

commit 8e9dba48759ce984898b55fd557524f320cf27aa

minimal-iso: Creating photon minimal-iso
-  Addded a new target 'minimal-iso' in Makefile
  * This generates photon-minimal-*.iso and corresponding debug iso
  * Size of minimal-iso is 263MB and size of debug iso is 1166M
- minimal-iso skips photon flavour selection(edge, developer and ostree) UI

Change-Id: I945d74e13c0fbca700cfd6a18467fdac42b68f87
Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/8403
Reviewed-by: Alexey Makhalov <amakhalov@vmware.com>
Tested-by: Anish Swaminathan <anishs@vmware.com>

installer/packageselector.py
@@ -48,6 +48,11 @@ class PackageSelector(object):
         base_path = os.path.dirname(options_file)
         package_list = []
 
+        if len(options_sorted) == 1:
+            if 'packagelist_file' not in self.install_config:
+                self.install_config['packagelist_file'] = list(options_sorted)[0][1]['packagelist_file']
+                list(options_sorted)[0][1]['visible'] = True
+
         default_selected = 0
         visible_options_cnt = 0
         for install_option in options_sorted:
@@ -77,4 +82,7 @@ class PackageSelector(object):
         return ActionResult(True, {'custom': True})
 
     def display(self):
+        if 'packagelist_file' in self.install_config:
+            return ActionResult(None, {"inactive_screen": True})
+
         return self.window.do_action()

Makefile
@@ -30,6 +30,7 @@ PHOTON_SOURCES ?= sources
 endif
 
 FULL_PACKAGE_LIST_FILE := build_install_options_all.json
+MINIMAL_PACKAGE_LIST_FILE := build_install_options_minimal.json
 
 ifdef PHOTON_PUBLISH_RPMS_PATH
 PHOTON_PUBLISH_RPMS := publish-rpms-cached
@@ -117,6 +118,29 @@ packages-minimal: check-tools photon-stage $(PHOTON_PUBLISH_RPMS) $(PHOTON_SOURC
 		--source-path $(PHOTON_SRCS_DIR) \
 		--build-root-path $(PHOTON_CHROOT_PATH) \
 		--packages-json-input $(PHOTON_DATA_DIR)/packages_minimal.json \
+		--log-path $(PHOTON_LOGS_DIR) \
+		--log-level $(LOGLEVEL) \
+		--publish-RPMS-path $(PHOTON_PUBLISH_RPMS_DIR) \
+		--pullsources-config $(PHOTON_PULLSOURCES_CONFIG) \
+		--dist-tag $(PHOTON_DIST_TAG) \
+		--build-number $(PHOTON_BUILD_NUMBER) \
+		--release-version $(PHOTON_RELEASE_VERSION) \
+		--pkginfo-file $(PHOTON_PKGINFO_FILE) \
+		$(PHOTON_RPMCHECK_FLAGS) \
+		$(PUBLISH_BUILD_DEPENDENCIES) \
+		$(PACKAGE_WEIGHTS) \
+		--threads ${THREADS}
+
+packages-initrd: check-tools photon-stage $(PHOTON_PUBLISH_RPMS) $(PHOTON_SOURCES) generate-dep-lists
+	@echo "Building all initrd package RPMS..."
+	@echo ""
+	@cd $(PHOTON_PKG_BUILDER_DIR) && \
+	$(PHOTON_PACKAGE_BUILDER) \
+		--spec-path $(PHOTON_SPECS_DIR) \
+		--rpm-path $(PHOTON_RPMS_DIR) \
+		--source-path $(PHOTON_SRCS_DIR) \
+		--build-root-path $(PHOTON_CHROOT_PATH) \
+		--packages-json-input $(PHOTON_DATA_DIR)/packages_installer_initrd.json \
 		--log-path $(PHOTON_LOGS_DIR) \
 		--log-level $(LOGLEVEL) \
 		--publish-RPMS-path $(PHOTON_PUBLISH_RPMS_DIR) \
@@ -316,9 +340,26 @@ iso: check-tools photon-stage $(PHOTON_PACKAGES) ostree-repo
 		--log-level $(LOGLEVEL) \
 		--rpm-path $(PHOTON_STAGE)/RPMS \
 		--srpm-path $(PHOTON_STAGE)/SRPMS \
-		--package-list-file $(PHOTON_GENERATED_DATA_DIR)/$(FULL_PACKAGE_LIST_FILE) \
+		--package-list-file $(PHOTON_DATA_DIR)/$(FULL_PACKAGE_LIST_FILE) \
 		--generated-data-path $(PHOTON_STAGE)/common/data \
 		--pkg-to-rpm-map-file $(PHOTON_PKGINFO_FILE)
+
+minimal-iso: check-tools photon-stage $(PHOTON_PUBLISH_XRPMS) packages-minimal packages-initrd
+	@echo "Building Photon Minimal ISO..."
+	@$(PHOTON_REPO_TOOL) $(PHOTON_RPMS_DIR)
+	@$(CP) -f $(PHOTON_DATA_DIR)/$(MINIMAL_PACKAGE_LIST_FILE) $(PHOTON_GENERATED_DATA_DIR)/
+	@cd $(PHOTON_IMAGE_BUILDER_DIR) && \
+	sudo $(PHOTON_IMAGE_BUILDER) \
+                --iso-path $(PHOTON_STAGE)/photon-minimal-$(PHOTON_RELEASE_VERSION)-$(PHOTON_BUILD_NUM).iso \
+                --debug-iso-path $(PHOTON_STAGE)/photon-minimal-$(PHOTON_RELEASE_VERSION)-$(PHOTON_BUILD_NUMBER).debug.iso \
+                --log-path $(PHOTON_STAGE)/LOGS \
+                --log-level $(LOGLEVEL) \
+                --rpm-path $(PHOTON_STAGE)/RPMS \
+                --srpm-path $(PHOTON_STAGE)/SRPMS \
+                --package-list-file $(PHOTON_DATA_DIR)/$(MINIMAL_PACKAGE_LIST_FILE) \
+                --generated-data-path $(PHOTON_STAGE)/common/data \
+                --pkg-to-rpm-map-file $(PHOTON_PKGINFO_FILE) \
+                --pkg-to-be-copied-conf-file $(PHOTON_GENERATED_DATA_DIR)/$(MINIMAL_PACKAGE_LIST_FILE)
 
 src-iso: check-tools photon-stage $(PHOTON_PACKAGES)
 	@echo "Building Photon Full Source ISO..."

installer/installer.py
@@ -16,6 +16,7 @@ import modules.commons
 import random
 import curses
 import stat
+import re
 from logger import Logger
 from commandutils import CommandUtils
 from jsonwrapper import JsonWrapper
@@ -745,6 +746,10 @@ class Installer(object):
                 regex = re.compile(r'(?!linux-[0-9].*)')
                 self.install_config['packages'] = list(filter(regex.match,self.install_config['packages']))
             self.install_config['packages'].append('linux-esx')
+        else:
+            regex = re.compile(r'(?!linux-esx-[0-9].*)')
+            self.install_config['packages'] = list(filter(regex.match,self.install_config['packages']))
+
 
     def _add_packages_to_install(self, package):
         """

support/image-builder/imagebuilder.py
@@ -34,7 +34,7 @@ def create_pkg_list_to_copy_to_iso(build_install_option, output_data_path):
     packages = []
     for install_option in options_sorted:
         if install_option[0] != "iso":
-            file_path = os.path.join(output_data_path, install_option[1]["file"])
+            file_path = os.path.join(output_data_path, install_option[1]["packagelist_file"])
             package_list_json = Utils.jsonread(file_path)
             packages = packages + package_list_json["packages"]
     return packages
@@ -120,12 +120,15 @@ def createIso(options):
             create_additional_file_list_to_copy_in_iso(
                 os.path.abspath(options.stage_path), options.package_list_file))
 
+        initrd_pkg_list_file = os.path.join(os.path.dirname(options.package_list_file), 'packages_installer_initrd.json')
+        initrd_pkgs = " ".join(Utils.jsonread(initrd_pkg_list_file)["packages"])
+
         retval = subprocess.call([script_directory + '/iso/mk-install-iso.sh',
                                   options.installer_path,
                                   working_directory, options.iso_path,
                                   options.rpm_path, options.package_list_file,
                                   rpm_list, options.stage_path, files_to_copy,
-                                  options.generated_data_path])
+                                  options.generated_data_path, initrd_pkgs])
 
         if retval:
             raise Exception("Unable to create install ISO")

support/image-builder/iso/mk-install-iso.sh
@@ -30,6 +30,7 @@ OUTPUT_DATA_PATH=$7
 PHOTON_COMMON_DIR=$(dirname "${PACKAGE_LIST_FILE}")
 PACKAGE_LIST_FILE_BASE_NAME=$(basename "${PACKAGE_LIST_FILE}")
 INITRD=${WORKINGDIR}/photon-chroot
+PACKAGES=$8
 
 rm -rf $WORKINGDIR/*
 mkdir -p $INITRD
@@ -57,14 +58,6 @@ repodir=${WORKINGDIR}
 EOF
 
 rpm --root $INITRD --initdb --dbpath /var/lib/rpm
-
-PACKAGES="filesystem glibc zlib file gmp libgcc libstdc++ bash sed haveged ncurses-terminfo \
-    bzip2 pkg-config python3-curses ncurses cracklib cracklib-dicts python3-cracklib \
-    shadow coreutils grep readline findutils xz util-linux e2fsprogs \
-    libffi expat linux cpio Linux-PAM attr libcap systemd dbus \
-    gzip sqlite nspr nss popt lua rpm gptfdisk tar \
-    hawkey python3 python3-libs pcre glib tdnf python3-requests grub2 \
-    grub2-pc grub2-efi efivar efibootmgr dracut curl dosfstools ostree ostree-grub2 ostree-libs lvm2"
 
 TDNF_CMD="tdnf install -y --installroot $INITRD --rpmverbosity 10 -c ${WORKINGDIR}/tdnf.conf -q $PACKAGES"
 

commit d8b217ebf4f3f46645d93fe45a72667597ba868f

installer: fix build_install_options with single item
Commit 8e9dba48759ce984898b55fd557524f320cf27aa
("minimal-iso: Creating photon minimal-iso") introduced
regression. It broke build_install_options with "ostree"
and "packages" items, since it honors menu items with
"packageslist_file" only by using presence of this field
as a flag for inactive screen. But "packageslist_file"
might not always exist - considering "ostree" and
"packages" options.
Fix it by introducing dedicated inactive_screen variable
and using exit_function() for proper install_config
initialization.

Change-Id: Ia50004a03d6faf4456652e399742a989bea99247
Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/8630
Tested-by: gerrit-photon <photon-checkins@vmware.com>
Reviewed-by: Anish Swaminathan <anishs@vmware.com>

installer/packageselector.py
@@ -11,6 +11,7 @@ from actionresult import ActionResult
 class PackageSelector(object):
     def __init__(self, maxy, maxx, install_config, options_file):
         self.install_config = install_config
+        self.inactive_screen = False
         self.maxx = maxx
         self.maxy = maxy
         self.win_width = 50
@@ -23,7 +24,8 @@ class PackageSelector(object):
 
         self.load_package_list(options_file)
 
-        self.window = Window(self.win_height, self.win_width, self.maxy, self.maxx,
+        if not self.inactive_screen:
+            self.window = Window(self.win_height, self.win_width, self.maxy, self.maxx,
                              'Select Installation', True, action_panel=self.package_menu,
                              can_go_next=True, position=1)
 
@@ -49,9 +51,8 @@ class PackageSelector(object):
         package_list = []
 
         if len(options_sorted) == 1:
-            if 'packagelist_file' not in self.install_config:
-                self.install_config['packagelist_file'] = list(options_sorted)[0][1]['packagelist_file']
-                list(options_sorted)[0][1]['visible'] = True
+            self.inactive_screen = True
+            list(options_sorted)[0][1]['visible'] = True
 
         default_selected = 0
         visible_options_cnt = 0
@@ -67,8 +68,11 @@ class PackageSelector(object):
                 visible_options_cnt = visible_options_cnt + 1
 
 
-        self.package_menu = Menu(self.menu_starty, self.maxx, self.package_menu_items,
-                                 default_selected=default_selected, tab_enable=False)
+        if self.inactive_screen:
+            self.exit_function(self.package_menu_items[0][2])
+        else:
+            self.package_menu = Menu(self.menu_starty, self.maxx, self.package_menu_items,
+                                     default_selected=default_selected, tab_enable=False)
 
     def exit_function(self, selected_item_params):
         if selected_item_params[0] == 'ostree_host':
@@ -82,7 +86,7 @@ class PackageSelector(object):
         return ActionResult(True, {'custom': True})
 
     def display(self):
-        if 'packagelist_file' in self.install_config:
+        if self.inactive_screen:
             return ActionResult(None, {"inactive_screen": True})
 
         return self.window.do_action()