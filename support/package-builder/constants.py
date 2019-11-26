vmware/photon

commit 1c5c18c9612065d6662a79ecf0aeeb9e0225b467

perf: Fix issues with interactive UI and C++ symbol demangling
When perf is used with its call-graph recording feature (-g option),
perf report is supposed to fold the callchain by default and let the
user expand the (interesting parts of the) call-graph interactively.
However this is not working as expected; instead perf report expands
the complete call-graph by default, making it rather unwieldy. Fix
this behavior by building perf with slang-devel.

perf is also not demangling C++ symbol information at the moment; fix
it by building perf with binutils-devel. perf also needs to build with
elfutils-libelf-devel to be able to resolve symbols; so add it to the
BuildRequires list.

perf also needs the libbfd shared library from binutils; so separate
out libbfd and libopcodes shared libraries into a sub-package called
binutils-libs, so as to avoid having perf (linux-tools) take a runtime
dependency on the binutils main package.

perf also needs libunwind support for demangling; so use it to build
perf, and also fix the libunwind-devel package by adding a 'Requires'
dependency on its base package.

While at it, update the last few of the Linux kernel's perf python
scripts to use the python3 syntax, so that linux-tools can avoid a
dependency on python2.

Change-Id: Ic0b29fddb3fd687c9165280a1373e0101138a2a7
Signed-off-by: Srivatsa S. Bhat (VMware) <srivatsa@csail.mit.edu>
Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/8124
Tested-by: gerrit-photon <photon-checkins@vmware.com>
Reviewed-by: Anish Swaminathan <anishs@vmware.com>
Reviewed-by: Alexey Makhalov <amakhalov@vmware.com>

support/package-builder/constants.py
@@ -51,6 +51,7 @@ class constants(object):
         "zlib",
         "file",
         "binutils",
+        "binutils-libs",
         "gmp",
         "mpfr",
         "mpc",
@@ -68,6 +69,7 @@ class constants(object):
         "zlib",
         "file",
         "binutils",
+        "binutils-libs",
         "gmp",
         "mpfr",
         "mpc",
@@ -134,6 +136,7 @@ class constants(object):
         "file-libs",
         "file",
         "binutils",
+        "binutils-libs",
         "binutils-devel",
         "gmp",
         "gmp-devel",

SPECS/binutils/binutils.spec
@@ -1,12 +1,14 @@
 Summary:        Contains a linker, an assembler, and other tools
 Name:           binutils
 Version:        2.32
-Release:        1%{?dist}
+Release:        2%{?dist}
 License:        GPLv2+
 URL:            http://www.gnu.org/software/binutils
 Group:          System Environment/Base
 Vendor:         VMware, Inc.
 Distribution:   Photon
+Requires:       %{name}-libs = %{version}-%{release}
+
 Source0:        http://ftp.gnu.org/gnu/binutils/%{name}-%{version}.tar.xz
 %define sha1 binutils=cd45a512af1c8a508976c1beb4f5825b3bb89f4d
 
@@ -24,6 +26,14 @@ Patch9:         binutils-CVE-2019-1010204.patch
 %description
 The Binutils package contains a linker, an assembler,
 and other tools for handling object files.
+
+%package    libs
+Summary:    Shared library files for binutils
+Obsoletes:  binutils <= 2.32-1
+
+%description    libs
+It contains the binutils shared libraries that applications can link
+to at runtime.
 
 %package    devel
 Summary:    Header and development files for binutils
@@ -69,8 +79,6 @@ sed -i 's/testsuite/ /g' gold/Makefile
 make %{?_smp_mflags} check
 
 
-%post   -p /sbin/ldconfig
-%postun -p /sbin/ldconfig
 %files -f %{name}.lang
 %defattr(-,root,root)
 %{_bindir}/dwp
@@ -110,6 +118,11 @@ make %{?_smp_mflags} check
 %{_mandir}/man1/windres.1.gz
 %{_mandir}/man1/size.1.gz
 %{_mandir}/man1/objdump.1.gz
+
+%post   libs -p /sbin/ldconfig
+%postun libs -p /sbin/ldconfig
+
+%files libs
 %{_libdir}/libbfd-%{version}.so
 %{_libdir}/libopcodes-%{version}.so
 
@@ -130,6 +143,9 @@ make %{?_smp_mflags} check
 %{_lib64dir}/libiberty.a
 
 %changelog
+*   Sun Sep 29 2019 Srivatsa S. Bhat (VMware) <srivatsa@csail.mit.edu> 2.32-2
+-   Separate out libbfd and libopcodes shared libraries into
+-   binutils-libs sub-package.
 *   Mon Aug 26 2019 Satya Naga Vasamsetty <svasamsetty@vmware.com> 2.32-1
 -   Update version to 2.32, fix CVE-2019-1010204, fix a make check failure
 *   Mon Aug 12 2019 Satya Naga Vasamsetty <svasamsetty@vmware.com> 2.31.1-6

SPECS/libunwind/libunwind.spec
@@ -1,7 +1,7 @@
 Summary:	Portable and efficient C programming interface (API) to determine the call-chain of a program.
 Name:		libunwind
 Version:	1.2
-Release:	2%{?dist}
+Release:	3%{?dist}
 License:	X11
 URL:		http://www.nongnu.org/libunwind/
 Source0:	http://download.savannah.gnu.org/releases/%{name}/%{name}-%{version}.tar.gz
@@ -16,6 +16,7 @@ Portable and efficient C programming interface (API) to determine the call-chain
 %package devel
 Summary:        libunwind devel
 Group:          Development/Tools
+Requires:       %{name} = %{version}-%{release}
 %description devel
 This contains development tools and libraries for libunwind.
 
@@ -43,6 +44,8 @@ find %{buildroot} -name '*.la' -delete
 %{_libdir}/pkgconfig/libunwind*
 
 %changelog
+*   Sun Sep 29 2019 Srivatsa S. Bhat (VMware) <srivatsa@csail.mit.edu> 1.2-3
+-   libunwind-devel needs libunwind.
 *   Fri Oct 13 2017 Alexey Makhalov <amakhalov@vmware.com> 1.2-2
 -   Use standard configure macros
 *   Mon Feb 06 2017 Vinay Kulkarni <kulkarniv@vmware.com> 1.2-1

SPECS/linux/linux.spec
@@ -2,7 +2,7 @@
 Summary:        Kernel
 Name:           linux
 Version:        4.19.76
-Release:        1%{?kat_build:.%kat_build}%{?dist}
+Release:        2%{?kat_build:.%kat_build}%{?dist}
 License:    	GPLv2
 URL:        	http://www.kernel.org/
 Group:        	System Environment/Kernel
@@ -30,6 +30,7 @@ Patch4:         SUNRPC-xs_bind-uses-ip_local_reserved_ports.patch
 Patch5:         vsock-transport-for-9p.patch
 Patch6:         4.18-x86-vmware-STA-support.patch
 Patch7:	        9p-trans_fd-extend-port-variable-to-u32.patch
+Patch8:         perf-scripts-python-Convert-python2-scripts-to-python3.patch
 # ttyXRUSB support
 Patch11:	usb-acm-exclude-exar-usb-serial-ports.patch
 #HyperV patches
@@ -123,7 +124,13 @@ BuildRequires:  libmspack-devel
 BuildRequires:  Linux-PAM-devel
 BuildRequires:  openssl-devel
 BuildRequires:  procps-ng-devel
-BuildRequires:	audit-devel
+BuildRequires:  audit-devel
+BuildRequires:  elfutils-libelf-devel
+BuildRequires:  binutils-devel
+BuildRequires:  xz-devel
+BuildRequires:  libunwind-devel
+BuildRequires:  slang-devel
+BuildRequires:  python3-devel
 Requires:       filesystem kmod
 Requires(post):(coreutils or toybox)
 Requires(postun):(coreutils or toybox)
@@ -176,7 +183,7 @@ Kernel driver for oprofile, a statistical profiler for Linux systems
 Summary:        This package contains the 'perf' performance analysis tools for Linux kernel
 Group:          System/Tools
 Requires:       (%{name} = %{version} or linux-esx = %{version} or linux-aws = %{version})
-Requires:       audit
+Requires:       audit elfutils-libelf binutils-libs xz-libs libunwind slang python3
 Obsoletes:      linux-aws-tools <= 4.19.52-1
 Provides:       linux-aws-tools
 %description tools
@@ -213,6 +220,7 @@ Kernel Device Tree Blob files for NXP FRWY ls1012a and ls1046a boards
 %patch5 -p1
 %patch6 -p1
 %patch7 -p1
+%patch8 -p1
 %patch11 -p1
 %patch13 -p1
 %patch24 -p1
@@ -294,7 +302,7 @@ sed -i 's/CONFIG_LOCALVERSION=""/CONFIG_LOCALVERSION="-%{release}"/' .config
 %include %{SOURCE7}
 
 make VERBOSE=1 KBUILD_BUILD_VERSION="1-photon" KBUILD_BUILD_HOST="photon" ARCH=${arch} %{?_smp_mflags}
-make -C tools perf
+make -C tools perf PYTHON=python3
 %ifarch x86_64
 # build ENA module
 bldroot=`pwd`
@@ -427,7 +435,7 @@ cp arch/arm64/kernel/module.lds %{buildroot}/usr/src/%{name}-headers-%{uname_r}/
 # disable (JOBS=1) parallel build to fix this issue:
 # fixdep: error opening depfile: ./.plugin_cfg80211.o.d: No such file or directory
 # Linux version that was affected is 4.4.26
-make -C tools JOBS=1 DESTDIR=%{buildroot} prefix=%{_prefix} perf_install
+make -C tools JOBS=1 DESTDIR=%{buildroot} prefix=%{_prefix} perf_install PYTHON=python3
 
 %include %{SOURCE2}
 %include %{SOURCE6}
@@ -517,6 +525,10 @@ ln -sf %{name}-%{uname_r}.cfg /boot/photon.cfg
 %endif
 
 %changelog
+*   Thu Oct 10 2019 Srivatsa S. Bhat (VMware) <srivatsa@csail.mit.edu> 4.19.76-2
+-   Add additional BuildRequires and Requires to fix issues with perf, related to
+-   interactive UI and C++ symbol demangling. Also update the last few perf python
+-   scripts in Linux kernel to use python3 syntax.
 *   Wed Oct 02 2019 Ajay Kaher <akaher@vmware.com> 4.19.76-1
 -   Update to version 4.19.76
 -   Enable USB_SERIAL_PL2303 for aarch64

SPECS/linux/perf-scripts-python-Convert-python2-scripts-to-python3.patch
@@ -1,0 +1,118 @@
+From fa9cb2d572f7e83bce4ab5e27e40b76f36b97cad Mon Sep 17 00:00:00 2001
+From: "Srivatsa S. Bhat (VMware)" <srivatsa@csail.mit.edu>
+Date: Sun, 29 Sep 2019 23:07:25 -0700
+Subject: [PATCH] perf scripts python: Convert python2 scripts to python3
+ syntax
+
+Signed-off-by: Srivatsa S. Bhat (VMware) <srivatsa@csail.mit.edu>
+---
+ tools/perf/scripts/python/call-graph-from-sql.py  |  4 ++--
+ tools/perf/scripts/python/export-to-postgresql.py | 24 +++++++++++------------
+ 2 files changed, 14 insertions(+), 14 deletions(-)
+
+diff --git a/tools/perf/scripts/python/call-graph-from-sql.py b/tools/perf/scripts/python/call-graph-from-sql.py
+index b494a67..fcc160e 100644
+--- a/tools/perf/scripts/python/call-graph-from-sql.py
++++ b/tools/perf/scripts/python/call-graph-from-sql.py
+@@ -1,4 +1,4 @@
+-#!/usr/bin/python2
++#!/usr/bin/python3
+ # call-graph-from-sql.py: create call-graph from sql database
+ # Copyright (c) 2014-2017, Intel Corporation.
+ #
+@@ -292,7 +292,7 @@ class MainWindow(QMainWindow):
+ 
+ if __name__ == '__main__':
+ 	if (len(sys.argv) < 2):
+-		print >> sys.stderr, "Usage is: call-graph-from-sql.py <database name>"
++		print("Usage is: call-graph-from-sql.py <database name>", file=sys.stderr)
+ 		raise Exception("Too few arguments")
+ 
+ 	dbname = sys.argv[1]
+diff --git a/tools/perf/scripts/python/export-to-postgresql.py b/tools/perf/scripts/python/export-to-postgresql.py
+index e46f51b..5aa2ecb 100644
+--- a/tools/perf/scripts/python/export-to-postgresql.py
++++ b/tools/perf/scripts/python/export-to-postgresql.py
+@@ -236,10 +236,10 @@ perf_db_export_callchains = False
+ 
+ 
+ def usage():
+-	print >> sys.stderr, "Usage is: export-to-postgresql.py <database name> [<columns>] [<calls>] [<callchains>]"
+-	print >> sys.stderr, "where:	columns		'all' or 'branches'"
+-	print >> sys.stderr, "		calls		'calls' => create calls and call_paths table"
+-	print >> sys.stderr, "		callchains	'callchains' => create call_paths table"
++	print("Usage is: export-to-postgresql.py <database name> [<columns>] [<calls>] [<callchains>]", file=sys.stderr)
++	print("where:	columns		'all' or 'branches'", file=sys.stderr)
++	print("		calls		'calls' => create calls and call_paths table", file=sys.stderr)
++	print("		callchains	'callchains' => create call_paths table", file=sys.stderr)
+ 	raise Exception("Too few arguments")
+ 
+ if (len(sys.argv) < 2):
+@@ -273,7 +273,7 @@ def do_query(q, s):
+ 		return
+ 	raise Exception("Query failed: " + q.lastError().text())
+ 
+-print datetime.datetime.today(), "Creating database..."
++print(datetime.datetime.today(), "Creating database...")
+ 
+ db = QSqlDatabase.addDatabase('QPSQL')
+ query = QSqlQuery(db)
+@@ -564,7 +564,7 @@ if perf_db_export_calls:
+ 	call_file		= open_output_file("call_table.bin")
+ 
+ def trace_begin():
+-	print datetime.datetime.today(), "Writing to intermediate files..."
++	print(datetime.datetime.today(), "Writing to intermediate files...")
+ 	# id == 0 means unknown.  It is easier to create records for them than replace the zeroes with NULLs
+ 	evsel_table(0, "unknown")
+ 	machine_table(0, 0, "unknown")
+@@ -579,7 +579,7 @@ def trace_begin():
+ unhandled_count = 0
+ 
+ def trace_end():
+-	print datetime.datetime.today(), "Copying to database..."
++	print(datetime.datetime.today(), "Copying to database...")
+ 	copy_output_file(evsel_file,		"selected_events")
+ 	copy_output_file(machine_file,		"machines")
+ 	copy_output_file(thread_file,		"threads")
+@@ -594,7 +594,7 @@ def trace_end():
+ 	if perf_db_export_calls:
+ 		copy_output_file(call_file,		"calls")
+ 
+-	print datetime.datetime.today(), "Removing intermediate files..."
++	print(datetime.datetime.today(), "Removing intermediate files...")
+ 	remove_output_file(evsel_file)
+ 	remove_output_file(machine_file)
+ 	remove_output_file(thread_file)
+@@ -609,7 +609,7 @@ def trace_end():
+ 	if perf_db_export_calls:
+ 		remove_output_file(call_file)
+ 	os.rmdir(output_dir_name)
+-	print datetime.datetime.today(), "Adding primary keys"
++	print(datetime.datetime.today(), "Adding primary keys")
+ 	do_query(query, 'ALTER TABLE selected_events ADD PRIMARY KEY (id)')
+ 	do_query(query, 'ALTER TABLE machines        ADD PRIMARY KEY (id)')
+ 	do_query(query, 'ALTER TABLE threads         ADD PRIMARY KEY (id)')
+@@ -624,7 +624,7 @@ def trace_end():
+ 	if perf_db_export_calls:
+ 		do_query(query, 'ALTER TABLE calls           ADD PRIMARY KEY (id)')
+ 
+-	print datetime.datetime.today(), "Adding foreign keys"
++	print(datetime.datetime.today(), "Adding foreign keys")
+ 	do_query(query, 'ALTER TABLE threads '
+ 					'ADD CONSTRAINT machinefk  FOREIGN KEY (machine_id)   REFERENCES machines   (id),'
+ 					'ADD CONSTRAINT processfk  FOREIGN KEY (process_id)   REFERENCES threads    (id)')
+@@ -659,8 +659,8 @@ def trace_end():
+ 		do_query(query, 'CREATE INDEX pcpid_idx ON calls (parent_call_path_id)')
+ 
+ 	if (unhandled_count):
+-		print datetime.datetime.today(), "Warning: ", unhandled_count, " unhandled events"
+-	print datetime.datetime.today(), "Done"
++		print(datetime.datetime.today(), "Warning: ", unhandled_count, " unhandled events")
++	print(datetime.datetime.today(), "Done")
+ 
+ def trace_unhandled(event_name, context, event_fields_dict):
+ 	global unhandled_count
+-- 
+2.7.4
+

support/pullpublishrpms/rpmfilelist-aarch64
@@ -1,6 +1,7 @@
 aarch64/bash-4.4-5.ph2.aarch64.rpm
-aarch64/binutils-2.29-3.ph2.aarch64.rpm
-aarch64/binutils-devel-2.29-3.ph2.aarch64.rpm
+aarch64/binutils-2.32-2.ph3.aarch64.rpm
+aarch64/binutils-devel-2.32-2.ph3.aarch64.rpm
+aarch64/binutils-libs-2.32-2.ph3.aarch64.rpm
 aarch64/bison-3.0.4-3.ph2.aarch64.rpm
 aarch64/bzip2-1.0.6-8.ph2.aarch64.rpm
 aarch64/bzip2-devel-1.0.6-8.ph2.aarch64.rpm
@@ -24,10 +25,10 @@ aarch64/gawk-4.1.4-2.ph3.aarch64.rpm
 aarch64/gcc-7.3.0-2.ph3.aarch64.rpm
 aarch64/gdbm-1.13-3.ph2.aarch64.rpm
 aarch64/gettext-0.19.8-1.ph2.aarch64.rpm
-aarch64/glibc-2.26-4.ph2.aarch64.rpm
-aarch64/glibc-devel-2.26-4.ph2.aarch64.rpm
-aarch64/glibc-iconv-2.26-4.ph2.aarch64.rpm
-aarch64/glibc-tools-2.26-4.ph2.aarch64.rpm
+aarch64/glibc-2.28-3.ph3.aarch64.rpm
+aarch64/glibc-devel-2.28-3.ph3.aarch64.rpm
+aarch64/glibc-iconv-2.28-3.ph3.aarch64.rpm
+aarch64/glibc-tools-2.28-3.ph3.aarch64.rpm
 aarch64/gmp-6.1.2-3.ph3.aarch64.rpm
 aarch64/gmp-devel-6.1.2-3.ph3.aarch64.rpm
 aarch64/go-1.8.1-2.ph2.aarch64.rpm

support/pullpublishrpms/rpmfilelist-x86_64
@@ -5,7 +5,7 @@ x86_64/python2-devel-2.7.11-1.ph1.x86_64.rpm
 x86_64/nspr-4.11-1.ph1.x86_64.rpm
 x86_64/rpm-4.13.0.1-5.ph2.x86_64.rpm
 x86_64/rpm-libs-4.13.0.1-5.ph2.x86_64.rpm
-x86_64/binutils-devel-2.30-2.ph1.x86_64.rpm
+x86_64/binutils-devel-2.31-9.ph1.x86_64.rpm
 x86_64/nss-3.21-1.ph1.x86_64.rpm
 x86_64/mpfr-devel-4.0.1-1.ph2.x86_64.rpm
 x86_64/cpio-2.12-1.ph1.x86_64.rpm
@@ -62,7 +62,8 @@ x86_64/python2-2.7.11-1.ph1.x86_64.rpm
 x86_64/elfutils-devel-0.165-1.ph1.x86_64.rpm
 x86_64/patch-2.7.5-1.ph1.x86_64.rpm
 x86_64/xz-5.2.2-1.ph1.x86_64.rpm
-x86_64/binutils-2.30-2.ph1.x86_64.rpm
+x86_64/binutils-2.31-9.ph1.x86_64.rpm
+x86_64/binutils-libs-2.31-9.ph1.x86_64.rpm
 x86_64/libgomp-devel-7.3.0-2.ph2.x86_64.rpm
 x86_64/bzip2-devel-1.0.6-4.ph1.x86_64.rpm
 x86_64/libgcc-7.3.0-2.ph2.x86_64.rpm

commit 573fdf2fe41c2278a89c6afdfb2d5bcd63ae52e6

package-builder: do not build binutils twice
Commit 1c5c18c9612065d6662a79ecf0aeeb9e0225b467
("perf: Fix issues with interactive UI and C++ symbol demangling")
introduced regression where binutils package got built twice as
part of Step 1:

 Building all RPMS...

 skipping spec file: /root/photon/SPECS/openjdk8/openjdk8_aarch64.spec
 skipping spec file: /root/photon/SPECS/u-boot/u-boot.spec
 Step 1 : Building the core toolchain packages.....
 ['filesystem', 'linux-api-headers', 'glibc', 'zlib', 'file', 'binutils',
     'binutils-libs', 'gmp', 'mpfr', 'mpc', 'gcc', 'pkg-config', 'ncurses',
     'readline', 'bash']

 The following core toolchain packages need to be built :
 ['binutils', 'binutils-libs']
 Building package : binutils
 binutils build done - RPMs : [ binutils-2.32-3.ph3.x86_64.rpm
     binutils-libs-2.32-3.ph3.x86_64.rpm binutils-devel-2.32-3.ph3.x86_64.rpm
     binutils-debuginfo-2.32-3.ph3.x86_64.rpm ]

 Building package : binutils-libs
 ...
 <building binutils rpm second time here !!>
 ...

Remove binutils-libs from the core toolchain lists as just binutils is
sufficient.

Change-Id: I7f5f66bb90176230d4058af745ac1242470f1a2f
Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/8650
Tested-by: gerrit-photon <photon-checkins@vmware.com>
Reviewed-by: Anish Swaminathan <anishs@vmware.com>

support/package-builder/constants.py
@@ -44,6 +44,7 @@ class constants(object):
         "sqlite-libs"]
 
     # These packages will be built in first order as build-core-toolchain stage
+    # Put only main pakage names here. Do not add subpackages such as libgcc
     listCoreToolChainPackages = [
         "filesystem",
         "linux-api-headers",
@@ -51,7 +52,6 @@ class constants(object):
         "zlib",
         "file",
         "binutils",
-        "binutils-libs",
         "gmp",
         "mpfr",
         "mpc",
@@ -62,6 +62,7 @@ class constants(object):
         "bash"]
 
     # These packages will be built in a second stage to replace publish RPMS
+    # Put only main pakage names here. Do not add subpackages such as libgcc
     listToolChainPackages = [
         "filesystem",
         "linux-api-headers",
@@ -69,7 +70,6 @@ class constants(object):
         "zlib",
         "file",
         "binutils",
-        "binutils-libs",
         "gmp",
         "mpfr",
         "mpc",