vmware/photon

commit 23a5d5280cca27e6efcc07503765f859b52c903f

Installer: add imagegenerator postactions
It allows imagegenerator to do not execute any postactions by
simply specifying thme in installer config section.

Added extra steps to postinstall module.
Added machineid and locale modules.

expirepassword moved to installer->password->age field
as an integer value.

Removed mknod creation in /dev

Fix remote repo=

Change-Id: I76f947f8770a3e87012843f2e1371ef775cd951f
Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/8245
Tested-by: michellew <michellew@vmware.com>
Reviewed-by: Anish Swaminathan <anishs@vmware.com>

installer/modules/m_updaterootpassword.py
@@ -27,3 +27,18 @@ def execute(installer):
 
     installer.cmd.run_in_chroot(installer.photon_root, "/usr/sbin/pwconv")
     installer.cmd.run_in_chroot(installer.photon_root, "/usr/sbin/grpconv")
+
+    if 'age' in installer.install_config['password']:
+        age = installer.install_config['password']['age']
+        login_defs_filename = os.path.join(installer.photon_root, 'etc/login.defs')
+
+        # Do not run 'chroot -R' from outside. It will not find nscd socket.
+        if age == -1:
+            installer.cmd.run_in_chroot(installer.photon_root, "chage -I -1 -m 0 -M 99999 -E -1 -W 7 root")
+            commons.replace_string_in_file(login_defs_filename, r'(PASS_MAX_DAYS)\s+\d+\s*', 'PASS_MAX_DAYS\t99999')
+        elif age == 0:
+            installer.cmd.run_in_chroot(installer.photon_root, "chage -d 0 root")
+        else:
+            installer.cmd.run_in_chroot(installer.photon_root, "chage -M {} root".format(age))
+            commons.replace_string_in_file(login_defs_filename, r'(PASS_MAX_DAYS)\s+\d+\s*', 'PASS_MAX_DAYS\t{}\n'.format(age))
+

Makefile
@@ -350,7 +350,7 @@ image: check-kpartx photon-stage $(VIXDISKUTIL) $(PHOTON_PACKAGES)
 		--config-file=$(CONFIG) \
 		--img-name=$(IMG_NAME) \
 		--src-root=$(SRCROOT) \
-		--generated-data-path=$(PHOTON_GENERATED_DATA_DIR) \
+		--generated-data-path=$(PHOTON_DATA_DIR) \
 		--stage-path=$(PHOTON_STAGE) \
 		--rpm-path $(PHOTON_STAGE)/RPMS
 
@@ -359,31 +359,31 @@ all-images: check-kpartx photon-stage $(VIXDISKUTIL) $(PHOTON_PACKAGES)
 	@cd $(PHOTON_IMAGE_BUILDER_DIR)
 	$(PHOTON_IMAGE_BUILDER) \
 		--src-root=$(SRCROOT) \
-		--generated-data-path=$(PHOTON_GENERATED_DATA_DIR) \
+		--generated-data-path=$(PHOTON_DATA_DIR) \
 		--stage-path=$(PHOTON_STAGE) \
 		--rpm-path $(PHOTON_STAGE)/RPMS \
 		--img-name=ami
 	$(PHOTON_IMAGE_BUILDER) \
 		--src-root=$(SRCROOT) \
-		--generated-data-path=$(PHOTON_GENERATED_DATA_DIR) \
+		--generated-data-path=$(PHOTON_DATA_DIR) \
 		--stage-path=$(PHOTON_STAGE) \
 		--rpm-path $(PHOTON_STAGE)/RPMS \
 		--img-name=gce
 	$(PHOTON_IMAGE_BUILDER) \
 		--src-root=$(SRCROOT) \
-		--generated-data-path=$(PHOTON_GENERATED_DATA_DIR) \
+		--generated-data-path=$(PHOTON_DATA_DIR) \
 		--stage-path=$(PHOTON_STAGE) \
 		--rpm-path $(PHOTON_STAGE)/RPMS \
 		--img-name=azure
 	$(PHOTON_IMAGE_BUILDER) \
 		--src-root=$(SRCROOT) \
-		--generated-data-path=$(PHOTON_GENERATED_DATA_DIR) \
+		--generated-data-path=$(PHOTON_DATA_DIR) \
 		--stage-path=$(PHOTON_STAGE) \
 		--rpm-path $(PHOTON_STAGE)/RPMS \
 		--img-name=ova
 	$(PHOTON_IMAGE_BUILDER) \
 		--src-root=$(SRCROOT) \
-		--generated-data-path=$(PHOTON_GENERATED_DATA_DIR) \
+		--generated-data-path=$(PHOTON_DATA_DIR) \
 		--stage-path=$(PHOTON_STAGE) \
 		--rpm-path $(PHOTON_STAGE)/RPMS \
 		--img-name=ova_uefi

installer/config.inc
@@ -1,17 +1,0 @@
-#	Editable variables follow
-BUILDROOT=/mnt/photon-root	# where to build photon
-#	Edit partition and mnt_point for the correct values.
-#	Failing to do so will cause you grief as in overwriting your host system.
-#	You have been warned!
-LOGFILE="$(date +%Y-%m-%d).log"	#	this is where we log the build to 
-HDD=/dev/sdb			# 	Hard drive to prepare for photon mount.
-PARTITION=/dev/sdb2			#	Partition to create filesystem for photon build
-SWAPPARTITION=/dev/sdb3			# 	Partition where swap partition is created
-
-#Check if the BUILDROOT parameter is passed
-if [[ $# > 1 ]]; then
-		if [[ "$1" =~ ^(-w|--working-directory)$ ]]; then
-				BUILDROOT="$2"
-				shift 2
-		fi
-fi

installer/function.inc
@@ -1,42 +1,0 @@
-fail() {	local _red="\\033[1;31m"
-	local _normal="\\033[0;39m"
-	[ -n "$*" ] && printf "${_red}$*${_normal}\n"
-	exit 1
-}
-print_message() {
-	printf "%s" "${1}"
-}
-print_failed_in_red() {
-	local _red="\\033[1;31m"
-	local _normal="\\033[0;39m"
-	printf "${_red}%s${_normal}\n" "FAILURE"
-	exit 2
-}
-print_succeeded_in_green() {
-	local _green="\\033[1;32m"
-	local _normal="\\033[0;39m"
-	printf "${_green}%s${_normal}\n" "SUCCESS"
-	return 0
-}
-run_command() {
-	# $1 = message 
-	# $2 = command
-	# $3 = log file
-	local _start=$(date +%s)
-	local _msg="${1}"
-	local _cmd="${2}"
-	local _logfile="${3}"
-	if [ "/dev/null" == "${_logfile}" ]; then
-		print_message "${_msg}: "
-		eval ${_cmd} >> ${_logfile} 2>&1 && print_succeeded_in_green || print_failed_in_red 
-	else
-		print_message "${_msg}: "
-		printf "\n%s\n\n" "###       ${_msg}       ###" >> ${_logfile} 2>&1
-		eval ${_cmd} >> ${_logfile} 2>&1 && print_succeeded_in_green || print_failed_in_red 
-		fi
-
-	local _end=$(date +%s)
-	local _elapsed=$((_end-_start))
-	echo "Elapsed time: ${_elapsed} seconds" >> ${_logfile} 2>&1
-	return 0
-}

installer/installer.py
@@ -37,6 +37,7 @@ class Installer(object):
     # List of allowed keys in kickstart config file.
     # Please keep ks_config.txt file updated.
     known_keys = {
+        'additional_files',
         'additional_packages',
         'additional_rpms_path',
         'arch',
@@ -55,7 +56,9 @@ class Installer(object):
         'partitions',
         'password',
         'postinstall',
+        'postinstallscripts',
         'public_key',
+        'search_path',
         'setup_grub_script',
         'shadow_password',
         'type',
@@ -181,15 +184,15 @@ class Installer(object):
         if "hostname" not in install_config or install_config['hostname'] == "":
             install_config['hostname'] = 'photon-%12x' % random.randrange(16**12)
 
-        # Set crypted password if needed
+        # Set password if needed
+        if 'password' not in install_config:
+            install_config['password'] = {'crypted': True, 'text': '*', 'age': -1}
+
         if 'shadow_password' not in install_config:
-            if 'password' in install_config:
-                if install_config['password']['crypted']:
-                    install_config['shadow_password'] = install_config['password']['text']
-                else:
-                    install_config['shadow_password'] = CommandUtils.generate_password_hash(install_config['password']['text'])
+            if install_config['password']['crypted']:
+                install_config['shadow_password'] = install_config['password']['text']
             else:
-                install_config['shadow_password'] = '*'
+                install_config['shadow_password'] = CommandUtils.generate_password_hash(install_config['password']['text'])
 
         # Do not show UI progress by default
         if 'ui' not in install_config:
@@ -198,6 +201,13 @@ class Installer(object):
         # Log level
         if 'log_level' not in install_config:
             install_config['log_level'] = 'info'
+
+        # Extend search_path by current dir and script dir
+        if 'search_path' not in install_config:
+            install_config['search_path'] = []
+        for dirname in [os.getcwd(), os.path.abspath(os.path.dirname(__file__))]:
+            if dirname not in install_config['search_path']:
+                install_config['search_path'].append(dirname)
 
     def _check_install_config(self, install_config):
         """
@@ -245,6 +255,10 @@ class Installer(object):
         # No BIOS for aarch64
         if install_config['arch'] == 'aarch64' and install_config['bootmode'] in ['dualboot', 'bios']:
             return "Aarch64 targets do not support BIOS boot. Set 'bootmode' to 'efi'."
+
+        if 'age' in install_config['password']:
+            if install_config['password']['age'] < -1:
+                return "Password age should be -1, 0 or positive"
 
         return None
 
@@ -344,7 +358,7 @@ class Installer(object):
         """
         Unmount partitions and special folders
         """
-        for d in ["/run", "/sys", "/dev/pts", "/dev", "/proc"]:
+        for d in ["/tmp", "/run", "/sys", "/dev/pts", "/dev", "/proc"]:
             retval = self.cmd.run(['umount', '-l', self.photon_root + d])
             if retval != 0:
                 self.logger.error("Failed to unmount {}".format(d))
@@ -562,27 +576,28 @@ class Installer(object):
                 self.logger.error("Failed to install filesystem rpm")
                 self.exit_gracefully()
 
-        # Create special devices (TODO: really need it?)
-        devices = {
-            'console': (600, stat.S_IFCHR, 5, 1),
-            'null': (666, stat.S_IFCHR, 1, 3),
-            'random': (444, stat.S_IFCHR, 1, 8),
-            'urandom': (444, stat.S_IFCHR, 1, 9)
-        }
-        for device, (mode, dev_type, major, minor) in devices.items():
-            os.mknod(os.path.join(self.photon_root, "dev", device),
-                    mode | dev_type, os.makedev(major, minor))
-
     def _mount_special_folders(self):
         for d in ["/proc", "/dev", "/dev/pts", "/sys"]:
             retval = self.cmd.run(['mount', '-o', 'bind', d, self.photon_root + d])
             if retval != 0:
                 self.logger.error("Failed to bind mount {}".format(d))
                 self.exit_gracefully()
-        retval = self.cmd.run(['mount', '-t', 'tmpfs', 'tmpfs', self.photon_root + "/run"])
-        if retval != 0:
-            self.logger.error("Failed to bind mount {}".format(d))
-            self.exit_gracefully()
+
+        for d in ["/tmp", "/run"]:
+            retval = self.cmd.run(['mount', '-t', 'tmpfs', 'tmpfs', self.photon_root + d])
+            if retval != 0:
+                self.logger.error("Failed to bind mount {}".format(d))
+                self.exit_gracefully()
+
+    def _copy_additional_files(self):
+        if 'additional_files' in self.install_config:
+            for filetuples in self.install_config['additional_files']:
+                for src, dest in filetuples.items():
+                    srcpath = self.getfile(src)
+                    if (os.path.isdir(srcpath)):
+                        shutil.copytree(srcpath, self.photon_root + dest, True)
+                    else:
+                        shutil.copyfile(srcpath, self.photon_root + dest)
 
     def _finalize_system(self):
         """
@@ -591,17 +606,12 @@ class Installer(object):
         if self.install_config['ui']:
             self.progress_bar.show_loading('Finalizing installation')
 
+        self._copy_additional_files()
+
         self.cmd.run_in_chroot(self.photon_root, "/sbin/ldconfig")
-        self.cmd.run_in_chroot(self.photon_root, "/bin/systemd-machine-id-setup")
+
         # Importing the pubkey
         self.cmd.run_in_chroot(self.photon_root, "rpm --import /etc/pki/rpm-gpg/*")
-        # Set locale
-        with open(os.path.join(self.photon_root, "etc/locale.conf"), "w") as locale_conf:
-            locale_conf.write("LANG=en_US.UTF-8\n")
-        #locale-gen.sh needs /usr/share/locale/locale.alias which is shipped with
-        #  glibc-lang rpm, in some photon installations glibc-lang rpm is not installed
-        #  by default. Call localedef directly here to define locale environment.
-        self.cmd.run_in_chroot(self.photon_root, "/usr/bin/localedef -c -i en_US -f UTF-8 en_US.UTF-8")
 
     def _cleanup_install_repo(self):
         self._unbind_installer()
@@ -730,7 +740,7 @@ class Installer(object):
             repo_file.write("[photon-local]\n")
             repo_file.write("name=VMWare Photon installer repo\n")
             if self.rpm_path.startswith("https://") or self.rpm_path.startswith("http://"):
-                repo_file.write("baseurl={}\n".format(self.rpm_path.replace('/', r'\/')))
+                repo_file.write("baseurl={}\n".format(self.rpm_path))
             else:
                 repo_file.write("baseurl=file://{}\n".format(self.rpm_cache_dir))
                 keepcache = True
@@ -1159,4 +1169,14 @@ class Installer(object):
                 raise Exception(
                     "Failed to format {} partition @ {}".format(partition['filesystem'],
                                                          partition['path']))
+
+    def getfile(self, filename):
+        """
+        Returns absolute filepath by filename.
+        """
+        for dirname in self.install_config['search_path']:
+            filepath = os.path.join(dirname, filename)
+            if os.path.exists(filepath):
+                return filepath
+        raise Exception("File {} not found in the following directories {}".format(filename, self.install_config['search_path']))
 

installer/modules/m_locale.py
@@ -1,0 +1,16 @@
+import os
+import commons
+
+install_phase = commons.POST_INSTALL
+enabled = True
+
+def execute(installer):
+    # Set locale
+    with open(os.path.join(installer.photon_root, "etc/locale.conf"), "w") as locale_conf:
+        locale_conf.write("LANG=en_US.UTF-8\n")
+
+    #locale-gen.sh needs /usr/share/locale/locale.alias which is shipped with
+    #  glibc-lang rpm, in some photon installations glibc-lang rpm is not installed
+    #  by default. Call localedef directly here to define locale environment.
+    installer.cmd.run_in_chroot(installer.photon_root, "/usr/bin/localedef -c -i en_US -f UTF-8 en_US.UTF-8")
+

installer/modules/m_machineid.py
@@ -1,0 +1,13 @@
+import commons
+
+install_phase = commons.POST_INSTALL
+enabled = True
+
+def execute(installer):
+    # Generage machine-id for live setup, but keep this file empty for images
+    # so it gets regenerated on boot.
+    if installer.install_config['live']:
+        installer.cmd.run_in_chroot(installer.photon_root, "/bin/systemd-machine-id-setup")
+    else:
+        open(installer.photon_root + "/etc/machine-id", "w").close()
+

installer/modules/m_postinstall.py
@@ -6,18 +6,32 @@ install_phase = commons.POST_INSTALL
 enabled = True
 
 def execute(installer):
-    if 'postinstall' not in installer.install_config:
+    if 'postinstall' not in installer.install_config or 'postinstallscripts' not in installer.install_config:
         return
 
-    installer.logger.info("Run postinstall script")
-    # run the script in the chroot environment
-    script = installer.install_config['postinstall']
+    tempdir = "/tmp/tempscripts"
+    tempdir_full = installer.photon_root + tempdir
+    if not os.path.exists(tempdir_full):
+        os.mkdir(tempdir_full)
 
-    script_file = os.path.join(installer.photon_root, 'etc/tmpfiles.d/postinstall.sh')
+    if 'postinstall' in installer.install_config:
+        installer.logger.info("Run postinstall script")
+        # run the script in the chroot environment
+        script = installer.install_config['postinstall']
 
-    with open(script_file, 'wb') as outfile:
-        outfile.write("\n".join(script).encode())
+        script_file = os.path.join(tempdir_full, 'builtin_postinstall.sh')
 
-    os.chmod(script_file, 0o700)
-    return installer.cmd.run_in_chroot(installer.photon_root,
-                                       "/etc/tmpfiles.d/postinstall.sh") == 0
+        with open(script_file, 'wb') as outfile:
+            outfile.write("\n".join(script).encode())
+        os.chmod(script_file, 0o700)
+
+    if 'postinstallscripts' in installer.install_config:
+        for scriptname in config['postinstallscripts']:
+            script_file = installer.getfile(scriptname)
+            shutil.copy(script_file, tempdir_full)
+
+    for script in os.listdir(tempdir_full):
+        installer.logger.info("Running script {}".format(script))
+        installer.cmd.run_in_chroot(installer.photon_root, "{}/{}".format(tempdir, script))
+
+    shutil.rmtree(tempdir_full, ignore_errors=True)

commit d4c8159d400de5c50a8a63956a669b329eeb2f5b

Installer: add missing \n to /etc/login.defs
commit 23a5d5280cca27e6efcc07503765f859b52c903f
("Installer: add imagegenerator postactions") introduced regression.

Logic of root password aging was moved from imagebuilder to
installer and improved to 3 values: -1, 0, N.
In case of -1 (no password aging) it generated "broken" login.defs
entry:

PASS_MAX_DAYS   99999PASS_MIN_DAYS      0

Adding \n after "PASS_MAX_DAYS   99999" fixes the problem.

Change-Id: Id32c58662ad7dcfca5f664fff2bcfad52ec7e190
Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/8749
Tested-by: gerrit-photon <photon-checkins@vmware.com>
Reviewed-by: Anish Swaminathan <anishs@vmware.com>

installer/modules/m_updaterootpassword.py
@@ -35,7 +35,7 @@ def execute(installer):
         # Do not run 'chroot -R' from outside. It will not find nscd socket.
         if age == -1:
             installer.cmd.run_in_chroot(installer.photon_root, "chage -I -1 -m 0 -M 99999 -E -1 -W 7 root")
-            commons.replace_string_in_file(login_defs_filename, r'(PASS_MAX_DAYS)\s+\d+\s*', 'PASS_MAX_DAYS\t99999')
+            commons.replace_string_in_file(login_defs_filename, r'(PASS_MAX_DAYS)\s+\d+\s*', 'PASS_MAX_DAYS\t99999\n')
         elif age == 0:
             installer.cmd.run_in_chroot(installer.photon_root, "chage -d 0 root")
         else: