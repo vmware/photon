2f1791857920e2b477fac61b226757f16c3eea2d
Tapas Kundu <tkundu@vmware.com>
2020-04-21 17:01:17 +0800

    openjdk11: Update to jdk11.0.7-ga
    Change-Id: I7e942876bffb7f7d3ef452038871207a242227d8
    Signed-off-by: Tapas Kundu <tkundu@vmware.com>
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/9599
    Reviewed-by: Srinidhi Rao <srinidhir@vmware.com>
    Tested-by: gerrit-photon <photon-checkins@vmware.com>

--- SPECS/openjdk11/generate_source_tarball_11.sh
+++ SPECS/openjdk11/generate_source_tarball_11.sh
@@ -2,20 +2,20 @@ tdnf install mercurial -y
 
 if [[ $# -eq 0 ]] ; then
     echo 'Usage: generate_source_tarball_11.sh <Mercurial-Tag-Name> <openjdk version>'
-    echo 'Example: generate_source_tarball_11.sh jdk-11+23 1.11.0.23'
-    echo 'visit http://hg.openjdk.java.net/jdk/jdk11/tags to use the appropriate tag name.'
+    echo 'Example: generate_source_tarball_11.sh jdk-11+23 11.0.23'
+    echo 'visit http://hg.openjdk.java.net/jdk-updates/jdk11u/tags to use the appropriate tag name.'
     exit 0
 fi
 rm -rf openjdk-$2
-hg clone http://hg.openjdk.java.net/jdk/jdk11 -u $1 openjdk-$2
+hg clone http://hg.openjdk.java.net/jdk-updates/jdk11u -u $1 openjdk-$2
 cd openjdk-$2
-hg clone http://hg.openjdk.java.net/jdk/jdk11/corba/ -u $1
-hg clone http://hg.openjdk.java.net/jdk/jdk11/hotspot/ -u $1
-hg clone http://hg.openjdk.java.net/jdk/jdk11/jaxp/ -u $1
-hg clone http://hg.openjdk.java.net/jdk/jdk11/jaxws/ -u $1
-hg clone http://hg.openjdk.java.net//jdk/jdk11/jdk/ -u $1
-hg clone http://hg.openjdk.java.net/jdk/jdk11/langtools/ -u $1
-hg clone http://hg.openjdk.java.net/jdk/jdk11/nashorn/ -u $1
+hg clone http://hg.openjdk.java.net/jdk-updates/jdk11u/corba/ -u $1
+hg clone http://hg.openjdk.java.net/jdk-updates/jdk11u/hotspot/ -u $1
+hg clone http://hg.openjdk.java.net/jdk-updates/jdk11u/jaxp/ -u $1
+hg clone http://hg.openjdk.java.net/jdk-updates/jdk11u/jaxws/ -u $1
+hg clone http://hg.openjdk.java.net/jdk-updates/jdk11u/jdk/ -u $1
+hg clone http://hg.openjdk.java.net/jdk-updates/jdk11u/langtools/ -u $1
+hg clone http://hg.openjdk.java.net/jdk-updates/jdk11u/nashorn/ -u $1
 
 rm -r .hg
 rm -r corba/.hg
