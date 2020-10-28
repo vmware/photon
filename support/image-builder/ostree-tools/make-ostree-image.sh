83e1692898bffafbffc4261c7d23c4a58cbf0113
Ankit Jain <ankitja@vmware.com>
2020-10-28 05:31:58 +0000

    image-builder: ostree-tools: Fix ostree-repo archive creation
    - Added a check if repo directory exist
    - Added -C option in tar to make sure it archive correct dir
    
    Change-Id: I60eeadbaa04853f0d05b3eab502dc3c7c9aa31f5
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/11593
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Anish Swaminathan <anishs@vmware.com>

--- support/image-builder/ostree-tools/make-ostree-image.sh
+++ support/image-builder/ostree-tools/make-ostree-image.sh
@@ -27,18 +27,21 @@ chmod +x ${SRCROOT}/support/image-builder/ostree-tools/mk-ostree-server.sh
 cp ${SRCROOT}/support/image-builder/ostree-tools/photon-ostree.repo ${SRCROOT}/support/image-builder/ostree-tools/photon-ostree.repo.bak
 echo "baseurl=file:///RPMS" >> ${SRCROOT}/support/image-builder/ostree-tools/photon-ostree.repo
 
-rm -rf stage/ostree-repo
-mkdir -p stage/ostree-repo
+rm -rf ${SRCROOT}/stage/ostree-repo
+mkdir -p ${SRCROOT}/stage/ostree-repo
 
 if [ $DOCK_ARCH == "x86_64" ]
 then
-  sudo docker run --privileged -v ${SRCROOT}:/photon -v $(pwd)/stage/RPMS:/RPMS -v $(pwd)/stage/ostree-repo:/srv/rpm-ostree -w="/photon/support/image-builder/ostree-tools/" vmware/photon-build:rpm-ostree-3.0 ./mk-ostree-server.sh /
+  sudo docker run --privileged -v ${SRCROOT}:/photon -v ${SRCROOT}/stage/RPMS:/RPMS -v ${SRCROOT}/stage/ostree-repo:/srv/rpm-ostree -w="/photon/support/image-builder/ostree-tools/" vmware/photon-build:rpm-ostree-3.0 ./mk-ostree-server.sh /
 elif [ $DOCK_ARCH == "aarch64" ]
 then
-  sudo docker run --privileged -v ${SRCROOT}:/photon -v $(pwd)/stage/RPMS:/RPMS -v $(pwd)/stage/ostree-repo:/srv/rpm-ostree -w="/photon/support/image-builder/ostree-tools/" vmware/photon-build:rpm-ostree-aarch64-3.0 ./mk-ostree-server.sh /
+  sudo docker run --privileged -v ${SRCROOT}:/photon -v ${SRCROOT}/stage/RPMS:/RPMS -v ${SRCROOT}/stage/ostree-repo:/srv/rpm-ostree -w="/photon/support/image-builder/ostree-tools/" vmware/photon-build:rpm-ostree-aarch64-3.0 ./mk-ostree-server.sh /
 fi
 
-(cd stage/ostree-repo/repo/; tar -zcf ../../ostree-repo.tar.gz .; )
+REPODIR=${SRCROOT}/stage/ostree-repo/repo
+if [ -d "$REPODIR" ]; then
+  tar -zcf ${SRCROOT}/stage/ostree-repo.tar.gz -C ${REPODIR} .
+fi
 
 # Restore file
 mv -f ${SRCROOT}/support/image-builder/ostree-tools/photon-ostree.repo.bak ${SRCROOT}/support/image-builder/ostree-tools/photon-ostree.repo
