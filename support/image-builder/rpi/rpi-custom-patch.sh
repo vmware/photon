1fb9341e80d3f51e89058230400daa9c25641fa6
Ankit Jain <ankitja@vmware.com>
2020-12-11 19:31:45 +0000

    rpi: Fix ethernet issue in rpi4 boards
    - To fix ethernet failed to connect PHY issue
    - Added genet.conf to load the module in particular sequence
      to avoid race between genet and mdio_bcm_unimac
    
    Change-Id: I70457a38cc3808ce7aa21cbf8959af682846c94c
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/11895
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Ajay Kaher <akaher@vmware.com>
    Reviewed-by: Alexey Makhalov <amakhalov@vmware.com>

--- support/image-builder/rpi/rpi-custom-patch.sh
+++ support/image-builder/rpi/rpi-custom-patch.sh
@@ -7,6 +7,16 @@ ln -s /lib/systemd/system/resizefs.service /etc/systemd/system/multi-user.target
 ln -s /lib/systemd/system/sshd-keygen.service /etc/systemd/system/multi-user.target.wants/sshd-keygen.service
 ln -s /lib/systemd/system/sshd.service /etc/systemd/system/multi-user.target.wants/sshd.service
 
+cat > /etc/modules-load.d/genet.conf << "EOF"
+# To fix https://github.com/raspberrypi/linux/issues/3108
+# Issue: bcmgenet_mii_probe() is getting called before unimac_mdio_probe() finishes
+#        which causes the ethernet "failed to connect PHY" in rpi4
+# Sol:   Load the modules in below order using systemd-modules
+
+mdio_bcm_unimac
+genet
+EOF
+
 kver="$(rpm -q --queryformat "%{VERSION}-%{RELEASE}" linux)"
 
 # Regenerate initramfs to reflect the new dracut config and also without the host-only flag
