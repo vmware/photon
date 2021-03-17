316bed4933311c4cff09316194d12f1a1b0296ef
Michael Roy <mike@mikeroysoft.com>
2021-03-16 17:38:30 -0700

    Doc cleanup, fix hamburger

--- themes/photon-theme/assets/js/base.js
+++ themes/photon-theme/assets/js/base.js
@@ -14,11 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// function mobileNavToggle() {
-//     var menu = document.getElementById("mobile-menu").parentElement;
-//     menu.classList.toggle('mobile-menu-visible');
-// }
-
 (function($) {
 
 
