7a62a1800e1a08bc97af40cd8312b612736e0e05
Piyush Gupta <gpiyush@vmware.com>
2021-04-30 16:14:58 +0000

    check_spec_files.sh: exit with status 1 on failure
    When exit 1 is invoked inside subshell the overall status after running
    the script is 0 as exit 1 is only applicable to subshell environment.
    This change uses here string so that while loop doesn't run in a subshell
    environment.
    
    Change-Id: I1c9302e5616ccd26bf20ec100d4110f233ff6e59
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/13564
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Ankit Jain <ankitja@vmware.com>

--- tools/scripts/check_spec_files.sh
+++ tools/scripts/check_spec_files.sh
@@ -52,7 +52,6 @@ function check-for-bogus-dates()
   local y=''
   local epoch_seconds=''
   
-  sed -e '1,/%changelog/d' "$1" | grep '^\*' | awk '{printf "%s %s %02d %04d\n", $2, $3, $4, $5}' | \
   while read D m d y
   do
     day=$(date --date="$m $d $y" '+%a')
@@ -67,7 +66,7 @@ function check-for-bogus-dates()
       exit 1
     fi
     prev_epoch_seconds=$epoch_seconds
-  done
+  done <<< "$(sed -e '1,/%changelog/d' "$1" | grep '^\*' | awk '{printf "%s %s %02d %04d\n", $2, $3, $4, $5}')"
 }
 
 # No trailing spaces
