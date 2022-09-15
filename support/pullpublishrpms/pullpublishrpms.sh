6bdcda61a8a35c6645751ca22b97ce8b25939b19
Shreenidhi Shedi <sshedi@vmware.com>
2022-09-15 11:58:58 +0530

    build.py: fix some issues that were introduced in refactoring
    And some more cleanups
    
    Change-Id: I234589ae1d39c75557f18d83d263e516f3275b9b
    Signed-off-by: Shreenidhi Shedi <sshedi@vmware.com>
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/17863
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Alexey Makhalov <amakhalov@vmware.com>

--- support/pullpublishrpms/pullpublishrpms.sh
+++ support/pullpublishrpms/pullpublishrpms.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-#################################################
-#	Title:	pullpublishrpms.sh	   	#
-#        Date:	2015-06-06          		#
-#     Version:	1.0				#
-#      Author:	dthaluru@vmware.com             #
-#################################################
 
 PRGNAME=${0##*/}
 
 if [ $# -lt 1 ]; then
-    echo "${PRGNAME}: No publish rpms path and log path are specified. Usage : ${PRGNAME} <publish-rpms-path>"
-    exit 1
+  echo "${PRGNAME}: No publish rpms path and log path are specified. Usage : ${PRGNAME} <publish-rpms-path>"
+  exit 1
 fi
 
 PUBLISHRPMSPATHDIR=$1
 INPUTFILE=rpmfilelist-$(uname -m)
 
 if [ $# -eq 2 ]; then
-   PUBLISHCACHE=$2
-   while read FILE; do cp -r $PUBLISHCACHE/$FILE $PUBLISHRPMSPATHDIR/$FILE; done < $INPUTFILE
+  PUBLISHCACHE=$2
+  while read FILE; do cp -r $PUBLISHCACHE/$FILE $PUBLISHRPMSPATHDIR/$FILE; done < $INPUTFILE
 else
-   cat $INPUTFILE | awk '{print "https://packages.vmware.com/photon/photon_publish_rpms/"$1}' | xargs -n 1 -P 10 wget --user-agent Mozilla/4.0 -c -nv -nc -r -nH --quiet --cut-dirs=2 -P ${PUBLISHRPMSPATHDIR}
+  cat $INPUTFILE | awk '{print "https://packages.vmware.com/photon/photon_publish_rpms/"$1}' | xargs -n 1 -P 10 wget --user-agent Mozilla/4.0 -c -nv -nc -r -nH --quiet --cut-dirs=2 -P ${PUBLISHRPMSPATHDIR}
 fi
