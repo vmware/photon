ab3b834ac817bac5608599844ca917857e7fefba
Shreenidhi Shedi <sshedi@vmware.com>
2023-02-08 12:16:11 +0530

    iso/mk-install-iso.sh: make the script a bit generic
    Cleanup files which are unneeded.
    Remove rpmdb files from the installer image, not needed in installer.
    
    Change-Id: I36c976a1dff2c1ee924a69243559e10149739803
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/19797
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Shreenidhi Shedi <sshedi@vmware.com>

--- support/image-builder/iso/mk-install-iso.sh
+++ support/image-builder/iso/mk-install-iso.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-set -e
-set -x
+set -ex
 
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 PRGNAME=${0##*/}    # script name minus the path
 
-# need to be changed when there is a python version change
+# Should be changed when there is a python version change
 PY_VER="3.10"
 
 WORKINGDIR=$1
@@ -24,13 +23,17 @@ INITRD=${WORKINGDIR}/photon-chroot
 PACKAGES=$8
 PHOTON_DOCKER_IMAGE=$9
 PH_BUILDER_TAG=${10}
+PH_VERSION=${11}
+ARCH="$(uname -m)"
 
 rm -rf $WORKINGDIR/*
 mkdir -m 755 -p $INITRD
 
 tar -xf $SCRIPT_PATH/open_source_license.tar.gz -C $WORKINGDIR/
-cp $STAGE_PATH/NOTICE-Apachev2 $WORKINGDIR/
-cp $STAGE_PATH/NOTICE-GPL2.0 $WORKINGDIR/
+
+cp $STAGE_PATH/NOTICE-Apachev2 \
+   $STAGE_PATH/NOTICE-GPL2.0 \
+   $WORKINGDIR/
 
 # 1. install rpms into initrd path
 cat > ${WORKINGDIR}/photon-local.repo <<EOF
@@ -69,7 +72,7 @@ TDNF_CMD="tdnf install -qy \
           -c ${WORKINGDIR}/tdnf.conf \
           ${PACKAGES}"
 
-# run host's tdnf, if fails - try one from photon:latest docker image
+# Run host's tdnf, if fails - try one from photon:latest docker image
 $TDNF_CMD || docker run --rm -v $RPMS_PATH:$RPMS_PATH -v $WORKINGDIR:$WORKINGDIR $PHOTON_DOCKER_IMAGE /bin/bash -c "$TDNF_CMD"
 
 rm -f ${WORKINGDIR}/photon-local.repo ${WORKINGDIR}/tdnf.conf
@@ -90,7 +93,9 @@ rm -rf ${INITRD}/var/cache/tdnf
 # Move entire /boot from initrd to ISO
 mv ${INITRD}/boot ${WORKINGDIR}/
 
-cp -r $SCRIPT_PATH/BUILD_DVD/isolinux $SCRIPT_PATH/BUILD_DVD/boot ${WORKINGDIR}/
+cp -pr $SCRIPT_PATH/BUILD_DVD/isolinux \
+       $SCRIPT_PATH/BUILD_DVD/boot \
+       ${WORKINGDIR}/
 
 #Generate efiboot image
 # efiboot is a fat16 image that has at least EFI/BOOT/bootx64.efi
@@ -136,7 +141,7 @@ cp $SCRIPT_PATH/BUILD_DVD/fstab ${INITRD}/etc/fstab
 mkdir -p ${INITRD}/etc/yum.repos.d
 cat > ${INITRD}/etc/yum.repos.d/photon-iso.repo << EOF
 [photon-iso]
-name=VMWare Photon Linux 1.0(x86_64)
+name=VMWare Photon Linux ${PH_VERSION}(${ARCH})
 baseurl=file:///mnt/media/RPMS
 gpgkey=file:///etc/pki/rpm-gpg/VMWARE-RPM-GPG-KEY
 gpgcheck=1
@@ -152,19 +157,18 @@ cd /installer
 ACTIVE_CONSOLE="\$(< /sys/devices/virtual/tty/console/active)"
 
 install() {
-  LANG=en_US.UTF-8 photon-installer -i iso -o $PACKAGE_LIST_FILE_BASE_NAME -e EULA.txt -t "VMWARE 4.0 LICENSE AGREEMENT" -v $PHOTON_RELEASE_VER && shutdown -r now
+  LANG=en_US.UTF-8 photon-installer -i iso -o $PACKAGE_LIST_FILE_BASE_NAME -e EULA.txt -t "VMWARE ${PH_VERSION} BETA LICENSE AGREEMENT" -v $PHOTON_RELEASE_VER && shutdown -r now
 }
 
 try_run_installer() {
   if [ "\$ACTIVE_CONSOLE" == "tty0" ]; then
-      [ "\$(tty)" == '/dev/tty1' ] && install
+    [ "\$(tty)" == '/dev/tty1' ] && install
   else
-      [ "\$(tty)" == "/dev/\$ACTIVE_CONSOLE" ] && install
+    [ "\$(tty)" == "/dev/\$ACTIVE_CONSOLE" ] && install
   fi
 }
 
 try_run_installer || exec /bin/bash
-
 EOF
 
 chmod 755 ${INITRD}/bin/bootphotoninstaller
@@ -175,12 +179,11 @@ mount -t proc proc /proc
 EOF
 chmod 755 ${INITRD}/init
 
-#adding autologin to the root user
-# and set TERM=linux for installer
+# Adding autologin to the root user and set TERM=linux for installer
 sed -i "s/ExecStart.*/ExecStart=-\/sbin\/agetty --autologin root --noclear %I linux/g" ${INITRD}/lib/systemd/system/getty@.service
 sed -i "s/ExecStart.*/ExecStart=-\/sbin\/agetty --autologin root --keep-baud 115200,38400,9600 %I screen/g" ${INITRD}/lib/systemd/system/serial-getty@.service
 
-#- Step 7 - Create installer script
+# Step 7 - Create installer script
 sed -i "s/root:.*/root:x:0:0:root:\/root:\/bin\/bootphotoninstaller/g" ${INITRD}/etc/passwd
 
 mkdir -p ${INITRD}/mnt/photon-root/photon-chroot
@@ -191,85 +194,95 @@ echo ${RPMS_PATH}
 (
 cd ${RPMS_PATH}
 mkdir -p ${WORKINGDIR}/RPMS
-for rpm_name in $RPM_LIST; do
-    cp --parent $rpm_name ${WORKINGDIR}/RPMS/
-    chmod 644 ${WORKINGDIR}/RPMS/$rpm_name
-done
+cp --verbose --parents ${RPM_LIST} ${WORKINGDIR}/RPMS/
+chmod 644 ${WORKINGDIR}/RPMS/${ARCH}/*.rpm ${WORKINGDIR}/RPMS/noarch/*.rpm
 )
 
 # Work in sub-shell using ( ... ) to come back to original folder.
 (
+file_list=""
 cd $STAGE_PATH
 for file_name in $ADDITIONAL_FILES_TO_COPY_FROM_STAGE; do
-    [ -n "$file_name" ] &&  cp $file_name ${WORKINGDIR}
+  [ -n "$file_name" ] && file_list+="${file_name} "
 done
+if [ -n "${file_list}" ]; then
+  cp ${file_list} ${WORKINGDIR}
+fi
 )
 
-#creating rpm repo in cd..
-createrepo --database ${WORKINGDIR}/RPMS
+# Creating rpm repo in cd..
+createrepo --update --database ${WORKINGDIR}/RPMS
 
 repodatadir=${WORKINGDIR}/RPMS/repodata
 if [ -d $repodatadir ]; then
-    pushd $repodatadir
-    metaDataFile=$(find -type f -name "*primary.xml.gz")
-    ln -sfv $metaDataFile primary.xml.gz
-    popd
+  pushd $repodatadir
+  metaDataFile=$(find -type f -name "*primary.xml.gz")
+  ln -sfv $metaDataFile primary.xml.gz
+  popd
 fi
 
 rm -rf ${INITRD}/LOGS
 
 # Cleaning up
-find ${INITRD}/usr/lib/ -maxdepth 1 -mindepth 1 -type f | xargs -i sh -c "grep ELF {} >/dev/null 2>&1 && strip {} || :"
+find ${INITRD}/usr/lib/ -maxdepth 1 -mindepth 1 -type f -print0 | \
+  xargs -0 -r -P$(nproc) -n32 sh -c "file \"\$@\" | \
+  sed -n -e 's/^\(.*\):[ 	]*ELF.*, not stripped.*/\1/p' | \
+  xargs -I\{\} strip \{\}" ARG0
 
-rm -rf ${INITRD}/home/*         \
-        ${INITRD}/var/lib/rpm*  \
+rm -rf ${INITRD}/home/* \
+        ${INITRD}/var/lib/rpm* \
         ${INITRD}/var/lib/.rpm* \
-        ${INITRD}/cache         \
-        ${INITRD}/boot          \
-        ${INITRD}/usr/include   \
-        ${INITRD}/usr/sbin/sln  \
+        ${INITRD}/usr/lib/sysimage/rpm* \
+        ${INITRD}/usr/lib/sysimage/.rpm* \
+        ${INITRD}/cache \
+        ${INITRD}/boot \
+        ${INITRD}/usr/include \
+        ${INITRD}/usr/sbin/sln \
         ${INITRD}/usr/bin/iconv \
-        ${INITRD}/usr/bin/oldfind       \
-        ${INITRD}/usr/bin/localedef     \
-        ${INITRD}/usr/bin/sqlite3       \
-        ${INITRD}/usr/bin/grub2-*       \
-        ${INITRD}/usr/bin/bsdcpio       \
-        ${INITRD}/usr/bin/bsdtar        \
-        ${INITRD}/usr/bin/networkctl    \
-        ${INITRD}/usr/bin/machinectl    \
-        ${INITRD}/usr/bin/pkg-config    \
-        ${INITRD}/usr/bin/openssl       \
-        ${INITRD}/usr/bin/timedatectl   \
-        ${INITRD}/usr/bin/localectl     \
-        ${INITRD}/usr/bin/systemd-cgls  \
-        ${INITRD}/usr/bin/systemd-analyze       \
-        ${INITRD}/usr/bin/systemd-nspawn        \
-        ${INITRD}/usr/bin/systemd-inhibit       \
+        ${INITRD}/usr/bin/oldfind \
+        ${INITRD}/usr/bin/localedef \
+        ${INITRD}/usr/bin/sqlite3 \
+        ${INITRD}/usr/bin/grub2-* \
+        ${INITRD}/usr/bin/bsdcpio \
+        ${INITRD}/usr/bin/bsdtar \
+        ${INITRD}/usr/bin/networkctl \
+        ${INITRD}/usr/bin/machinectl \
+        ${INITRD}/usr/bin/pkg-config \
+        ${INITRD}/usr/bin/openssl \
+        ${INITRD}/usr/bin/timedatectl \
+        ${INITRD}/usr/bin/localectl \
+        ${INITRD}/usr/bin/systemd-cgls \
+        ${INITRD}/usr/bin/systemd-analyze \
+        ${INITRD}/usr/bin/systemd-nspawn \
+        ${INITRD}/usr/bin/systemd-inhibit \
         ${INITRD}/usr/bin/systemd-studio-bridge \
-        ${INITRD}/usr/lib/python${PY_VER}/lib2to3     \
-        ${INITRD}/usr/lib/python${PY_VER}/lib-tk      \
-        ${INITRD}/usr/lib/python${PY_VER}/ensurepip   \
-        ${INITRD}/usr/lib/python${PY_VER}/distutils   \
-        ${INITRD}/usr/lib/python${PY_VER}/pydoc_data  \
-        ${INITRD}/usr/lib/python${PY_VER}/idlelib     \
-        ${INITRD}/usr/lib/python${PY_VER}/unittest    \
-        ${INITRD}/usr/lib/librpmbuild.so*       \
-        ${INITRD}/usr/lib/libdb_cxx*            \
-        ${INITRD}/usr/lib/libnss_compat*        \
+        ${INITRD}/usr/lib/python${PY_VER}/lib2to3 \
+        ${INITRD}/usr/lib/python${PY_VER}/lib-tk \
+        ${INITRD}/usr/lib/python${PY_VER}/ensurepip \
+        ${INITRD}/usr/lib/python${PY_VER}/distutils \
+        ${INITRD}/usr/lib/python${PY_VER}/pydoc_data \
+        ${INITRD}/usr/lib/python${PY_VER}/idlelib \
+        ${INITRD}/usr/lib/python${PY_VER}/unittest \
+        ${INITRD}/usr/lib/librpmbuild.so* \
+        ${INITRD}/usr/lib/libdb_cxx* \
+        ${INITRD}/usr/lib/libnss_compat* \
         ${INITRD}/usr/lib/grub/i386-pc/*.module \
         ${INITRD}/usr/lib/grub/x86_64-efi/*.module \
-        ${INITRD}/lib64/libmvec*        \
-        ${INITRD}/usr/lib64/gconv
+        ${INITRD}/usr/lib/grub/arm64-efi/*.module \
+        ${INITRD}/lib/libmvec* \
+        ${INITRD}/usr/lib/gconv
 
 find "${INITRD}/usr/sbin" -mindepth 1 -maxdepth 1 -name "grub2*" \
-                        ! -name grub2-install -exec rm -rvf {} \;
+                        ! -name grub2-install -print0 | \
+                        xargs -0 -r -P$(nproc) -n32 rm -rvf
 
 find "${INITRD}/usr/share" -mindepth 1 -maxdepth 1 \
                         ! -name terminfo \
                         ! -name cracklib \
-                        ! -name grub    \
+                        ! -name grub \
                         ! -name factory \
-                        ! -name dbus-1 -exec rm -rvf {} \;
+                        ! -name dbus-1 -print0 |
+                        xargs -0 -r -P$(nproc) -n32 rm -rvf
 
 # Set password max days to 99999 (disable aging)
 chroot ${INITRD} /bin/bash -c "chage -M 99999 root"
@@ -280,11 +293,15 @@ pushd $INITRD
 popd
 rm -rf $INITRD
 
-#Step 9 Generate the ISO!!!!
+# Step 9 Generate the ISO!!!!
 pushd $WORKINGDIR
-mkisofs -R -l -L -D -b isolinux/isolinux.bin -c isolinux/boot.cat \
-        -no-emul-boot -boot-load-size 4 -boot-info-table \
-        -eltorito-alt-boot -e ${EFI_IMAGE} -no-emul-boot \
+mkisofs -R -l -L -D -b isolinux/isolinux.bin \
+        -c isolinux/boot.cat \
+        -no-emul-boot \
+        -boot-load-size 4 \
+        -boot-info-table \
+        -eltorito-alt-boot -e ${EFI_IMAGE} \
+        -no-emul-boot \
         -V "PHOTON_$(date +%Y%m%d)" \
-        $WORKINGDIR >$ISO_OUTPUT_NAME
+        $WORKINGDIR > $ISO_OUTPUT_NAME
 popd
