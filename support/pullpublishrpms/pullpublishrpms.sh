eb71496a02bb6aabb327a2649960f9bca328e0c2
Shreenidhi Shedi <sshedi@vmware.com>
2023-10-09 23:12:55 +0530

    support/pullpublishrpms: calculate cut dir value based on url
    Change-Id: Ib05cbf56d1aed075562f31c7883d22d483f8fe3a
    Signed-off-by: Shreenidhi Shedi <sshedi@vmware.com>
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/c/photon/+/22079
    Tested-by: Michelle Wang <michellew@vmware.com>
    Reviewed-by: Alexey Makhalov <amakhalov@vmware.com>

--- support/pullpublishrpms/pullpublishrpms.sh
+++ support/pullpublishrpms/pullpublishrpms.sh
@@ -3,8 +3,8 @@
 PRGNAME=${0##*/}
 
 if [ $# -ne 3 ]; then
-  echo "${PRGNAME}: Invalid input args."
-  echo "Usage: ${PRGNAME} <rpms-path> <rpms-url> <input-file>"
+  echo "${PRGNAME}: Invalid input args." 1>&2
+  echo "Usage: ${PRGNAME} <rpms-path> <rpms-url> <input-file>" 1>&2
   exit 1
 fi
 
@@ -16,7 +16,10 @@ INPUTFILE="$3-${ARCH}"
 
 mkdir -p ${RPMSPATHDIR}/{${ARCH},noarch}
 
-sed "s|^|${RPMS_URL}/|g" ${INPUTFILE} | xargs -n 1 -P 10 wget --user-agent Mozilla/4.0 -c -nv -nc -r -nH --quiet --cut-dirs=2 -P ${RPMSPATHDIR}
+c=$(($(echo "$RPMS_URL" | tr -cd '/' | wc -c) - 2))
+
+sed "s|^|${RPMS_URL}/|g" ${INPUTFILE} | \
+  xargs -n 1 -P 10 wget --user-agent Mozilla/4.0 -c -nv -nc -r -nH --quiet --cut-dirs=$c -P ${RPMSPATHDIR}
 
 if ls ${RPMSPATHDIR}/*.${ARCH}.rpm &> /dev/null; then
   mv ${RPMSPATHDIR}/*.${ARCH}.rpm ${RPMSPATHDIR}/${ARCH}
