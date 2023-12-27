c82b36dc191ef3fad25c8835d79e6cc27792d218
Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
2023-12-27 16:21:36 +0530

    pullpublishrpms: validate wget exit status
    Improve error message strings.
    
    Change-Id: I60f0a018f69087493463f4448653d4406a95ae44
    Signed-off-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/c/photon/+/22953
    (cherry picked from commit 07883a0949cafe5baf96220756e1e1956501b10c)
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/c/photon/+/22969

--- support/pullpublishrpms/pullpublishrpms-cached.sh
+++ support/pullpublishrpms/pullpublishrpms-cached.sh
@@ -3,8 +3,8 @@
 PRGNAME=${0##*/}
 
 if [ $# -ne 3 ]; then
-  echo "${PRGNAME}: Invalid input args." 1>&2
-  echo "Usage: ${PRGNAME} <rpms-path> <publish-cache> <input-file>" 1>&2
+  echo -e "\nERROR: '${PRGNAME}' Invalid input args.
+Usage: ${PRGNAME} <rpms-path> <rpms-url> <input-file>\n" 1>&2
   exit 1
 fi
 
@@ -17,6 +17,11 @@ INPUTFILE="$3-${ARCH}"
 mkdir -p ${RPMSPATHDIR}/{${ARCH},noarch}
 
 sed -e "s|^|${PUBLISHCACHE}/|g" ${INPUTFILE} | xargs -n 1 -P 10 cp -t ${RPMSPATHDIR}
+if [ $? -ne 0 ]; then
+  echo -e "\nERROR: '${PRGNAME}' failed to copy one or more rpms.
+Thoroughly check entries in ${INPUTFILE} and ensure that file(s) are present in server\n" 1>&2
+  exit 1
+fi
 
 if ls ${RPMSPATHDIR}/*.${ARCH}.rpm &> /dev/null; then
   mv ${RPMSPATHDIR}/*.${ARCH}.rpm ${RPMSPATHDIR}/${ARCH}
