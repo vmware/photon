ab22ef9a40a352a0cc67c10ae23c53c48184d5a9
Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
2024-01-12 16:04:41 +0530

    ca-certificate: spec cleanups
    Helper scripts were getting created upon every build.
    Keep them as sources instead.
    
    Change-Id: Id7901f0358a1c174ee5bab9091a7a707ee6f3d7f
    Signed-off-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/c/photon/+/23076
    Tested-by: gerrit-photon <photon-checkins@vmware.com>

---
+++ SPECS/ca-certificates/remove-expired-certs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Begin /bin/remove-expired-certs.sh
+# Version 20120211
+# Make sure the date is parsed correctly on all systems
+
+function mydate() {
+  local y=$( echo $1 | cut -d" " -f4 )
+  local M=$( echo $1 | cut -d" " -f1 )
+  local d=$( echo $1 | cut -d" " -f2 )
+  local m
+  if [ ${d} -lt 10 ]; then d="0${d}"; fi
+  case $M in
+    Jan) m="01";;
+    Feb) m="02";;
+    Mar) m="03";;
+    Apr) m="04";;
+    May) m="05";;
+    Jun) m="06";;
+    Jul) m="07";;
+    Aug) m="08";;
+    Sep) m="09";;
+    Oct) m="10";;
+    Nov) m="11";;
+    Dec) m="12";;
+  esac
+  certdate="${y}${m}${d}"
+}
+
+OPENSSL=/usr/bin/openssl
+DIR=/etc/ssl/certs
+if [ $# -gt 0 ]; then
+  DIR="$1"
+fi
+
+certs=$( find ${DIR} -type f -name "*.pem" -o -name "*.crt" )
+today=$( date +%Y%m%d )
+for cert in $certs; do
+  notafter=$( $OPENSSL x509 -enddate -in "${cert}" -noout )
+  date=$( echo ${notafter} |  sed 's/^notAfter=//' )
+  mydate "$date"
+  if [ ${certdate} -lt ${today} ]; then
+    echo "${cert} expired on ${certdate}! Removing..."
+    rm -f "${cert}"
+  fi
+done
