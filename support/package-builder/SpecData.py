02d275df0d3b88d83ac976d187d41b21d7cba036
Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
2024-01-25 22:57:39 +0530

    package-builder/SpecData.py: fix regex to silence python warning
    Change-Id: Ie9fe471bee7935d78ee17035d67b7a2cda8f262f
    Signed-off-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/c/photon/+/23159

--- support/package-builder/SpecData.py
+++ support/package-builder/SpecData.py
@@ -351,7 +351,7 @@ class SPECS(object):
         # adding kernelrelease rpm macro
         kernelrelease = defPkg.release
         kernelrelease_comp  = kernelrelease.split('.')
-        if re.fullmatch('rc\d+', kernelrelease_comp[0]):
+        if re.fullmatch(r'rc\d+', kernelrelease_comp[0]):
             kernelrelease_comp.pop(0)
 
         # adding kernelsubrelease rpm macro
