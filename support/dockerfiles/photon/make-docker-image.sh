24d5afdd934759f303fe4fd02b8f95f904e91688
Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
2024-04-20 13:00:05 +0530

    make-docker-image.sh: nit pick fixes
    Change-Id: I113cc137bee25e96e50e268ffaa883e242d2262b
    Signed-off-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/c/photon/+/23774
    Tested-by: gerrit-photon <photon-checkins@vmware.com>

--- support/dockerfiles/photon/make-docker-image.sh
+++ support/dockerfiles/photon/make-docker-image.sh
@@ -2,44 +2,43 @@
 
 set -ex
 
-echoerr()
-{
+echoerr() {
   echo -e "$*" 1>&2
 }
 
-echo PHOTON_RELEASE_VERSION=$PHOTON_RELEASE_VERSION
+echo "PHOTON_RELEASE_VERSION=${PHOTON_RELEASE_VERSION}"
 arch="$(uname -m)"
-TEMP_CHROOT=$(pwd)/temp_chroot
-ROOTFS_TAR_FILENAME=photon-rootfs-$PHOTON_RELEASE_VERSION-$PHOTON_BUILD_NUMBER.${arch}.tar.gz
-STAGE_DIR=$(pwd)/stage
+TEMP_CHROOT="${PWD}/temp_chroot"
+ROOTFS_TAR_FILENAME="photon-rootfs-${PHOTON_RELEASE_VERSION}-${PHOTON_BUILD_NUMBER}.${arch}.tar.gz"
+STAGE_DIR="${PWD}/stage"
 
 rm -rf /etc/yum.repos.d/*
 
 cat > /etc/yum.repos.d/photon-local.repo <<- EOF
 [photon-local]
 name=VMware Photon Linux ${PHOTON_RELEASE_VERSION}($arch)
-baseurl=file://$(pwd)/stage/RPMS
+baseurl=file://${PWD}/stage/RPMS
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY
 gpgcheck=0
 enabled=1
 skip_if_unavailable=True
 EOF
 
-rm -rf $TEMP_CHROOT
-mkdir $TEMP_CHROOT
+rm -rf ${TEMP_CHROOT}
+mkdir ${TEMP_CHROOT}
 
 tdnf install -y --setopt=tsflags=nodocs rpm tar gzip grep coreutils
 
-rpm --root $TEMP_CHROOT/ --initdb
+rpm --root ${TEMP_CHROOT}/ --initdb
 
 tdnf --releasever ${PHOTON_RELEASE_VERSION} \
-     --installroot $TEMP_CHROOT/ \
+     --installroot ${TEMP_CHROOT}/ \
      --rpmverbosity error \
      --setopt=tsflags=nodocs \
      install -y \
      filesystem bash toybox tdnf photon-release photon-repos curl
 
-actual_pkg_list=($(tdnf --installroot $TEMP_CHROOT/ \
+actual_pkg_list=($(tdnf --installroot ${TEMP_CHROOT}/ \
                         --disablerepo=* -q \
                         list installed 2>/dev/null | cut -d'.' -f1))
 
@@ -57,38 +56,38 @@ expected_pkg_count=${#expected_pkg_list[@]}
 pkg_diff="$(echo ${expected_pkg_list[@]} ${actual_pkg_list[@]} | \
             tr ' ' '\n' | sort | uniq -u)"
 
-if [ $expected_pkg_count -ne $actual_pkg_count ] || [ -n "${pkg_diff}" ]; then
+if [ ${expected_pkg_count} -ne ${actual_pkg_count} ] || [ -n "${pkg_diff}" ]; then
   echoerr "Following package difference found in docker image:\n${pkg_diff}"
-  echoerr "Expected package count: $expected_pkg_count"
-  echoerr "Actual package count: $actual_pkg_count"
-  rm -rf $TEMP_CHROOT
+  echoerr "Expected package count: ${expected_pkg_count}"
+  echoerr "Actual package count: ${actual_pkg_count}"
+  rm -rf ${TEMP_CHROOT}
   exit 1
 fi
 
-rpm --root $TEMP_CHROOT/ --import $TEMP_CHROOT/etc/pki/rpm-gpg/*
+rpm --root ${TEMP_CHROOT}/ --import ${TEMP_CHROOT}/etc/pki/rpm-gpg/*
 
 # cleanup anything not needed inside rootfs
-pushd $TEMP_CHROOT
+pushd ${TEMP_CHROOT}
 rm -rf usr/src/ home/* var/log/* var/cache/tdnf/
 # set TERM to linux due to stripped terminfo
 echo "export TERM=linux" >> etc/bash.bashrc
 
-tar -I 'gzip -9' -cpf ../$ROOTFS_TAR_FILENAME .
+tar -I 'gzip -9' -cpf ../${ROOTFS_TAR_FILENAME} .
 popd
 
 # expected size plus 2% wiggle room
 max_size=17313736
 
-actual_size=$(wc -c $ROOTFS_TAR_FILENAME | cut -d' ' -f1)
-if (( $actual_size > $max_size )); then
+actual_size=$(wc -c ${ROOTFS_TAR_FILENAME} | cut -d' ' -f1)
+if (( ${actual_size} > ${max_size} )); then
   echoerr "ERROR: docker image tarball size is bigger than expected"
-  echoerr "Expected size(in bytes): $max_size"
-  echoerr "Actual size(in bytes): $actual_size"
-  rm -rf $TEMP_CHROOT $ROOTFS_TAR_FILENAME
+  echoerr "Expected size(in bytes): ${max_size}"
+  echoerr "Actual size(in bytes): ${actual_size}"
+  rm -rf ${TEMP_CHROOT} ${ROOTFS_TAR_FILENAME}
   exit 1
 fi
 
-mv $ROOTFS_TAR_FILENAME $STAGE_DIR/
+mv ${ROOTFS_TAR_FILENAME} ${STAGE_DIR}/
 
 # cleanup
-rm -rf $TEMP_CHROOT
+rm -rf ${TEMP_CHROOT}
