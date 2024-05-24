c288653d7e5fbefbae35e1dc3f62731eb2c1991e
Mukul Sikka <msikka@vmware.com>
2024-05-24 08:48:44 +0000

    Creating Main branch for Photon OS
    - Cloned from dev branch
    - Removed all SPECS except linux
    - Moved linux spec to linux/v6.1
    
    Change-Id: I505c5d6779a9bad40353507c0e4405cfc43bf60e
    Signed-off-by: Mukul Sikka <msikka@vmware.com>

--- SPECS/openresty/openresty.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-orprefix="@@ORPREFIX@@"
-[ -d "${orprefix}/bin" ] && PATH="${PATH}:${orprefix}/bin"
-[ -d "${orprefix}/nginx/sbin" ] && PATH="${PATH}:${orprefix}/nginx/sbin"
-export PATH
