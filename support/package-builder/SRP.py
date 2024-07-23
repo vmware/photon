c0bfef03cab4b28eca647a85dbdeaa5fb8ae72b3
Alexey Makhalov <alexey.makhalov@broadcom.com>
2024-07-23 11:33:40 -0700

    Package Builder: add missing field in uid.obj.comp.package.rpm
    Fix partial_input and partial_output issue by adding missing
    original_repository property in uid.obj.comp.package.rpm.
    
    Extra:
    - Add compute resource, no-debugger and non-root descriptions.
    - Remove old way of adding output components.
    - StringUtils.splitRPMFilename(): change return type to dictionary.
    
    Things still to implement:
    - Get 3 missing fields from CICD: location, build id and vm template.
    - Specify input source tarball.
    - Add proxy observations.
    
    Change-Id: Ib01be99b21c8281c4feee2ab8de2e15965861c97
    Reviewed-on: http://photon-jenkins.eng.vmware.com:8082/c/photon/+/24437
    Tested-by: gerrit-photon <photon-checkins@vmware.com>
    Reviewed-by: Alexey Makhalov <amakhalov@vmware.com>
    Reviewed-by: Brennan Lamoreaux <brennan.lamoreaux@broadcom.com>

--- support/package-builder/SRP.py
+++ support/package-builder/SRP.py
@@ -1,6 +1,8 @@
+import os
 import os.path
 import json
 import copy
+import csv
 
 from constants import constants
 from CommandUtils import CommandUtils
@@ -16,12 +18,17 @@ class SRP(object):
             return
 
         self.pkg = pkg
-        self.package, self.version, self.release, self.distTag, arch= StringUtils.splitRPMFilename(self.pkg)
+        rpm = StringUtils.splitRPMFilename(self.pkg)
+        self.package = rpm["name"]
+        self.version = rpm["version"]
+        self.release = rpm["release"]
+        self.distTag = rpm["tag"]
         self.fullVersion = f"{self.version}-{self.release}.{self.distTag}"
 
         self.logger = logger
         self.cmdUtils = CommandUtils()
         self.srp_workdir = os.path.join(constants.stagePath, "SRP", f"{pkg}")
+        # Will be used as a package schematic json file at finalize step.
         self.schematic = {
             "schema_id": "1.0",
             "sources": {
@@ -36,6 +43,7 @@ class SRP(object):
             },
             "outputs": {}
         }
+        # SPDX template for output RPMs.
         self.spdx_package_common = {
             "package": {
                 "license_declared": f"{SPECS.getData().getLicense(self.package, self.fullVersion)}",
@@ -45,6 +53,13 @@ class SRP(object):
             }
         }
 
+    def getOSRelease(self):
+        path = "/etc/os-release"
+        with open(path) as stream:
+            reader = csv.reader(stream, delimiter="=")
+            os_release = dict(reader)
+        return f"{os_release['NAME']} {os_release['VERSION']}"
+
     def initialize(self):
         if not self.srpcli:
             return
@@ -52,9 +67,29 @@ class SRP(object):
         self.cmdUtils.runBashCmd(f"SRP_WORKING_DIR={self.srp_workdir} {self.srpcli} provenance add-build photon --package={self.package} --version={self.version} --release={self.release} --dist-tag={self.distTag} --arch={constants.targetArch}")
         self.cmdUtils.runBashCmd(f"SRP_WORKING_DIR={self.srp_workdir} {self.srpcli} provenance action start  --name=build-{self.pkg}")
 
+        # Add a compute resource information. From all available types vm.nimbus is closest to Photon build VM.
+        # TODO: next 3 variables must be provided by CI/CD pipeline. Variable names can be different from what specified below.
+        # Do not stop the build if not provided.
+        location = os.environ.get("LOCATION", "FIXME-LOCATION-NOT-DEFINED")
+        build_id = os.environ.get("BUILD_ID", "FIXME-BUILD_ID-NOT-DEFINED")
+        vm_template = os.environ.get("VM_TEMPLATE", "FIXME-VM_TEMPLATE-NOT-DEFINED")
+        self.cmdUtils.runBashCmd(f"SRP_WORKING_DIR={self.srp_workdir} {self.srpcli} provenance action add-compute-resource vm.nimbus --os-type linux --kernel-version {os.uname()[2]} --distro-version '{self.getOSRelease()}' --machine {os.uname()[1]} --ephemeral=true --firewall-present=true --location {location} --build-id {build_id} --template {vm_template}")
+        self.cmdUtils.runBashCmd(f"SRP_WORKING_DIR={self.srp_workdir} {self.srpcli} provenance action set-process-debugger --enabled=false")
+        self.cmdUtils.runBashCmd(f"SRP_WORKING_DIR={self.srp_workdir} {self.srpcli} provenance action set-root-access --enabled=false")
+
     def rpmFileNameToUid(self, filename):
-        n, v, r, t, a = StringUtils.splitRPMFilename(filename)
-        return f"uid.obj.comp.package.rpm(name='{n}',version='{v}',release='{r}.{t}',arch='{a}')"
+        rpm = StringUtils.splitRPMFilename(filename)
+        n = rpm["name"]
+        v = rpm["version"]
+        r = rpm["release"]
+        t = rpm["tag"]
+        a = rpm["arch"]
+        if t.startswith("ph"):
+            branch = f"{t[2:]}.0"
+        else:
+            raise Exception("Can not parse dist tag.")
+        repo = f"https://packages.vmware.com/photon/{branch}/photon_{branch}_{constants.targetArch}"
+        return f"uid.obj.comp.package.rpm(name='{n}',version='{v}',release='{r}.{t}',arch='{a}',original_repository='{repo}')"
 
     def addInputRPMS(self, files):
         if not self.srpcli:
@@ -76,10 +111,12 @@ class SRP(object):
 
         for file in files:
             filename = os.path.basename(file)
-            n, v, r, t, a = StringUtils.splitRPMFilename(filename)
-
-            # Old way of adding outputs
-            # self.cmdUtils.runBashCmd(f"SRP_WORKING_DIR={self.srp_workdir} {self.srpcli} provenance add-output general --set-key={self.package} --action-key=build-{self.pkg} --uid-type uid.obj.comp.package.rpm --uid-arg name={n} --uid-arg version={v} --uid-arg release={r}.{t} --uid-arg arch={a}")
+            rpm = StringUtils.splitRPMFilename(filename)
+            n = rpm["name"]
+            v = rpm["version"]
+            r = rpm["release"]
+            t = rpm["tag"]
+            a = rpm["arch"]
 
             if n == self.package + "-debuginfo":
                 description = f"Debuginfo for {self.package}"
@@ -92,7 +129,6 @@ class SRP(object):
             spdx_info = copy.deepcopy(self.spdx_package_common)
             spdx_info["package"]["detailed_description"] = description
 
-            # New way - via schematic
             self.schematic["outputs"][self.rpmFileNameToUid(filename)] = {
                 "merge_input_templates": ["rpm-comps"],
                 "spdx_info": spdx_info,
