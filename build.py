ce935fb40756c8142241d43ae615a393c3c97729
Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
2024-11-15 10:40:27 +0530

    build.py: don't check for pre requisites during 'make clean'
    Change-Id: Ib922fda628cccd478cd6119d88787c1bcb586334
    Signed-off-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
    Reviewed-on: http://photon-gerrit.lvn.broadcom.net/c/photon/+/24809
    Reviewed-by: Guruswamy Basavaiah <guruswamy.basavaiah@broadcom.com>
    Reviewed-by: Srinidhi Rao <srinidhi.rao@broadcom.com>
    Tested-by: gerrit-photon <svc.photon-ci@broadcom.com>
    (cherry picked from commit 3f61a2b10224fa4012b91aa90e88f6d6a97ae500)
    Reviewed-on: http://photon-gerrit.lvn.broadcom.net/c/photon/+/25295
    Reviewed-by: Vamsi Krishna Brahmajosyula <vamsi-krishna.brahmajosyula@broadcom.com>
    Tested-by: <mukul.sikka@broadcom.com>
    Reviewed-by: <mukul.sikka@broadcom.com>

--- build.py
+++ build.py
@@ -1909,7 +1909,8 @@ def main():
 
     configdict["targetName"] = targetName.replace("-", "_")
 
-    CheckTools.check_pre_reqs()
+    if targetName != "clean":
+        CheckTools.check_pre_reqs()
 
     try:
         attr = None
