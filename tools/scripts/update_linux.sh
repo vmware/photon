e345157f302ab9bde9918ab7991ec39e7ba440c1
Mukul Sikka <mukul.sikka@broadcom.com>
2025-03-07 08:16:50 +0000

    update_linux.sh: Fix to work with multiple linux versions
    Change-Id: If84bb6eb3b87f94c223ca19d923cb624f15977a3
    Reviewed-on: http://photon-gerrit.lvn.broadcom.net/c/photon/+/25316
    Reviewed-by: Vamsi Krishna Brahmajosyula <vamsi-krishna.brahmajosyula@broadcom.com>
    Reviewed-by: Ajay Kaher <ajay.kaher@broadcom.com>
    Tested-by: gerrit-photon <svc.photon-ci@broadcom.com>

--- tools/scripts/update_linux.sh
+++ tools/scripts/update_linux.sh
@@ -1,19 +1,70 @@
-#! /bin/sh
+#!/bin/sh
 
-# linux-api-headers has been excluded from the list of specs to be updated
-specs="linux/linux.spec linux/linux-esx.spec linux/linux-secure.spec linux/linux-rt.spec"
+# Check if no arguments are provided, try to read from build-config.json
+if [ -z "$1" ] && [ -z "$2" ]; then
+  release_branch_path=$(jq -r '.["release-branch-path"]' build-config.json)
+fi
 
-tarball_url=`curl -s https://www.kernel.org  | grep -Eo 'https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.[0-9]*.tar.xz' | uniq`
-tarball=$(basename $tarball_url)
-version=`echo $tarball | sed 's/linux-//; s/.tar.xz//'`
-echo latest linux version: $version
-test -f stage/SOURCES/$tarball && echo up to date && exit 0
-$(cd stage/SOURCES && wget $tarball_url)
-sha512=`sha512sum stage/SOURCES/$tarball | awk '{print $1}'`
-changelog_entry=$(echo "`date +"%a %b %d %Y"` `git config user.name` <`git config user.email`> $version-1")
-for spec in $specs; do
-	sed -i '/^Version:/ s/6.1.[0-9]*/'$version'/' SPECS/$spec
-	sed -i '/^Release:/ s/[0-9]*%/1%/' SPECS/$spec
-	sed -i '/^%define sha512 linux/ s/=[0-9a-f]*$/='$sha512'/' SPECS/$spec
-	sed -i '/^%changelog/a* '"$changelog_entry"'\n- Update to version '"$version"'' SPECS/$spec
+# Check if release_branch_path is provided (either as an argument or from build-config.json)
+if [ -z "$release_branch_path" ] &&  [ -z "$1" ]; then
+  echo "Error: Missing release_branch_path."
+  echo "Usage: $0 <release_branch_path> <major_version>"
+  echo "Example: $0 /path/to/release/branch 6.1"
+  exit 1
+else
+  if [ -n "$1" ]; then
+    release_branch_path=$1
+  fi
+fi
+
+# Check if major_version is provided
+if [ -z "$2" ]; then
+  # If major_version is not provided, extract the list of available versions from the spec files in the release branch path
+  major_versions=$(find SPECS/linux/ -mindepth 1 -maxdepth 1 -type d -name "v*" | sed -E 's/.*\/v([0-9]+\.[0-9]+)/\1/' | sort -u)
+
+  if [ -z "$major_versions" ]; then
+    echo "Error: No major versions found in SPECS/linux/."
+    exit 1
+  fi
+
+  echo "Available major versions:"
+  echo "$major_versions"
+else
+  major_versions=$2
+fi
+
+# Loop over major_versions if no major_version argument is provided
+for major_version in $major_versions; do
+  # Update the specs with the provided major version
+  specs=$(find SPECS/linux/v${major_version} -name "*.spec")
+
+  # Fetch the latest tarball URL based on the major version input
+  tarball_url=$(curl -s https://www.kernel.org | grep -Eo "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${major_version}\.[0-9]*.tar.xz" | uniq)
+  tarball=$(basename $tarball_url)
+  version=$(echo $tarball | sed 's/linux-//; s/.tar.xz//')
+  echo "Latest Linux version for $major_version: $version"
+
+  # Check if the tarball already exists, exit if up to date
+  if test -f ${release_branch_path}/stage/SOURCES/$tarball; then
+    echo "Up to date for $major_version"
+    continue
+  fi
+
+  # Download the tarball if not up to date
+  $(cd ${release_branch_path}/stage/SOURCES && wget $tarball_url)
+
+  # Calculate the sha512 checksum of the tarball
+  sha512=$(sha512sum ${release_branch_path}/stage/SOURCES/$tarball | awk '{print $1}')
+
+  # Create the changelog entry
+  changelog_entry=$(echo "`date +"%a %b %d %Y"` `git config user.name` <`git config user.email`> $version-1")
+
+  # Update the spec files
+  for spec in $specs; do
+    sed -i "/^Version:/ s/${major_version}.[0-9]*/$version/" $spec
+    sed -i '/^Release:/ s/[0-9]*%/1%/' $spec
+    sed -i "/^%define sha512 linux/ s/=[0-9a-f]*$/=$sha512/" $spec
+    sed -i "/^%changelog/a* $changelog_entry\n- Update to version $version" $spec
+  done
 done
+
