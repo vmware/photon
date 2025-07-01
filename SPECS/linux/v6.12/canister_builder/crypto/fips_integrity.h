a7773b5f812d734c7ed273c6dbd82a45676172c4
Alexey Makhalov <alexey.makhalov@broadcom.com>
2025-07-01 20:06:35 +0000

    linux: Move ECB out
    ECB is not FIPS approved. Move it out of the canister.
    
    fips_integrity: Clean up and reorder SREL type & addon combinations.
    
    Change-Id: Id77d6eec59ae07b48e0cfd870fe3cca6312537f5
    Signed-off-by: Alexey Makhalov <alexey.makhalov@broadcom.com>
    Signed-off-by: Vamsi Krishna Brahmajosyula <vamsi-krishna.brahmajosyula@broadcom.com>
    Reviewed-on: http://photon-gerrit.lvn.broadcom.net/c/photon/+/25982
    Reviewed-by: Srinidhi Rao <srinidhi.rao@broadcom.com>
    Reviewed-by: Ankit Jain <ankit-aj.jain@broadcom.com>

--- SPECS/linux/v6.12/canister_builder/crypto/fips_integrity.h
+++ SPECS/linux/v6.12/canister_builder/crypto/fips_integrity.h
@@ -13,13 +13,19 @@
  */
 #include <linux/types.h>
 
-#define SREL_INSN_TYPE_ADD_1				0x0		/* "0000" = Rel type 1, Addend -4 */
-#define SREL_INSN_TYPE_ADD_2				0x1		/* "0001" = Rel type 1, Addend -5 */
-#define SREL_INSN_TYPE_ADD_3				0x2		/* "0010" = Rel type 1, Addend 0 */
-#define SREL_INSN_TYPE_ADD_4				0x3		/* "0011" = Rel type 2, Addend 0 */
-#define SREL_INSN_TYPE_ADD_5				0x6		/* "0110" = Rel type 1, Addend 4 */
-#define SREL_INSN_TYPE_ADD_6				0x5		/* "0101" = Rel type 0, Addend 5 */
-#define SREL_INSN_TYPE_ADD_7				0x7		/* "0111" = Rel type 3, Addend 0 */
+/*
+ * Relocation type & Addend combinations for SREL instruction
+ *
+ *			imm		type	addend
+ */
+#define SREL_INSN_TA_0	0	/*	0	0	*/
+#define SREL_INSN_TA_1	1	/*	0	5	*/
+#define SREL_INSN_TA_2	2	/*	1	-5	*/
+#define SREL_INSN_TA_3	3	/*	1	-4	*/
+#define SREL_INSN_TA_4	4	/*	1	0	*/
+#define SREL_INSN_TA_5	5	/*	1	4	*/
+#define SREL_INSN_TA_6	6	/*	2	0	*/
+#define SREL_INSN_TA_7	7	/*	3	0	*/
 
 struct __attribute__((packed)) relocation {
 	unsigned char section;
