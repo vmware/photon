b38cca8524e00468440bbc89d327302153703bb4
Bo Gan <bo.gan@broadcom.com>
2026-04-16 17:06:32 -0700

    package-builder: logger: enhancement for auto-closing file handle
    To avoid running out of file descriptors used by log file handlers,
    garbage collect them. Wrap the builtin logging.Logger with another
    layer (into our custom Logger object), and track them with weak-ref.
    When the custom Logger gets GC'ed, close the file handler associated
    with the builtin Logger object.
    
    Also fixes the issue where we were wrongly setting the log-level,
    and the handlers of the root logger, instead of individual ones.
    
    Change-Id: I8c04bbd6ed804f66b4243b3c774a07eefaf86e96
    Reviewed-on: http://photon-gerrit.lvn.broadcom.net/c/photon/+/27648
    Reviewed-by: Brennan Lamoreaux <brennan.lamoreaux@broadcom.com>
    Reviewed-by: gerrit-photon <svc.photon-ci@broadcom.com>
    Reviewed-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
    Tested-by: gerrit-photon <svc.photon-ci@broadcom.com>

--- support/package-builder/Logger.py
+++ support/package-builder/Logger.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
-import os
 import logging
 import threading
+import weakref
+import time
+import os
 
+LOG_FORMAT = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+
+_loggers_alive = {}
+# _loggers_alive maps builtin logging.Logger to our Logger class.
+# Our Logger class holds the reference to the corresponding loging.Logger and file handle
+# Let l = logging.getLogger(module)
+#  1. Logger is not yet created: `l` not in _loggers_alive
+#  2. Logger is known, but dead or just created: `_loggers_alive[l]` maps to a dead weak ref
+#  3. Logger is alive: `_loggers_alive[l]` maps to a live weak ref
+# State transitions:
+#  1 -> 2 (lock held)
+#  2 -> 3 (lock held)
+#  3 -> 2 (lockless)
+
+_lock = threading.Lock()
+_console = logging.StreamHandler()
+_console.setFormatter(logging.Formatter(LOG_FORMAT))
+
+def _finalize_logger(l, fh):
+    fh.close()
+    l.removeHandler(fh)
 
 class Logger(object):
-    _configured = False
-    _lock = threading.Lock()
 
     @staticmethod
     def string_to_loglevel(loglevel):
@@ -19,41 +40,75 @@ class Logger(object):
         }.get(loglevel, logging.INFO)
 
     @staticmethod
-    def getLogger(mymodule="Main", logpath=None, loglevel="info"):
-        with Logger._lock:
-            if Logger._configured:
-                return logging.getLogger(mymodule)
-
-            logfile = f"{mymodule}.log"
-            if logpath:
+    def getLogger(module="Main", logpath=None, loglevel="info"):
+        l = logging.getLogger(module)
+        with _lock:
+            logger_ref = _loggers_alive.get(l)
+            if logger_ref is None:
+                # module is not known yet
+                l.setLevel(Logger.string_to_loglevel(loglevel))
+                l.addHandler(_console)
+            else:
+                logger = logger_ref()
+                if logger is not None:
+                    # We are racing with some other thread, and the logger is alive again
+                    return logger
+            # module logger is dead, dying, or just created
+            while len(l.handlers) > 1:
+                # Yield and wait for the finalizer to complete
+                time.sleep(0)
+            logfile = f'{module}.log'
+            if logpath is not None:
                 os.makedirs(logpath, exist_ok=True)
-                logfile = f"{logpath}/{logfile}"
-
-            root = logging.getLogger()
-            root.setLevel(Logger.string_to_loglevel(loglevel))
-
-            # File handler (ONE)
+                logfile = f'{logpath}/{logfile}'
             fh = logging.FileHandler(logfile)
-            fh.setFormatter(
-                logging.Formatter("%(asctime)s - %(name)s - %(message)s")
-            )
+            fh.setFormatter(logging.Formatter(LOG_FORMAT))
+            l.addHandler(fh)
+            logger = Logger(l)
+            weakref.finalize(logger, _finalize_logger, l, fh)
+            _loggers_alive[l] = weakref.ref(logger)
+            return logger
 
-            # Console handler (ONE)
-            ch = logging.StreamHandler()
-            ch.setFormatter(logging.Formatter("%(message)s"))
+    def debug(self, *args, **kwargs):
+        self.l.debug(*args, **kwargs)
 
-            root.addHandler(fh)
-            root.addHandler(ch)
+    def info(self, *args, **kwargs):
+        self.l.info(*args, **kwargs)
 
-            Logger._configured = True
+    def warning(self, *args, **kwargs):
+        self.l.warning(*args, **kwargs)
 
-        return logging.getLogger(mymodule)
+    def error(self, *args, **kwargs):
+        self.l.error(*args, **kwargs)
 
+    def exception(self, *args, **kwargs):
+        self.l.exception(*args, **kwargs)
+
+    def __init__(self, l):
+        self.l = l
+
+
+def test1(i):
+    logger = Logger.getLogger('mymodule', loglevel='debug')
+    logger.debug(f'test1 {i}')
+    logger.info(f'test1 {i}')
+
+def test2(i):
+    logger = Logger.getLogger('mymodule', loglevel='debug')
+    logger.warning(f'test2 {i}')
+    logger.error(f'test2 {i}')
+
+def thread_func(t, n):
+    i = 0
+    while n:
+        t(i)
+        n = n - 1
+        i = i + 1
 
 if __name__ == "__main__":
-    # Logger.getLogger("my module")
-    t1 = Logger.getLogger("my module")
-    t1.info("test1")
-
-    t2 = Logger.getLogger("my module")
-    t2.info("test2")
+    t1 = threading.Thread(target=thread_func, args=(test1, 10000))
+    t2 = threading.Thread(target=thread_func, args=(test2, 10000))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
