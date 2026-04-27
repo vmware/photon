vmware/photon

commit 3bdb879fb4bce6352150d953923256ca7fd8c3a1

SpecData: locate linux.spec in subrelease-specific paths too
Also look under <specDir>/<subrelease>/linux/ so that
SPECS/<subrelease>/linux/linux.spec is picked up alongside the
existing <specDir>/linux/ layout.

Change-Id: I2cb5d6a11520d2b0cef171b970bffcd10b9f6003
Signed-off-by: Ankit Jain <ankit-aj.jain@broadcom.com>
Reviewed-on: http://photon-gerrit.lvn.broadcom.net/c/photon/+/27700
Reviewed-by: Vamsi Krishna Brahmajosyula <vamsi-krishna.brahmajosyula@broadcom.com>
Tested-by: gerrit-photon <svc.photon-ci@broadcom.com>
Reviewed-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>

support/package-builder/SpecData.py
@@ -557,17 +557,26 @@ class SPECS(object):
     def initialize(self):
         defPkg = None
 
+        # linux.spec can live under SPECS/linux/ or SPECS/<subrelease>/linux/.
+        # The active one is the first that is not skipped (via build_if).
         for specDir in constants.specPaths:
             if defPkg:
                 break
-            specDir = f"{specDir}/linux"
-            for root, _, files in os.walk(specDir):
-                if "linux.spec" in files:
-                    spec = SpecParser(f"{root}/linux.spec", constants.buildArch)
-                    if spec.skipSpec:
-                        continue
-                    defPkg = spec.packages.get("default")
+            linuxDirs = [f"{specDir}/linux"]
+            if constants.subreleaseVersion:
+                linuxDirs.append(f"{specDir}/{constants.subreleaseVersion}/linux")
+            for linuxDir in linuxDirs:
+                if defPkg:
                     break
+                if not os.path.isdir(linuxDir):
+                    continue
+                for root, _, files in os.walk(linuxDir):
+                    if "linux.spec" in files:
+                        spec = SpecParser(f"{root}/linux.spec", constants.buildArch)
+                        if spec.skipSpec:
+                            continue
+                        defPkg = spec.packages.get("default")
+                        break
 
         kernelversion = defPkg.version
         kernelrelease = defPkg.release

commit 2affb48f5249ad8eb52a1389c366d8e2a003990c

SpecData: scan all SPECS/<dir>/linux/ for linux.spec
The previous change only checked SPECS/<subrelease>/linux/, which
misses kernels pinned to other subreleases via build_if (e.g.
SPECS/91/linux/ with build_if <= 91 also covers subrelease 90).
Glob SPECS/*/linux/ so any such directory is considered.

Tested with following command:
PHOTON_SUBRELEASE=90 make generate-pkg-info

Fixes: 3bdb879fb4bc ("SpecData: locate linux.spec in subrelease-specific paths too")
Change-Id: I6cb7f9437be1565ade4213ae272679129bb8202f
Signed-off-by: Ankit Jain <ankit-aj.jain@broadcom.com>
Reviewed-on: http://photon-gerrit.lvn.broadcom.net/c/photon/+/27720
Reviewed-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
Tested-by: gerrit-photon <svc.photon-ci@broadcom.com>
Reviewed-by: Michelle Wang <michelle.wang@broadcom.com>

support/package-builder/SpecData.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import glob
 import os
 import re
 import copy
@@ -557,19 +558,18 @@ class SPECS(object):
     def initialize(self):
         defPkg = None
 
-        # linux.spec can live under SPECS/linux/ or SPECS/<subrelease>/linux/.
+        # linux.spec can live under SPECS/linux/ or SPECS/<dir>/linux/
+        # (e.g. SPECS/91/linux/ for kernels pinned to older subreleases).
         # The active one is the first that is not skipped (via build_if).
         for specDir in constants.specPaths:
             if defPkg:
                 break
-            linuxDirs = [f"{specDir}/linux"]
-            if constants.subreleaseVersion:
-                linuxDirs.append(f"{specDir}/{constants.subreleaseVersion}/linux")
+            linuxDirs = sorted(
+                glob.glob(f"{specDir}/linux") + glob.glob(f"{specDir}/*/linux")
+            )
             for linuxDir in linuxDirs:
                 if defPkg:
                     break
-                if not os.path.isdir(linuxDir):
-                    continue
                 for root, _, files in os.walk(linuxDir):
                     if "linux.spec" in files:
                         spec = SpecParser(f"{root}/linux.spec", constants.buildArch)