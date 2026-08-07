vmware/photon

commit b3da2fa729ea7f7dc8cdf7525ca36b89467da5a0

tini: Initial commit
Also seperate out tini from docker spec

Change-Id: Ieb250d9a9dee113f9a929c75a8f821542014a13f
Reviewed-on: http://photon-gerrit.lvn.broadcom.net/c/photon/+/25555
Reviewed-by: Shreenidhi Shedi <shreenidhi.shedi@broadcom.com>
Reviewed-by: Harinadh Dommaraju <harinadh.dommaraju@broadcom.com>
Reviewed-by: Tapas Kundu <tapas.kundu@broadcom.com>
Tested-by: gerrit-photon <svc.photon-ci@broadcom.com>

SPECS/docker/docker.spec
@@ -5,7 +5,6 @@
 # Must be in sync with package version
 %define DOCKER_ENGINE_GITCOMMIT 3ab5c7d
 %define DOCKER_CLI_GITCOMMIT 3ab4256
-%define TINI_GITCOMMIT de40ad0
 
 %define gopath_comp_engine github.com/docker/docker
 %define gopath_comp_cli github.com/docker/cli
@@ -14,7 +13,7 @@
 Summary:        Docker
 Name:           docker
 Version:        27.3.1
-Release:        4%{?dist}
+Release:        5%{?dist}
 URL:            http://docs.docker.com
 Group:          Applications/File
 Vendor:         VMware, Inc.
@@ -22,21 +21,18 @@ Distribution:   Photon
 
 Source0: https://github.com/moby/moby/archive/moby-%{version}.tar.gz
 
-Source1: https://github.com/krallin/tini/archive/tini-0.19.0.tar.gz
+Source1: https://github.com/docker/libnetwork/archive/libnetwork-64b7a45.tar.gz
 
-Source2: https://github.com/docker/libnetwork/archive/libnetwork-64b7a45.tar.gz
+Source2: https://github.com/docker/cli/archive/refs/tags/docker-cli-%{version}.tar.gz
 
-Source3: https://github.com/docker/cli/archive/refs/tags/docker-cli-%{version}.tar.gz
+Source3:       docker-post19.service
+Source4:       docker-post19.socket
+Source5:       default-disable.preset
 
-Source4:       docker-post19.service
-Source5:       docker-post19.socket
-Source6:       default-disable.preset
+Source6: license.txt
+%include %{SOURCE6}
 
-Source7: license.txt
-%include %{SOURCE7}
-
-Patch0:        tini-disable-git.patch
-Patch1:        bridge-networking.patch
+Patch0:        bridge-networking.patch
 
 BuildRequires:  systemd-devel
 BuildRequires:  device-mapper-devel
@@ -58,6 +54,7 @@ Requires:       docker-engine = %{version}-%{release}
 Requires:       docker-cli = %{version}-%{release}
 # bash completion uses awk
 Requires:       gawk
+Requires:       tini
 
 %description
 Docker is an open source project to build, ship and run any application as a lightweight container.
@@ -110,28 +107,20 @@ Use dockerd-rootless-setuptool.sh to setup systemd for dockerd-rootless.sh.
 # Using autosetup is not feasible
 %setup -q -c -n moby-%{version}
 pushd moby-%{version}
-%autopatch -p1 1
+%autopatch -p1 0
 popd
 
 mkdir -p "$(dirname "src/%{gopath_comp_engine}")" \
          "$(dirname "src/%{gopath_comp_cli}")" \
          "src/%{gopath_comp_libnetwork}" \
-         tini \
          bin
 
 mv moby-%{version} src/%{gopath_comp_engine}
 
-tar -xf %{SOURCE3}
+tar -xf %{SOURCE2}
 mv cli-%{version} src/%{gopath_comp_cli}
 
-tar -C tini -xf %{SOURCE1}
-
-tar -C src/%{gopath_comp_libnetwork} -xf %{SOURCE2}
-
-# Patch sources
-pushd tini
-%autopatch -p1 0
-popd
+tar -C src/%{gopath_comp_libnetwork} -xf %{SOURCE1}
 
 %build
 export GOPATH="${PWD}"
@@ -175,17 +164,6 @@ pushd "src/%{gopath_comp_libnetwork}"
   go build -buildmode=pie -ldflags=-linkmode=external -o "$GOPATH/bin/docker-proxy" %{gopath_comp_libnetwork}/cmd/proxy
 popd
 
-# init
-pushd tini
-%cmake \
-    -Dtini_VERSION_GIT:STRING=%{TINI_GITCOMMIT} \
-    -Dgit_version_check_ret=0
-
-cd %{__cmake_builddir}
-make tini-static %{?_smp_mflags}
-cp tini-static "$GOPATH/bin/docker-init"
-popd
-
 jq -n \
   --arg platform "$PLATFORM" \
   --arg engine_image "$ENGINE_IMGE" \
@@ -212,14 +190,14 @@ install -p -m 755 src/%{gopath_comp_engine}/bundles/dynbinary-daemon/dockerd %{b
 install -p -m 755 bin/docker-proxy %{buildroot}%{_bindir}/docker-proxy
 
 # install tini
-install -p -m 755 bin/docker-init %{buildroot}%{_bindir}/docker-init
+ln -srv %{buildroot}%{_bindir}/tini %{buildroot}%{_bindir}/docker-init
 
 # install udev rules
 install -p -m 644 src/%{gopath_comp_engine}/contrib/udev/80-docker.rules %{buildroot}%{_udevrulesdir}/80-docker.rules
 
 # add init scripts
-install -p -m 644 %{SOURCE4} %{buildroot}%{_unitdir}/docker.service
-install -p -m 644 %{SOURCE5} %{buildroot}%{_unitdir}/docker.socket
+install -p -m 644 %{SOURCE3} %{buildroot}%{_unitdir}/docker.service
+install -p -m 644 %{SOURCE4} %{buildroot}%{_unitdir}/docker.socket
 
 # add docker-engine metadata
 install -p -m 644 distribution_based_engine.json %{buildroot}%{_sharedstatedir}/docker-engine/distribution_based_engine.json
@@ -242,7 +220,7 @@ for cli_file in AUTHORS LICENSE MAINTAINERS NOTICE README.md; do
   cp "src/%{gopath_comp_cli}/$cli_file" "build-docs/cli-$cli_file"
 done
 
-install -v -D -m 0644 %{SOURCE6} %{buildroot}%{_presetdir}/50-docker.preset
+install -v -D -m 0644 %{SOURCE5} %{buildroot}%{_presetdir}/50-docker.preset
 
 # docker-rootless
 install -D -p -m 0755 %{_builddir}/moby-%{version}/src/github.com/docker/docker/contrib/dockerd-rootless.sh %{buildroot}%{_bindir}/dockerd-rootless.sh
@@ -324,6 +302,8 @@ rm -rf %{buildroot}/*
 %{_bindir}/dockerd-rootless-setuptool.sh
 
 %changelog
+* Mon Apr 21 2025 Harinadh Dommaraju <Harinadh.Dommaraju@broadcom.com> 27.3.1-5
+- Make tini a separate package
 * Fri Jan 10 2025 Vamsi Krishna Brahmajosyula <vamsi-krishna.brahmajosyula@broadcom.com> 27.3.1-4
 - Fix go input dependencies which have Capital letters in name.
 * Wed Jan 08 2025 Vamsi Krishna Brahmajosyula <vamsi-krishna.brahmajosyula@broadcom.com> 27.3.1-3

SPECS/tini/tini.spec
@@ -1,0 +1,50 @@
+%global debug_package %{nil}
+%define TINI_GITCOMMIT de40ad0
+
+Name:           tini
+Version:        0.19.0
+Release:        1%{?dist}
+Summary:        A tiny but valid init for containers
+Vendor:         VMware, Inc.
+Group:          System Environment/Base
+Distribution:   Photon
+URL:            https://github.com/krallin/tini
+Source0:        https://github.com/krallin/tini/archive/%{name}-%{version}.tar.gz
+
+Source1: license.txt
+%include %{SOURCE1}
+
+BuildRequires:  cmake
+BuildRequires:  gcc
+BuildRequires:  glibc-devel
+
+%description
+Tini is a trivial implementation for an "init" program.
+All Tini does is spawn a single child (Tini is meant to be run in a container),
+and wait for it to exit, all the while reaping zombies and performing signal forwarding.
+libc will be needed inside the container.
+
+%prep
+%autosetup -p1 -c
+
+%build
+CFLAGS="${CFLAGS-} -DPR_SET_CHILD_SUBREAPER=36 -DPR_GET_CHILD_SUBREAPER=37"
+export CFLAGS
+
+%cmake \
+    -Dtini_VERSION_GIT:STRING=%{TINI_GITCOMMIT} \
+    -Dgit_version_check_ret=0
+
+%cmake_build
+
+%install
+%cmake_install
+
+%files
+%defattr(-,root,root,-)
+%{_bindir}/tini
+%exclude %{_bindir}/tini-static
+
+%changelog
+* Tue Apr 08 2025 Harinadh Dommaraju <Harinadh.Dommaraju@broadcom.com> 0.19.0-1
+- Initial build

SPECS/docker/tini-disable-git.patch
@@ -1,34 +1,0 @@
-From a778da75578051d31a4badcf781f6d3af2b6b65b Mon Sep 17 00:00:00 2001
-From: Bo Gan <ganb@vmware.com>
-Date: Mon, 28 Oct 2019 17:14:22 -0700
-Subject: [PATCH] disable git invocation
-
----
- CMakeLists.txt | 4 ++++
- 1 file changed, 4 insertions(+)
-
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 490bec8..0c897a7 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -13,6 +13,8 @@ if(MINIMAL)
- 	add_definitions(-DTINI_MINIMAL=1)
- endif()
- 
-+if(FALSE)
-+
- # Extract git version and dirty-ness
- execute_process (
-   COMMAND git --git-dir "${PROJECT_SOURCE_DIR}/.git" --work-tree "${PROJECT_SOURCE_DIR}" log -n 1 --date=local --pretty=format:%h
-@@ -27,6 +29,8 @@ execute_process(
-   OUTPUT_VARIABLE git_dirty_check_out
- )
- 
-+endif()
-+
- if("${git_version_check_ret}" EQUAL 0)
-   set(tini_VERSION_GIT " - git.${tini_VERSION_GIT}")
-   if(NOT "${git_dirty_check_out}" STREQUAL "")
--- 
-2.7.4
-

SPECS/docker/config.yaml
@@ -6,10 +6,10 @@ sources:
     package:
       detailed_description: Docker CLI
       home_page: https://github.com/docker/cli
-      license_concluded: BSD-3-Clause AND BSD-2-Clause AND (BSD-3-Clause AND Apache-2.0)
-        AND GPL-1.0-or-later AND CC-BY-SA-4.0 AND Apache-2.0 AND MIT
-      license_declared: BSD-3-Clause AND BSD-2-Clause AND (BSD-3-Clause AND Apache-2.0)
-        AND GPL-1.0-or-later AND CC-BY-SA-4.0 AND Apache-2.0 AND MIT
+      license_concluded: BSD-3-Clause AND CC-BY-SA-4.0 AND Apache-2.0 AND BSD-2-Clause
+        AND MIT
+      license_declared: BSD-3-Clause AND CC-BY-SA-4.0 AND Apache-2.0 AND BSD-2-Clause
+        AND MIT
       short_summary: Docker CLI
       supplier: 'Organization: Broadcom, Inc.'
 - archive: libnetwork-64b7a45.tar.gz
@@ -34,24 +34,11 @@ sources:
       detailed_description: 'Docker is an open source project to build, ship and run
         any application as a lightweight container. '
       home_page: http://docs.docker.com
-      license_concluded: BSD-2-Clause AND GPL-1.0-or-later AND MIT AND CC-BY-4.0 AND
-        MPL-2.0 AND GPL-2.0-only AND CC0-1.0 AND ISC AND (Apache-2.0 OR GPL-2.0-or-later)
-        AND BSD-3-Clause AND Apache-2.0 AND (Apache-2.0 OR GPL-2.0-or-later) AND CC-BY-SA-4.0
-        AND (BSD-3-Clause AND Apache-2.0)
-      license_declared: BSD-2-Clause AND GPL-1.0-or-later AND MIT AND CC-BY-4.0 AND
-        MPL-2.0 AND GPL-2.0-only AND CC0-1.0 AND ISC AND (Apache-2.0 OR GPL-2.0-or-later)
-        AND BSD-3-Clause AND Apache-2.0 AND (Apache-2.0 OR GPL-2.0-or-later) AND CC-BY-SA-4.0
-        AND (BSD-3-Clause AND Apache-2.0)
+      license_concluded: (OpenSSL OR BSD-3-Clause) AND (Apache-2.0 OR GPL-2.0-or-later)
+        AND MIT AND ISC AND CC-BY-SA-4.0 AND CC0-1.0 AND GPL-2.0-only AND BSD-2-Clause
+        AND Apache-2.0 AND CC-BY-4.0 AND GPL-1.0-or-later AND BSD-3-Clause AND MPL-2.0
+      license_declared: (OpenSSL OR BSD-3-Clause) AND (Apache-2.0 OR GPL-2.0-or-later)
+        AND MIT AND ISC AND CC-BY-SA-4.0 AND CC0-1.0 AND GPL-2.0-only AND BSD-2-Clause
+        AND Apache-2.0 AND CC-BY-4.0 AND GPL-1.0-or-later AND BSD-3-Clause AND MPL-2.0
       short_summary: Docker
-      supplier: 'Organization: Broadcom, Inc.'
-- archive: tini-0.19.0.tar.gz
-  archive_sha512sum: 3591a6db54b8f35c30eafc6bbf8903926c382fd7fe2926faea5d95c7b562130b5264228df550f2ad83581856fd5291cf4aab44ee078aef3270c74be70886055c
-  skip_validation: true
-  spdx:
-    package:
-      detailed_description: A tiny but valid init for containers
-      home_page: https://github.com/krallin/tini
-      license_concluded: MIT
-      license_declared: MIT
-      short_summary: A tiny but valid init for containers
       supplier: 'Organization: Broadcom, Inc.'

SPECS/tini/config.yaml
@@ -1,0 +1,23 @@
+sources:
+- archive: tini-0.19.0.tar.gz
+  archive_type: None
+  skip_validation: true
+  archive_sha512sum: 3591a6db54b8f35c30eafc6bbf8903926c382fd7fe2926faea5d95c7b562130b5264228df550f2ad83581856fd5291cf4aab44ee078aef3270c74be70886055c
+  name: tini-0.19.0
+  version: "0.19.0"
+  url: https://github.com/krallin/tini/archive/refs/tags/v0.19.0.tar.gz
+  repo_url: https://github.com/krallin/tini.git
+  commit_id: de40ad007797e0dcd8b7126f27bb87401d224240
+  skip_list: []
+  missing: []
+  spdx:
+      package:
+          detailed_description: Tini is a trivial implementation for an "init" program.
+                All Tini does is spawn a single child (Tini is meant to be run in a container),
+                and wait for it to exit, all the while reaping zombies and performing signal forwarding.
+                libc will be needed inside the contain
+          home_page: https://github.com/krallin/tini
+          license_concluded: MIT
+          license_declared: MIT
+          short_summary: A tiny but valid init for containers
+          supplier: 'Organization: Broadcom, Inc.'

commit 447764f375c3f7465e89b33dea17116f3b0f1aa8

docker, tini: link docker-init statically to fix `docker run --init`
`docker run --init` fails on any container whose rootfs has no glibc loader:

    exec /sbin/docker-init: no such file or directory

/sbin/docker-init is not a host path. It is the hardcoded in-container
destination (inContainerInitPath in moby daemon/oci_linux.go); the daemon
resolves the host binary via LookupInitPath() and bind-mounts it there, then
execs it as PID 1 inside the container. Because /usr/bin/docker-init is a
dynamic PIE, the kernel must load /lib64/ld-linux-x86-64.so.2 from the
*container's* rootfs. Alpine/musl, scratch and distroless images do not have
it, and execve reports the missing interpreter as ENOENT - which surfaces as
"no such file or directory" for a file that plainly exists.

This regressed in commit b3da2fa72 ("tini: Initial commit / Also seperate out
tini from docker spec"), which is present on both 5.0 and 6.0. Before it,
docker.spec built tini itself and installed a statically linked binary:

    make tini-static
    cp tini-static "$GOPATH/bin/docker-init"

after it, docker-init became a symlink to the dynamically linked /usr/bin/tini
from the new standalone tini package, whose spec builds tini-static and then
discards it with %exclude.

Upstream moby links docker-init statically on purpose
(hack/dockerfile/install/tini.installer runs `make tini-static`, and the
Dockerfile asserts it with `xx-verify --static`). Fedora does the same: its
tini package ships a tini-static subpackage that moby-engine depends on.
photon master/dev/4.0 are unaffected, they still build it statically.

Changes:

  tini: package tini-static in a new subpackage instead of excluding it. The
  binary is already built and installed into the buildroot by %cmake_build /
  %cmake_install - the %exclude only existed to stop RPM's unpackaged-files
  check from failing the build.

  tini: restore tini-disable-git.patch, dropped by b3da2fa72. tini's

SPECS/docker/docker.spec
@@ -12,7 +12,7 @@
 Summary:        Docker
 Name:           docker
 Version:        28.2.2
-Release:        3%{?dist}
+Release:        4%{?dist}
 URL:            http://docs.docker.com
 Group:          Applications/File
 Vendor:         VMware, Inc.
@@ -56,6 +56,11 @@ Docker is an open source project to build, ship and run any application as a lig
 
 %package        engine
 Summary:        Docker Engine
+# docker-init is bind-mounted into containers and exec'd as PID 1 there, so it
+# must not depend on the container's libc. The dependency is versioned because
+# tini-static first appears in tini-0.19.0-2; an unversioned one would let an
+# older tini satisfy it and leave /usr/bin/docker-init dangling.
+Requires:       tini-static >= 0.19.0-2
 Requires:       libapparmor
 Requires:       libseccomp
 Requires:       libltdl
@@ -188,7 +193,7 @@ install -p -m 755 src/%{gopath_comp_engine}/bundles/dynbinary-daemon/dockerd %{b
 install -p -m 755 src/%{gopath_comp_engine}/bundles/dynbinary-daemon/docker-proxy %{buildroot}%{_bindir}/docker-proxy
 
 # install tini
-ln -srv %{buildroot}%{_bindir}/tini %{buildroot}%{_bindir}/docker-init
+ln -srv %{buildroot}%{_bindir}/tini-static %{buildroot}%{_bindir}/docker-init
 
 # install udev rules
 install -p -m 644 src/%{gopath_comp_engine}/contrib/udev/80-docker.rules %{buildroot}%{_udevrulesdir}/80-docker.rules
@@ -298,6 +303,8 @@ rm -rf %{buildroot}/*
 %{_bindir}/dockerd-rootless-setuptool.sh
 
 %changelog
+* Fri Aug 07 2026 Daniel Casota <dcasota@gmail.com> 28.2.2-4
+- Point docker-init at tini-static so --init works in non-glibc containers
 * Mon Nov 17 2025 Mukul Sikka <mukul.sikka@broadcom.com> 28.2.2-3
 - Bump up version as a part of containerd upgrade
 * Mon Nov 10 2025 Mukul Sikka <mukul.sikka@broadcom.com> 28.2.2-2

SPECS/tini/tini-disable-git.patch
@@ -1,0 +1,34 @@
+From a778da75578051d31a4badcf781f6d3af2b6b65b Mon Sep 17 00:00:00 2001
+From: Bo Gan <ganb@vmware.com>
+Date: Mon, 28 Oct 2019 17:14:22 -0700
+Subject: [PATCH] disable git invocation
+
+---
+ CMakeLists.txt | 4 ++++
+ 1 file changed, 4 insertions(+)
+
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 490bec8..0c897a7 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -13,6 +13,8 @@ if(MINIMAL)
+ 	add_definitions(-DTINI_MINIMAL=1)
+ endif()
+ 
++if(FALSE)
++
+ # Extract git version and dirty-ness
+ execute_process (
+   COMMAND git --git-dir "${PROJECT_SOURCE_DIR}/.git" --work-tree "${PROJECT_SOURCE_DIR}" log -n 1 --date=local --pretty=format:%h
+@@ -27,6 +29,8 @@ execute_process(
+   OUTPUT_VARIABLE git_dirty_check_out
+ )
+ 
++endif()
++
+ if("${git_version_check_ret}" EQUAL 0)
+   set(tini_VERSION_GIT " - git.${tini_VERSION_GIT}")
+   if(NOT "${git_dirty_check_out}" STREQUAL "")
+-- 
+2.7.4
+

SPECS/tini/tini.spec
@@ -3,7 +3,7 @@
 
 Name:           tini
 Version:        0.19.0
-Release:        1%{?dist}
+Release:        2%{?dist}
 Summary:        A tiny but valid init for containers
 Vendor:         VMware, Inc.
 Group:          System Environment/Base
@@ -14,6 +14,13 @@ Source0:        https://github.com/krallin/tini/archive/%{name}-%{version}.tar.g
 Source1: license.txt
 %include %{SOURCE1}
 
+# tini's CMakeLists.txt overwrites tini_VERSION_GIT and git_version_check_ret
+# from execute_process(), which clobbers the -D values passed below. An RPM
+# builds from a tarball with no .git, so the git call fails and the version
+# suffix is silently dropped, leaving `docker info` with an empty init version.
+# This patch disables the git invocation so the -D values take effect.
+Patch0:         tini-disable-git.patch
+
 BuildRequires:  cmake
 BuildRequires:  gcc
 BuildRequires:  glibc-devel
@@ -23,6 +30,16 @@ Tini is a trivial implementation for an "init" program.
 All Tini does is spawn a single child (Tini is meant to be run in a container),
 and wait for it to exit, all the while reaping zombies and performing signal forwarding.
 libc will be needed inside the container.
+
+%package        static
+Summary:        Standalone static build of tini
+
+%description    static
+Statically linked build of tini, meant to be used inside a container.
+Because this binary carries no dynamic dependencies it can be bind-mounted
+into a container built against any libc (glibc, musl) or into one with no
+libc at all, which is what the container runtime requires of an init that
+it injects from the host.
 
 %prep
 %autosetup -p1 -c
@@ -43,8 +60,14 @@ export CFLAGS
 %files
 %defattr(-,root,root,-)
 %{_bindir}/tini
-%exclude %{_bindir}/tini-static
+
+%files static
+%defattr(-,root,root,-)
+%{_bindir}/tini-static
 
 %changelog
+* Fri Aug 07 2026 Daniel Casota <dcasota@gmail.com> 0.19.0-2
+- Ship tini-static in a new subpackage instead of discarding it
+- Restore tini-disable-git.patch so the version suffix is not dropped
 * Tue Apr 08 2025 Harinadh Dommaraju <Harinadh.Dommaraju@broadcom.com> 0.19.0-1
 - Initial build